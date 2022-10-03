83296d3f1a3b62e139bbda1c948e9a85ff613909
antip00 <antipov@altinity.com>
2022-10-04 02:28:08 +0300

    Adding load and backup tests.

--- lightweight_delete/tests/disk_space.py
+++ lightweight_delete/tests/disk_space.py
@@ -1,3 +1,5 @@
+import time
+
 from lightweight_delete.requirements import *
 from lightweight_delete.tests.steps import *
 
@@ -7,22 +9,49 @@ from lightweight_delete.tests.steps import *
 def disk_space(self, node=None):
     """Check that clickhouse does not greatly increase table size after lightweight delete."""
 
-    with When("I have a table"):
-        pass
+    if node is None:
+        node = self.context.node
 
-    with When("I insert data into the table"):
-        pass
+    table_name = f"table_{getuid()}"
+
+    with Given("I have a table"):
+        create_table(table_name=table_name)
+
+    with When(
+        "I insert data into the table",
+        description="100 partitions 1 part block_size=10000",
+    ):
+        insert(
+            table_name=table_name,
+            partitions=100,
+            parts_per_partition=1,
+            block_size=1000,
+            settings=[("mutations_sync", "2")],
+        )
 
     with When("I measure table size on disk"):
-        pass
-
-    # todo may be stop merges
+        size_on_disk_before_deletions = int(
+            node.query(
+                f"select sum(bytes_on_disk) from system.parts where table = '{table_name}'"
+            ).output
+        )
 
     with When("I perform a lot of deletes"):
-        pass
+        for i in range(30):
+            for j in range(20):
+                delete(
+                    table_name=table_name,
+                    condition=f"id = {i} and x = {j}",
+                    settings=[],
+                )
 
     with Then("I check table size on disk does not increased a lot"):
-        pass
+        size_on_disk_after_deletions = int(
+            node.query(
+                f"select sum(bytes_on_disk) from system.parts where table = '{table_name}' and active"
+            ).output
+        )
+        assert size_on_disk_after_deletions < 2 * size_on_disk_before_deletions, error()
 
 
 @TestFeature
