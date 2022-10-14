3f2ea0b6a83bc66a2911c04062668427e8e0e5ad
MyroTk <myroslav6@gmail.com>
2022-10-14 19:06:35 +0200

    Updated Parquet based on Wednesday review

--- parquet/tests/common.py
+++ parquet/tests/common.py
@@ -11,7 +11,10 @@ from testflows.asserts import values, error, snapshot
 
 @TestStep(When)
 def insert_test_data(self, name, node=None):
-    """Insert some data into table."""
+    """Insert data necessarily for Parquet testing into the specified table."""
+
+    xfail("Needs redesign")
+
     if node is None:
         node = self.context.node
 
@@ -53,14 +56,14 @@ def allow_experimental_map_type(self):
 
 @TestStep(Given)
 def table(self, engine, name="table0", create="CREATE"):
-    """Create a table."""
+    """Create or attach a table with specified name and engine."""
     node = current().context.node
 
     try:
         with By("creating table"):
             node.query(
                 f"""
-                {create} TABLE {name} ({all_test_data_types})
+                {create} TABLE {name} {generate_all_column_types()}
                 Engine = {engine}
             """
             )
@@ -110,7 +113,7 @@ def getuid():
 
 @TestStep(Given)
 def upload_parquet_to_aws_s3(self, s3_client):
-    """Upload Parquet data file to aws s3."""
+    """Upload Parquet file to aws s3 container."""
 
     with By("Uploading a file"):
         s3_client.upload_file(
@@ -122,7 +125,7 @@ def upload_parquet_to_aws_s3(self, s3_client):
 
 @TestStep(Then)
 def check_query_output(self, query, expected=None):
-    """Check the output of a query against either snapshot or provided values.
+    """Check the output of the provided query against either snapshot or provided values.
     """
 
     node = current().context.node
@@ -156,7 +159,7 @@ def check_source_file(self, path, expected=None):
     name = basename(current().name)
 
     with By("reading the file"):
-        r = node.command(f"python3 -c \"import pyarrow.parquet as pq;[print(i.columns) for i in pq.ParquetFile('{path}').iter_batches()];\"").output.strip()
+        r = node.command(f"python3 -c \"import pyarrow.parquet as pq; import pandas as pd;print(pq.ParquetFile('{path}').schema);print(pd.read_parquet('{path}', engine=pyarrow))\"").output.strip()
 
     if expected:
         with Then(f"result should match the expected values", description=expected):
@@ -178,7 +181,7 @@ def check_source_file(self, path, expected=None):
 
 @TestStep(Then)
 def check_aws_s3_file(self, s3_client, file, expected):
-    """Download file from aws s3 and check the contents."""
+    """Download specified file from aws s3 and check the contents."""
 
     with By("Downloading the file"):
         s3_client.download_file(self.context.uri, file, file)
@@ -191,11 +194,148 @@ def check_aws_s3_file(self, s3_client, file, expected):
 def check_mysql(self, name, mysql_node, expected):
     """NOT IMPLEMENTED. NEEDS REDESIGN."""
 
+    xfail("Needs redesign.")
+
     with By("I selecting from table using mysql"):
         msql_out = mysql_node.command(
             f"mysql -D default -u default -e 'SELECT * FROM {name} FORMAT Parquet'"
         ).output
         assert msql_out == expected, error()
+
+def generate_all_column_types():
+    """Generate a list of every type of column necessary for Parquet tests."""
+
+    basic_columns = [
+        "uint8 UInt8",
+        "int8 Int8",
+        "uint16 UInt16",
+        "int16 Int16",
+        "uint32 UInt32",
+        "int32 Int32",
+        "uint64 UInt64",
+        "int64 Int64",
+        "float32 Float32",
+        "float64 Float64",
+        "decimal Decimal128(38)",
+        "date Date",
+        "datetime DateTime",
+        "string String",
+        "fixedstring FixedString(16)"
+    ]
+
+    container_columns = [
+        "array Array(UInt8)",
+        "tuple Tuple(UInt8,Int8,UInt16,Int16,UInt32,Int32,UInt64,Int64,Float32,Float64,Decimal128(38),Date,DateTime,String,FixedString(8),"
+            "Array(UInt8),Tuple(UInt8,UInt8,UInt8),Map(String, UInt64))",
+        "map Map(String, UInt64)"
+    ]
+
+    array_columns = []
+    map_columns = []
+    null_columns = []
+    low_cardinality_columns = []
+
+    for type in basic_columns:
+        array_columns.append("array_" + type.split(" ",1)[0] + f" Array({type.split(' ',1)[1]})")
+        null_columns.append("nullable_" + type.split(" ",1)[0] + f" Nullable({type.split(' ',1)[1]})")
+        null_columns.append("array_nullable_" + type.split(" ",1)[0] + f" Array(Nullable({type.split(' ',1)[1]}))")
+
+        if type != 'decimal Decimal128(38)':
+            low_cardinality_columns.append('low_card_' + type.split(" ",1)[0] + f" LowCardinality({type.split(' ',1)[1]})")
+            low_cardinality_columns.append('array_low_card_' + type.split(" ",1)[0] + f" Array(LowCardinality({type.split(' ',1)[1]}))")
+            low_cardinality_columns.append('array_low_card_nullable_' + type.split(" ",1)[0] + f" Array(LowCardinality(Nullable({type.split(' ',1)[1]})))")
+
+        if type not in ['float32 Float32', 'float64 Float64', 'decimal Decimal128(38)']:
+            for value_type in basic_columns:
+                map_columns.append("map_" + type.split(" ",1)[0] + "_" + value_type.split(" ",1)[0] + f" Map({type.split(' ',1)[1]}, {value_type.split(' ',1)[1]})")
+                null_columns.append("map_" + type.split(" ",1)[0] + "_nullable_" + value_type.split(" ",1)[0] + f" Map({type.split(' ',1)[1]}, Nullable({value_type.split(' ',1)[1]}))")
+                low_cardinality_columns.append("map_low_card_" + type.split(" ",1)[0] + value_type.split(" ",1)[0] + f" Map(LowCardinality({type.split(' ',1)[1]}), {value_type.split(' ',1)[1]})")
+                low_cardinality_columns.append("map_low_card_" + type.split(" ",1)[0] + "_nullable_" + value_type.split(" ",1)[0] + f" Map(LowCardinality({type.split(' ',1)[1]}), Nullable({value_type.split(' ',1)[1]}))")
+
+                if value_type != 'decimal Decimal128(38)':
+                    low_cardinality_columns.append("map_low_card_" + type.split(" ",1)[0] + "_low_card_" + value_type.split(" ",1)[0] + f" Map(LowCardinality({type.split(' ',1)[1]}), LowCardinality({value_type.split(' ',1)[1]}))")
+                    low_cardinality_columns.append("map_low_card_" + type.split(" ",1)[0] + "_low_card_nullable_" + value_type.split(" ",1)[0] + f" Map(LowCardinality({type.split(' ',1)[1]}), LowCardinality(Nullable({value_type.split(' ',1)[1]})))")
+
+            for value_type in container_columns:
+                map_columns.append("map_" + type.split(" ",1)[0] + value_type.split(" ",1)[0] + f" Map({type.split(' ',1)[1]}, {value_type.split(' ',1)[1]})")
+
+    for type in container_columns:
+        array_columns.append("array_" + type.split(" ",1)[0] + f" Array({type.split(' ',1)[1]})")
+
+    null_columns.append("tuple_nullable Tuple("
+        "Nullable(UInt8),"
+        "Nullable(Int8),"
+        "Nullable(UInt16),"
+        "Nullable(Int16),"
+        "Nullable(UInt32),"
+        "Nullable(Int32),"
+        "Nullable(UInt64),"
+        "Nullable(Int64),"
+        "Nullable(Float32),"
+        "Nullable(Float64),"
+        "Nullable(Decimal128(38)),"
+        "Nullable(Date),"
+        "Nullable(DateTime),"
+        "Nullable(String),"
+        "Nullable(FixedString(8)),"
+        "Array(Nullable(UInt8)),"
+        "Tuple(Nullable(UInt8),"
+            "Nullable(UInt8),"
+            "Nullable(UInt8)),"
+        "Map(String, Nullable(UInt64)))"
+        )
+
+    low_cardinality_columns.append("tuple_low_cardinality Tuple("
+        "LowCardinality(UInt8),"
+        "LowCardinality(Int8),"
+        "LowCardinality(UInt16),"
+        "LowCardinality(Int16),"
+        "LowCardinality(UInt32),"
+        "LowCardinality(Int32),"
+        "LowCardinality(UInt64),"
+        "LowCardinality(Int64),"
+        "LowCardinality(Float32),"
+        "LowCardinality(Float64),"
+        "LowCardinality(Decimal128(38)),"
+        "LowCardinality(Date),"
+        "LowCardinality(DateTime),"
+        "LowCardinality(String),"
+        "LowCardinality(FixedString(8)),"
+        "Array(LowCardinality(UInt8)),"
+        "Tuple(LowCardinality(UInt8),"
+            "LowCardinality(UInt8),"
+            "LowCardinality(UInt8)),"
+        "Map(String, LowCardinality(UInt64)))"
+        )
+
+    low_cardinality_columns.append("tuple_low_cardinality_nullable Tuple("
+        "LowCardinality(Nullable(UInt8)),"
+        "LowCardinality(Nullable(Int8)),"
+        "LowCardinality(Nullable(UInt16)),"
+        "LowCardinality(Nullable(Int16)),"
+        "LowCardinality(Nullable(UInt32)),"
+        "LowCardinality(Nullable(Int32)),"
+        "LowCardinality(Nullable(UInt64)),"
+        "LowCardinality(Nullable(Int64)),"
+        "LowCardinality(Nullable(Float32)),"
+        "LowCardinality(Nullable(Float64)),"
+        "LowCardinality(Nullable(Decimal128(38))),"
+        "LowCardinality(Nullable(Date)),"
+        "LowCardinality(Nullable(DateTime)),"
+        "LowCardinality(Nullable(String)),"
+        "LowCardinality(Nullable(FixedString(8))),"
+        "Array(LowCardinality(Nullable(UInt8))),"
+        "Tuple(LowCardinality(Nullable(UInt8)),"
+            "LowCardinality(Nullable(UInt8)),"
+            "LowCardinality(Nullable(UInt8))),"
+        "Map(String, LowCardinality(Nullable(UInt64))))"
+        )
+
+    all_test_columns = basic_columns + container_columns + map_columns + array_columns + null_columns # + low_cardinality_columns
+    all_test_columns = "(" + ",".join(all_test_columns) + ")"
+
+    return all_test_columns
+
 
 all_test_data_types = """
     a UInt8,
--- parquet/tests/file.py
+++ parquet/tests/file.py
@@ -6,17 +6,15 @@ from helpers.common import *
 @TestScenario
 @Requirements(RQ_SRS_032_ClickHouse_Parquet_TableEngines_Special_File("1.0"))
 def insert_into_engine(self):
-    """Check that Parquet format is correctly writen into `File` table engine."""
-
-    if node is None:
-        node = self.context.node
+    """Check that when data is inserted into a table with File(Parquet) engine, it is written into the source file correctly."""
 
     table_name = "table_" + getuid()
 
-    with Given("I have a table"):
+    with Given("I have a table with a Parquet type file engine"):
         table(name=table_name, engine='File(Parquet)')
 
-    with When("I insert data into the table", desciption="Inserted data includes all of the ClickHouse data types supported by Parquet, including nested types and nulls"):
+    with When("I insert data into the table",
+        description="insert data includes all of the ClickHouse data types supported by Parquet, including nested types and nulls"):
         insert_test_data(name=table_name)
 
     with Then("I check that the data inserted into the table was correctly written to the file"):
@@ -25,26 +23,23 @@ def insert_into_engine(self):
 @TestScenario
 @Requirements(RQ_SRS_032_ClickHouse_Parquet_TableEngines_Special_File("1.0"))
 def select_from_engine(self):
-    """Check that Parquet format is correctly read from a `File` table engine."""
-
-    if node is None:
-        node = self.context.node
+    """Check that when a table with File(Parquet) engine is attached on top of a parquet file, it reads the data correctly."""
 
     table_name = "table_" + getuid()
 
-    with Given("I attach a table on top of a Parquet file"):
+    with Given("I attach a table with a File(Parquet) engine on top of a Parquet file"):
         table(name=table_name, engine='File(Parquet)', create="ATTACH")
 
-    with Then("I check that the data from the table is read correctly"):
+    with Then("I check that the table reads the data correctly"):
         check_query_output(query=f"SELECT * FROM {table_name}")
 
 @TestScenario
 @Requirements(RQ_SRS_032_ClickHouse_Parquet_TableEngines_Special_File("1.0"))
 def engine_to_file_to_engine(self):
-    """Check that Parquet format is correctly read from a `File` table engine."""
+    """Check that when data is inserted into a table with File(Parquet) engine, 
+    the data can be read back correctly from the source file using a different table with File(Parquet) engine."""
 
-    if node is None:
-        node = self.context.node
+    node = self.context.node
 
     table0_name = "table0_" + getuid()
     table1_name = "table1_" + getuid()
@@ -52,60 +47,73 @@ def engine_to_file_to_engine(self):
     with Given("I have a table"):
         table(name=table0_name, engine='File(Parquet)')
 
-    with When("I insert data into the table", desciption="Inserted data includes all of the ClickHouse data types supported by Parquet, including nested types and nulls"):
+    with When("I insert data into the table",
+        description="insert data includes all of the ClickHouse data types supported by Parquet, including nested types and nulls"):
         insert_test_data(name=table0_name)
 
     with Then("I check that the data inserted into the table was correctly written to the file"):
         check_source_file(path=f'/var/lib/clickhouse/data/default/{table0_name}/data.Parquet')
 
-    with When("I copy of the source file to a new directory"):
+    with When("I copy of the Paruqet source file to a new directory"):
         node.command(f"cp /var/lib/clickhouse/data/default/{table0_name}/data.Parquet /var/lib/clickhouse/data/default/{table1_name}/data.Parquet")
 
-    with And("I attach a new table on top of the file created by the previous table"):
+    with And("I attach a new table on top of the Parquet source file created by the previous table"):
         table(name=table1_name, engine='File(Parquet)', create="ATTACH")
 
-    with Then("I check that the new file has correct data"):
+    with Then("I check that the new table is able to read the data from the file correctly"):
         check_query_output(query=f"SELECT * FROM {table1_name}")
 
 @TestSuite
 def engine(self):
-    """Check that File table engine correctly reads and writes in Parquet format."""
+    """Check that File table engine correctly reads and writes Parquet format.
+    """
 
     Scenario(run=insert_into_engine)
     Scenario(run=select_from_engine)
     Scenario(run=engine_to_file_to_engine)
 
-@TestSuite
-@Requirements()
-def function(self, node=None):
-    """Check that ClickHouse reads Parquet format correctly using the File table function."""
-
-    if node is None:
-        node = self.context.node
+@TestScenario
+@Requirements(RQ_SRS_032_ClickHouse_Parquet_TableFunctions_File("1.0"))
+def insert_into_function(self):
+    """Check that when data is inserted into a table with file(Parquet) table function, it is written into the source file correctly.
+    """
+    node = self.context.node
 
     table_name = "table_" + getuid()
     file_name = "file_" + getuid()
 
-    with Scenario("Write Parquet into File function"):
+    with When("I insert test data into file function in Parquet format"):
+        insert_test_data(name=f"FUNCTION file('{file_name}.Parquet', 'Parquet', {all_test_data_types}")
 
-        with Given("I have a table"):
-            table(name=table_name, engine='File(Parquet)')
+    with Then("I check the file specified by the file function has correct data"):
+        check_source_file(path=f'/var/lib/clickhouse/user_files/{file_name}.Parquet')
 
-        with When("I insert some data"):
-            insert_test_data(name=f"FUNCTION file('{file_name}.Parquet', 'Parquet', {all_test_data_types}")
+@TestScenario
+@Requirements(RQ_SRS_032_ClickHouse_Parquet_TableFunctions_File("1.0"))
+def select_from_function_manual(self):
+    """Check that when data is inserted into a table with file(Parquet) table function, it is written into the source file correctly.
+    """
 
-        with Then("I check the file in the table function"):
-            check_source_file(path=f'/var/lib/clickhouse/user_files/{file_name}.Parquet')
+    node = self.context.node
 
-    with Scenario("Read Parquet from table function, default type cast"):
+    table_name = "table_" + getuid()
+    file_name = "file_" + getuid()
 
-        with Then("I check the data from the table function"):
-            check_query_output(query=f"SELECT *, toTypeName(*) FROM file('{file_name}.Parquet', 'Parquet')")
+    with Given("I have a table"):
+        table(name=table_name, engine='File(Parquet)')
 
-    with Scenario("Read Parquet from table function, manual type cast"):
+    with When("I insert some data"):
+        insert_test_data(name=f"FUNCTION file('{file_name}.Parquet', 'Parquet', {all_test_data_types}")
 
-        with Then("I check the data from the table function"):
-            check_query_output(query=f"SELECT * FROM file('{file_name}.Parquet', 'Parquet', {all_test_data_types})")
+    with Then("I check the file in the table function"):
+        check_source_file(path=f'/var/lib/clickhouse/user_files/{file_name}.Parquet')
+
+@TestSuite
+def function(self, node=None):
+    """Check that File table function correctly reads and writes Parquet format.
+    """
+    Scenario(run=insert_into_function)
+    Scenario(run=select_from_function_manual)
 
 @TestFeature
 @Name("file")
