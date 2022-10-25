fdcdd71423fb8184db3cead623af4fd93d83d12e
MyroTk <myroslav6@gmail.com>
2022-10-25 10:29:53 +0200

    parquet

--- parquet/test_files/test_file_generators.py
+++ parquet/test_files/test_file_generators.py
@@ -1,9 +1,144 @@
 import pyarrow as pa
 import pyarrow.parquet as pq
 
+pyarrow_types = {
+    "bool": pa.bool_(),
+    "uint8": pa.uint8(),
+    "int8": pa.int8(),
+    "uint16": pa.uint16(),
+    "int16": pa.int16(),
+    "uint32": pa.uint32(),
+    "int32": pa.int32(),
+    "uint64": pa.uint64(),
+    "int64": pa.int64(),
+    "float": pa.float32(),
+    "double": pa.float64(),
+    "date64": pa.date64(),
+    "date32": pa.date32(),
+    "string": pa.string(),
+    "binary": pa.binary(),
+}
+
+data_types_and_values = {
+    "bool": [True, False, True],
+    "uint8": [0, 255, 128],
+    "int8": [-128, 127, 101],
+    "uint16": [0, 65535, 32767],
+    "int16": [-32768, 32767, 10101],
+    "uint32": [0, 4294967295, 101010101],
+    "int32": [-2147483648, 2147483647, 10101010],
+    "uint64": [0, 18446744073709551615, 1010101010101010],
+    "int64": [-9223372036854775808, 9223372036854775807, 1010101010101010],
+    "float": [-3.4028235e38, 3.4028235e38, 10101010],
+    "double": [-1.7976909999999999e308, 1.7976909999999999e308, 5.6e266],
+    "date32": [0, 65535, 10],
+    "date64": [0, 1000000, 10],
+    "string": [
+        "''",
+        "'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTUVWXYZ!@#$%^&*()_-=+[]\{\}\\|\/?<>,.:;~`'",
+        "'hello'",
+    ],
+    "binary": ["1", "0", "111100111111"],
+}
+
 
 def chunked_array_file():
     arr = pa.array([[("a" * 2**30, 1)]], type=pa.map_(pa.string(), pa.int32()))
     arr = pa.chunked_array([arr, arr])
     tab = pa.table({"arr": arr})
     pq.write_table(tab, "chunked_array_test_file.parquet")
+
+
+def generate_parquet_test_files():
+    """Generate Parquet files used in ClickHouse Parquet format tests.
+    Does NOT include TimeStamp and Decimal Parquet types.
+    """
+
+    value_dict = {}
+    schema = []
+
+    for type, values in data_types_and_values.items():
+        value_dict[type] = values
+        schema.append((type, pyarrow_types[type]))
+
+        value_dict["list_" + type] = [[value] for value in values]
+        schema.append(("list_" + type, pa.list_(pyarrow_types[type])))
+
+        value_dict["list_struct_" + type] = [[(value,)] for value in values]
+        schema.append(
+            ("list_struct_" + type, pa.list_(pa.struct([("a", pyarrow_types[type])])))
+        )
+
+        if type not in ["float", "double"]:
+            for inner_key, inner_values in data_types_and_values.items():
+                value_dict["map_" + type + "_" + inner_key] = [
+                    [(value, inner_value) for inner_value in inner_values]
+                    for value in values
+                ]
+                schema.append(
+                    (
+                        "map_" + type + "_" + inner_key,
+                        pa.map_(pyarrow_types[type], pyarrow_types[inner_key]),
+                    )
+                )
+
+                value_dict["list_map_" + type + "_" + inner_key] = [
+                    [[(value, inner_value)] for inner_value in inner_values]
+                    for value in values
+                ]
+                schema.append(
+                    (
+                        "list_map_" + type + "_" + inner_key,
+                        pa.list_(
+                            pa.map_(pyarrow_types[type], pyarrow_types[inner_key])
+                        ),
+                    )
+                )
+
+                value_dict["map_" + type + "_list_" + inner_key] = [
+                    [(value, [inner_value]) for inner_value in inner_values]
+                    for value in values
+                ]
+                schema.append(
+                    (
+                        "map_" + type + "_list_" + inner_key,
+                        pa.map_(
+                            pyarrow_types[type], pa.list_(pyarrow_types[inner_key])
+                        ),
+                    )
+                )
+
+                value_dict["map_" + type + "_struct_" + inner_key] = [
+                    [(value, (inner_value,)) for inner_value in inner_values]
+                    for value in values
+                ]
+                schema.append(
+                    (
+                        "map_" + type + "_struct_" + inner_key,
+                        pa.map_(
+                            pyarrow_types[type],
+                            pa.struct([("a", pyarrow_types[inner_key])]),
+                        ),
+                    )
+                )
+
+    value_dict["struct"] = [
+        tuple([value[i] for value in data_types_and_values.values()]) for i in range(3)
+    ]
+    schema.append(
+        (
+            "struct",
+            pa.struct([(f"{value}", value) for value in data_types_and_values.keys()]),
+        )
+    )
+
+    table = pa.table(value_dict, schema=pa.schema(schema))
+
+    for compression in ["NONE", "SNAPPY", "GZIP", "BROTLI", "LZ4", "ZSTD"]:
+        pq.write_table(
+            table,
+            f"C:\\Users\\myros\\Altinity\\data_{compression}.Parquet",
+            compression=compression,
+        )
+
+    return
--- parquet/tests/s3.py
+++ parquet/tests/s3.py
@@ -5,102 +5,329 @@ from parquet.tests.common import *
 from s3.tests.common import *
 
 
-@TestFeature
-@Requirements()
+@TestScenario
+def insert_into_engine(self):
+    """Check that when data is inserted into a table with `S3` engine, it is written into the source file correctly."""
+
+    compression_type = self.context.compression_type
+    table_name = "table_" + getuid()
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with And("I have a table with S3 engine"):
+        table(
+            name=table_name,
+            engine=f"S3('{self.context.uri}{table_name}.Parquet', '{self.context.access_key_id}', '{self.context.secret_access_key}', 'Parquet', '{compression_type.lower()}')",
+        )
+
+    with When(
+        "I insert data into the table",
+        description="insert data includes all of the ClickHouse data types supported by Parquet, including nested types and nulls",
+    ):
+        insert_test_data(name=table_name)
+
+    with Then(
+        "I check that the data inserted into the table was correctly written to the file"
+    ):
+        check_source_file_on_s3(
+            file=table_name + ".Parquet", compression_type=compression_type
+        )
+
+
+@TestScenario
+def select_from_engine(self):
+    """Check that when a table with `S3` engine is attached on top of a Parquet file, it reads the data correctly."""
+
+    xfail("TODO: create Parquet files")
+
+    compression_type = self.context.compression_type
+    table_name = "table_" + getuid()
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with And("I upload parquet data to s3"):
+        upload_file_to_s3(
+            file_src="/var/lib/clickhouse/user_files/data.Parquet",
+            file_dest=f"/{table_name}/data.Parquet",
+        )
+
+    with When("I attach a table with a `S3` engine on top of a Parquet file"):
+        table(
+            name=table_name,
+            engine=f"S3('{self.context.uri}{table_name}.Parquet', '{self.context.access_key_id}', '{self.context.secret_access_key}', 'Parquet', '{compression_type.lower()}')",
+            create="ATTACH",
+        )
+
+    with Then("I check that the table reads the data correctly"):
+        check_query_output(query=f"SELECT * FROM {table_name}")
+
+
+@TestScenario
+def engine_to_file_to_engine(self):
+    """Check that when data is inserted into a table with `S3` engine,
+    the data can be read back correctly from the source file using a different table with `S3` engine."""
+
+    compression_type = self.context.compression_type
+    node = self.context.node
+
+    table0_name = "table0_" + getuid()
+    table1_name = "table1_" + getuid()
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with And("I have a table with S3 engine"):
+        table(
+            name=table0_name,
+            engine=f"S3('{self.context.uri}{table0_name}.Parquet', '{self.context.access_key_id}', '{self.context.secret_access_key}', 'Parquet', '{compression_type.lower()}')",
+        )
+
+    with When(
+        "I insert data into the table",
+        description="insert data includes all of the ClickHouse data types supported by Parquet, including nested types and nulls",
+    ):
+        insert_test_data(name=table0_name)
+
+    with Then(
+        "I check that the data inserted into the table was correctly written into the file"
+    ):
+        check_source_file_on_s3(file=table0_name)
+
+    with When("I attach a table with a `S3` engine on top of a Parquet file"):
+        table(
+            name=table1_name,
+            engine=f"S3('{self.context.uri}{table1_name}.Parquet', '{self.context.access_key_id}', '{self.context.secret_access_key}', 'Parquet', '{compression_type.lower()}')",
+            create="CREATE",
+        )
+
+    with Then(
+        "I check that the new table is able to read the data from the file correctly"
+    ):
+        check_query_output(query=f"SELECT * FROM {table1_name}")
+
+
+@TestScenario
+def insert_into_engine_from_file(self):
+    """Check that that data read from a Parquet file using the `INFILE` clause in `INSERT` query is
+    correctly written into a table with a `S3` engine.
+    """
+
+    xfail("TODO: create parquet test files.")
+
+    compression_type = self.context.compression_type
+    node = self.context.node
+    table_name = "table_" + getuid()
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with And("I have a table with S3 engine"):
+        table(
+            name=table0_name,
+            engine=f"S3('{self.context.uri}{table_name}.Parquet', '{self.context.access_key_id}', '{self.context.secret_access_key}', 'Parquet', '{compression_type.lower()}')",
+        )
+
+    with When("I insert data into the table from a Parquet file"):
+        node.query(
+            f"INSERT INTO {table_name} FROM INFILE '/var/lib/clickhouse/user_files/data.Parquet' COMPRESSION '{compression_type}' FORMAT Parquet"
+        )
+
+    with Then("I check that the table contains correct data"):
+        check_query_output(query=f"SELECT * FROM {table_name}")
+
+
+@TestScenario
+def engine_select_output_to_file(self):
+    """Check that data is correctly written into a Parquet file when using `SELECT` query with `OUTFILE` clause on a table with `S3` engine."""
+
+    compression_type = self.context.compression_type
+    node = self.context.node
+    client = self.context.client()
+    table_name = "table_" + getuid()
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with And("I have a table with S3 engine"):
+        table(
+            name=table_name,
+            engine=f"S3('{self.context.uri}{table_name}.Parquet', '{self.context.access_key_id}', '{self.context.secret_access_key}', 'Parquet', '{compression_type.lower()}')",
+        )
+
+    with When(
+        "I insert data into the table",
+        description="insert data includes all of the ClickHouse data types supported by Parquet, including nested types and nulls",
+    ):
+        insert_test_data(name=table_name)
+
+    with When("I select data from the table and write it into a Parquet file"):
+        node.query(
+            f"SELECT * FROM {table_name} INTO OUTFILE '/var/lib/clickhouse/user_files/{table_name}.Parquet' COMPRESSION '{compression_type}' FORMAT Parquet"
+        )
+
+    with Then(
+        "I check that the data inserted into the table was correctly written to the file"
+    ):
+        check_source_file_on_s3(client=client, file=table_name)
+
+
+@TestScenario
+def insert_into_function_manual_cast_types(self):
+    """Check that when data is inserted into `s3` table function with manually defined structure,
+    it is written into the source file correctly.
+    """
+
+    compression_type = self.context.compression_type
+    file_name = "file_" + getuid()
+    client = self.context.client()
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with When("I insert test data into s3 table function in Parquet format"):
+        insert_test_data(
+            name=f"FUNCTION s3('{self.context.uri}{file_name}', '{self.context.secret_access_key}', '{self.context.access_key_id}', 'Parquet', '{','.join(generate_all_column_types())}', '{compression_type}')"
+        )
+
+    with Then(
+        "I check that the data inserted into the table was correctly written to the file"
+    ):
+        check_source_file_on_s3(client=client, file=f"{file_name}.Parquet")
+
+
+@TestScenario
+def insert_into_function_auto_cast_types(self):
+    """Check that when data is inserted into `s3` table function with automatically defined structure,
+    it is written into the source file correctly.
+    """
+
+    compression_type = self.context.compression_type
+    file_name = "file_" + getuid()
+    client = self.context.client()
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with When("I insert test data into s3 table function in Parquet format"):
+        insert_test_data(
+            name=f"FUNCTION s3('{self.context.uri}{file_name}', '{self.context.secret_access_key}', '{self.context.access_key_id}', 'Parquet', '{compression_type}')"
+        )
+
+    with Then(
+        "I check that the data inserted into the table was correctly written to the file"
+    ):
+        check_source_file_on_s3(file=f"{file_name}.Parquet")
+
+
+@TestScenario
+def select_from_function_manual_cast_types(self):
+    """Check that when data is selected from an `s3` table function with manually cast column types,
+    it is read correctly.
+    """
+
+    compression_type = self.context.compression_type
+
+    xfail("TODO: add parquet files")
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with And("I upload parquet data to s3"):
+        upload_file_to_s3(
+            file_src="/var/lib/clickhouse/user_files/data.Parquet",
+            file_dest=f"/{table_name}/data.Parquet",
+            client=client,
+        )
+
+    with Then("I check that the `file` table function contains correct data"):
+        check_query_output(
+            query=f"SELECT * FROM s3('{self.context.uri}{table_name}/data.Parquet', '{self.context.secret_access_key}', '{self.context.access_key_id}', 'Parquet', '{','.join(generate_all_column_types())}', '{compression_type}')"
+        )
+
+
+@TestScenario
+def select_from_function_auto_cast_types(self):
+    """Check that when data is selected from an `s3` table function with automatic cast column types,
+    it is read correctly.
+    """
+
+    compression_type = self.context.compression_type
+
+    xfail("TODO: add parquet files")
+
+    with Given("I have a disk configuration with a S3 storage disk, access id and key"):
+        default_s3_disk_and_volume()
+
+    with And("I upload parquet data to s3"):
+        upload_file_to_s3(
+            file_src="/var/lib/clickhouse/user_files/data.Parquet",
+            file_dest=f"/{table_name}/data.Parquet",
+            client=client,
+        )
+
+    with Then("I check that the `file` table function contains correct data"):
+        check_query_output(
+            query=f"SELECT * FROM s3('{self.context.uri}{table_name}/data.Parquet', '{self.context.secret_access_key}', '{self.context.access_key_id}', 'Parquet', '{compression_type}')"
+        )
+
+
+@TestSuite
+@Requirements(RQ_SRS_032_ClickHouse_Parquet_TableEngines_Integration_S3("1.0"))
+def engine(self):
+    """Check that table with `S3` engine correctly reads and writes Parquet format."""
+
+    Scenario(run=insert_into_engine)
+    Scenario(run=select_from_engine)
+    Scenario(run=engine_to_file_to_engine)
+    Scenario(run=insert_into_engine_from_file)
+    Scenario(run=engine_select_output_to_file)
+
+
+@TestSuite
+@Requirements(RQ_SRS_032_ClickHouse_Parquet_TableFunctions_S3("1.0"))
+def function(self):
+    """Check that `s3` table function correctly reads and writes Parquet format."""
+    Scenario(run=insert_into_function_manual_cast_types)
+    Scenario(run=insert_into_function_auto_cast_types)
+    Scenario(run=select_from_function_manual_cast_types)
+    Scenario(run=select_from_function_auto_cast_types)
+
+
+@TestOutline(Feature)
+@Examples(
+    "compression_type",
+    [
+        (
+            "NONE",
+            Requirements(RQ_SRS_032_ClickHouse_Parquet_Insert_Compression_None("1.0")),
+        ),
+        (
+            "GZIP",
+            Requirements(RQ_SRS_032_ClickHouse_Parquet_Insert_Compression_Gzip("1.0")),
+        ),
+        (
+            "BROTLI",
+            Requirements(
+                RQ_SRS_032_ClickHouse_Parquet_Insert_Compression_Brotli("1.0")
+            ),
+        ),
+        (
+            "LZ4",
+            Requirements(
+                RQ_SRS_032_ClickHouse_Parquet_Insert_Compression_Lz4("1.0"),
+                RQ_SRS_032_ClickHouse_Parquet_Insert_Compression_Lz4Raw("1.0"),
+            ),
+        ),
+    ],
+)
 @Name("s3")
-def feature(self, node="clickhouse1"):
+def feature(self, compression_type):
     """Run checks for clickhouse using Parquet format using s3 storage."""
 
-    xfail("Not implemented.")
+    self.context.compression_type = compression_type
+    self.context.node = self.context.cluster.node("clickhouse1")
 
-    node = self.context.cluster.node(node)
-
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=self.context.access_key_id,
-        aws_secret_access_key=self.context.secret_access_key,
-    )
-    with TestScenario("Write to s3 table engine"):
-
-        table_name = "table_" + getuid()
-
-        with Given(
-            "I have a disk configuration with a S3 storage disk, access id and key"
-        ):
-            default_s3_disk_and_volume()
-
-        with Given("I have a table."):
-            table(
-                name=table_name,
-                engine=f"s3({self.context.uri}{table_name}, {self.context.access_key}, {self.context.key_id})",
-            )
-
-        with When("I insert some data."):
-            insert_test_data(name=table_name)
-
-        with Then("I check the file."):
-            check_aws_s3_file(s3_client=s3_client, file=table_name)
-
-    with TestScenario("Read from s3 table engine"):
-
-        table_name = "s3_table"
-
-        with Given(
-            "I have a disk configuration with a S3 storage disk, access id and key"
-        ):
-            default_s3_disk_and_volume()
-
-        with And("There is parquet data on s3"):
-            upload_parquet_to_aws_s3(s3_client=s3_client)
-
-        with When("I attach table"):
-            table(
-                name=table_name,
-                create="ATTACH",
-                engine=f"s3({self.context.uri}s3_table/data.Parquet, {self.context.access_key}, {self.context.key_id})",
-            )
-
-        with Then("I check the table"):
-            check_query(query=f"SELECT * FROM {table_name}")
-
-    with TestScenario("Write to s3 table function"):
-
-        table_name = "table_" + getuid()
-
-        with Given(
-            "I have a disk configuration with a S3 storage disk, access id and key"
-        ):
-            default_s3_disk_and_volume()
-
-        with When("I insert some data into the s3 function."):
-            node.query(
-                f"""
-                INSERT INTO FUNCTION s3({self.context.uri}{table_name}, {self.context.access_key}, {self.context.key_id}, Parquet)
-                SELECT (1,2,3,4,5,6,7,8,9,10,11,'2022-01-01','2022-01-01 00:00:00','A','B',[1,2,3],(1,2,3), {{'a':1, 'b',2}})
-                """
-            )
-
-        with Then("I check the file."):
-            check_aws_s3_file(s3_client=s3_client, file=table_name)
-
-    with TestScenario("Read from s3 table function"):
-
-        table_name = "s3_table"
-
-        with Given(
-            "I have a disk configuration with a S3 storage disk, access id and key"
-        ):
-            default_s3_disk_and_volume()
-
-        with And("There is parquet data on s3"):
-            upload_parquet_to_aws_s3(s3_client=s3_client)
-
-        with When("I attach table"):
-            table(
-                name=table_name,
-                create="ATTACH",
-                engine=f"s3({self.context.uri}s3_table/data.Parquet, {self.context.access_key}, {self.context.key_id})",
-            )
-
-        with Then("I check the table"):
-            check_query(query=f"SELECT * FROM {table_name}")
+    Suite(run=engine)
+    Suite(run=function)
