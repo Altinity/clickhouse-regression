99415b3955d96f5c501cc883d838d488b11d57f0
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-11-10 15:18:54 -0500

    Adding tests for stddevPopStable, stddevSampStable, varPopStable, varSampStable

---
+++ aggregate_functions/tests/stddevPopStable.py
@@ -0,0 +1,20 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Standard_StddevPop,
+)
+
+from aggregate_functions.tests.stddevPop import feature as stddevpop_feature
+
+
+@TestFeature
+@Name("stddevPopStable")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Standard_StddevPop("1.0"))
+def feature(self, func="stddevPopStable({params})", table=None):
+    """Check stddevPopStable aggregate function by using the same checks as for stddevPop."""
+    self.context.snapshot_id = name.basename(current().name)
+
+    if table is None:
+        table = self.context.table
+
+    stddevpop_feature(func=func, table=table)
---
+++ aggregate_functions/tests/stddevSampStable.py
@@ -0,0 +1,20 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Standard_StddevSamp,
+)
+
+from aggregate_functions.tests.stddevSamp import feature as stddevsamp_feature
+
+
+@TestFeature
+@Name("stddevSampStable")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Standard_StddevSamp("1.0"))
+def feature(self, func="stddevSampStable({params})", table=None):
+    """Check stddevSampStable aggregate function by using the same checks as for stddevSamp."""
+    self.context.snapshot_id = name.basename(current().name)
+
+    if table is None:
+        table = self.context.table
+
+    stddevsamp_feature(func=func, table=table)
