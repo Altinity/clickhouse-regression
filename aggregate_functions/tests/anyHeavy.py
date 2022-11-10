ef28ea1c852363ad937f06bd2290efa3f6f65766
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-11-10 12:19:24 -0500

    Adding tests and snapshots for anyLast and anyHeavy functions.

---
+++ aggregate_functions/tests/anyHeavy.py
@@ -0,0 +1,20 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_AnyHeavy,
+)
+
+from aggregate_functions.tests.any import feature as any_feature
+
+
+@TestFeature
+@Name("anyHeavy")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_AnyHeavy("1.0"))
+def feature(self, func="anyHeavy({params})", table=None):
+    """Check anyHeavy aggregate function by using the same tests as for any."""
+    self.context.snapshot_id = name.basename(current().name)
+
+    if table is None:
+        table = self.context.table
+
+    any_feature(func=func, table=table)
