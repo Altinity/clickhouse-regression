c81a3ca90621d75cbf88b0562ffb8d431c2bea97
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-11-10 15:09:25 -0500

    Adding tests for stddevPop, stddevSamp, varPop, varSamp functions.

---
+++ aggregate_functions/tests/stddevSamp.py
@@ -0,0 +1,20 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Standard_StddevSamp,
+)
+
+from aggregate_functions.tests.avg import feature as avg_feature
+
+
+@TestFeature
+@Name("stddevSamp")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Standard_StddevSamp("1.0"))
+def feature(self, func="stddevSamp({params})", table=None):
+    """Check stddevSamp aggregate function by using the same checks as for avg."""
+    self.context.snapshot_id = name.basename(current().name)
+
+    if table is None:
+        table = self.context.table
+
+    avg_feature(func=func, table=table, exclude_types=["Decimal128(38)"])
---
+++ aggregate_functions/tests/varPop.py
@@ -0,0 +1,20 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Standard_VarPop,
+)
+
+from aggregate_functions.tests.avg import feature as avg_feature
+
+
+@TestFeature
+@Name("varPop")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Standard_VarPop("1.0"))
+def feature(self, func="varPop({params})", table=None):
+    """Check varPop aggregate function by using the same checks as for avg."""
+    self.context.snapshot_id = name.basename(current().name)
+
+    if table is None:
+        table = self.context.table
+
+    avg_feature(func=func, table=table, exclude_types=["Decimal128(38)"])
