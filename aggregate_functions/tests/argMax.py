6e20331bd3a76e88f4f586caa90bd98e02e7ac6e
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-11-11 08:13:55 -0500

    Updating requirements to include all aggregate functions as of 22.7.1

--- aggregate_functions/tests/argMax.py
+++ aggregate_functions/tests/argMax.py
@@ -1,7 +1,7 @@
 from testflows.core import *
 
 from aggregate_functions.requirements import (
-    RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_Argmax,
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_ArgMax,
 )
 
 from aggregate_functions.tests.argMin import feature as argmin_feature
@@ -9,7 +9,7 @@ from aggregate_functions.tests.argMin import feature as argmin_feature
 
 @TestFeature
 @Name("argMax")
-@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_Argmax("1.0"))
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_ArgMax("1.0"))
 def feature(self, func="argMax({params})", table=None):
     """Check argMax aggregate function by using the same tests as for argMin."""
     self.context.snapshot_id = name.basename(current().name)
