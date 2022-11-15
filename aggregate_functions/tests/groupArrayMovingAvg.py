32d232740ed92f36a9aec0d17738b5f2ead3eec1
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-11-15 11:55:33 -0500

    Adding groupArrayMovingAvg and groupArrayMovingSum.

---
+++ aggregate_functions/tests/groupArrayMovingAvg.py
@@ -0,0 +1,20 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_GroupArrayMovingAvg,
+)
+
+from aggregate_functions.tests.avg import feature as checks
+
+
+@TestFeature
+@Name("groupArrayMovingAvg")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_GroupArrayMovingAvg("1.0"))
+def feature(self, func="groupArrayMovingAvg({params})", table=None):
+    """Check groupArrayMovingAvg aggregate function by using the same tests as for avg."""
+    self.context.snapshot_id = name.basename(current().name)
+
+    if table is None:
+        table = self.context.table
+
+    checks(func=func, table=table)
