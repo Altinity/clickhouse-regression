5f60bdbdb5d88431276477f14d9e79e8b8c7d76f
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-11-15 11:03:58 -0500

    Adding topK and topKWeighted.

--- aggregate_functions/tests/exponentialMovingAverage.py
+++ aggregate_functions/tests/exponentialMovingAverage.py
@@ -10,11 +10,12 @@ from aggregate_functions.tests.covarPop import feature as checks
 @TestFeature
 @Name("exponentialMovingAverage")
 @Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_ExponentialMovingAverage("1.0"))
-def feature(self, func="exponentialMovingAverage(0.5)({params})", table=None):
+def feature(self, func="exponentialMovingAverage({params})", table=None):
     """Check exponentialMovingAverage aggregate function by using the same checks as for covarPop."""
     self.context.snapshot_id = name.basename(current().name)
 
     if table is None:
         table = self.context.table
 
+    func = func.replace("({params})", "(0.5)({params})")
     checks(func=func, table=table, decimal=True)
