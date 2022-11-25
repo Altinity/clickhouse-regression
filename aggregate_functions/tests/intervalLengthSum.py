0155866a837fa348c4d028137fa80390d0f6d59c
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-11-25 00:07:29 -0500

    Updating snapshots for <22.4

--- aggregate_functions/tests/intervalLengthSum.py
+++ aggregate_functions/tests/intervalLengthSum.py
@@ -1,5 +1,7 @@
 from testflows.core import *
 
+from helpers.common import check_clickhouse_version
+
 from aggregate_functions.requirements import (
     RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_IntervalLengthSum,
 )
@@ -14,9 +16,17 @@ from aggregate_functions.tests.deltaSumTimestamp import feature as checks
 )
 def feature(self, func="intervalLengthSum({params})", table=None):
     """Check intervalLengthSum aggregate function by using the same tests as for deltaSumTimestamp."""
-    self.context.snapshot_id = name.basename(current().name)
+    if check_clickhouse_version("<22.4")(self):
+        self.context.snapshot_id = name.basename(current().name) + "<22.4"
+    else:
+        self.context.snapshot_id = name.basename(current().name)
 
     if table is None:
         table = self.context.table
 
-    checks(func=func, table=table, both_arguments_with_the_same_datatype=True)
+    checks(
+        func=func,
+        table=table,
+        both_arguments_with_the_same_datatype=True,
+        snapshot_id=self.context.snapshot_id,
+    )
