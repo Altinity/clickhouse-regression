08bf0084ccb970ac00a357ea91a8aa744d9c3bab
AlmostIvanSidorov <vanosidorov@gmail.com>
2022-12-03 16:59:41 +0200

    base tables upd.

--- selects/tests/steps.py
+++ selects/tests/steps.py
@@ -10,67 +10,34 @@ from helpers.common import getuid, instrument_clickhouse_server_log
 class Table:
     def __init__(self, name, engine, final_modifier_available):
         self.name = name
-        self.schema = schema
         self.engine = engine
         self.final_modifier_available = final_modifier_available
 
-    def insert_test_data(self, node=None):
-        """Insert data into the specified table."""
 
-        if node is None:
-            node = current().context.node
-
-        name = self.name
-
-
-        elif self.engine.startswith("Collapsing"):
-            return (
-                [
-                    node.query(
-                        f"INSERT INTO {name} VALUES (4324182021466249494, {i}, 146, 1)"
-                    )
-                    for i in range(50)
-                ],
-                [
-                    node.query(
-                        f"INSERT INTO {name} VALUES (4324182021466249494, {i}, 146, -1),"
-                        f"(4324182021466249494, {i}, 185, 1)"
-                    )
-                    for i in range(50)
-                ],
-            )
-        elif self.engine.startswith("Aggregating"):
-            return (
-                [
-                    node.query(f"INSERT INTO {name} VALUES ('a', {i}, 1);")
-                    for i in range(50)
-                ],
-                [
-                    node.query(f"INSERT INTO {name}  VALUES ('a', {i + 1}, 2);")
-                    for i in range(50)
-                ],
-            )
-
-        
 @TestStep(Given)
 def create_and_populate_tables(self):
-    """Create and popualate all test tables for different table engines.
-    """
+    """Create and populate all test tables for different table engines."""
+    version = "eventTime"
+    sign = "Sign"
     engines = [
         "ReplacingMergeTree",
-        "ReplacingMergeTree({version})",
-        "CollapsingMergeTree({sign})",
-        "AggregatingMergeTree"
+        f"ReplacingMergeTree({version})",
+        f"CollapsingMergeTree({sign})",
+        "AggregatingMergeTree",
+        "SummingMergeTree",
+        f"VersionedCollapsingMergeTree({sign},version)",
+        # "MergeTree",
+        # "StripeLog",
+        # "TinyLog",
+        # "Log",
     ]
     for engine in engines:
         with Given(f"{engine} table"):
-            self.context.tables.append(
-                create_and_populate_table(engine=engine)
-            )
+            self.context.tables.append(create_and_populate_table(engine=engine))
 
-            
+
 @TestStep(Given)
-def create_and_populate_table(self, engine, schema, name=None):
+def create_and_populate_table(self, engine, name=None):
     """
     Create clickhouse table.
 
@@ -80,67 +47,78 @@ def create_and_populate_table(self, engine, schema, name=None):
     :param final_modifier_available: true if `FINAL` modifier available for engine
     """
     if name is None:
-        # generate proper table name based on engine
-        name = f"table_{getuid()}"
+        name = f"{engine.replace('(','_').replace(',','_').replace(')','')}_table_{getuid()}"
 
     if engine.startswith("Replacing"):
         return create_and_populate_replacing_table(
             name=name,
-            schema=schema,
-            final_modifier_available=final_modifier_available,
             engine=engine,
         )
     elif engine.startswith("Collapsing"):
         return create_and_populate_collapsing_table(
             name=name,
-            schema=schema,
-            final_modifier_available=final_modifier_available,
             engine=engine,
         )
 
     elif engine.startswith("Aggregating"):
         return create_and_populate_aggregating_table(
             name=name,
-            schema=schema,
-            final_modifier_available=final_modifier_available,
             engine=engine,
         )
 
     elif engine.startswith("Summing"):
-        pass
+        return create_and_populate_summing_table(
+            name=name,
+            engine=engine,
+        )
+
+    elif engine.startswith("Merge"):
+        return create_and_populate_merge_table(
+            name=name,
+            engine=engine,
+        )
+    elif engine.startswith("Versioned"):
+        return create_and_populate_versioned_table(name=name, engine=engine)
+    elif engine.startswith("StripeLog"):
+        return create_and_populate_stripelog_table(name=name, engine=engine)
+    elif engine.startswith("TinyLog"):
+        return create_and_populate_tinylog_table(name=name, engine=engine)
+    elif engine.startswith("Log"):
+        return create_and_populate_log_table(name=name, engine=engine)
 
 
 @TestStep(Given)
 def create_and_populate_replacing_table(
-    self, name, final_modifier_available, engine="ReplacingMergeTree", node=None
+    self, name, final_modifier_available=True, engine="ReplacingMergeTree", node=None
 ):
     if node is None:
         node = current().context.node
     try:
-        schema = "(key Int64, someCol String, eventTime DateTime)",
-        
+        schema = "(key Int64, someCol String, eventTime DateTime)"
+
         with By(f"creating table {name}"):
             node.query(
                 f"CREATE TABLE {name} {schema} " f"ENGINE = {engine} ORDER BY key;"
             )
-        
+
         with And("populating table"):
             (
+                node.query("system stop merges"),
                 [
                     node.query(
                         f"INSERT INTO {name} VALUES ({i}, 'first', '2020-01-01 01:01:01')"
                     )
-                    for i in range(50)
+                    for i in range(5)
                 ],
                 [
                     node.query(
                         f"INSERT INTO {name} VALUES ({i}, 'second', '2020-01-01 00:00:00')"
                     )
-                    for i in range(50)
+                    for i in range(5)
                 ],
             )
-    
-        yield Table(name, schema, engine, final_modifier_available)
+
+        yield Table(name, engine, final_modifier_available)
 
     finally:
         with Finally(f"drop the table {name}"):
@@ -148,22 +126,40 @@ def create_and_populate_replacing_table(
 
 
 @TestStep
-def create_collapsing_table(
+def create_and_populate_collapsing_table(
     self,
     name,
-    schema,
-    final_modifier_available,
+    final_modifier_available=True,
     engine="CollapsingMergeTree",
     node=None,
 ):
     if node is None:
         node = current().context.node
     try:
+
+        schema = "( key UInt64, PageViews UInt8, Duration UInt8, Sign Int8)"
+
         with By(f"creating table {name}"):
-            node.query(
-                f"CREATE TABLE {name} {schema} " f"ENGINE = {engine} ORDER BY UserID;"
+            node.query(f"CREATE TABLE {name} {schema} ENGINE = {engine} ORDER BY key;")
+
+        with And("populating table"):
+            (
+                node.query("system stop merges"),
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES (4324182021466249494, {i}, 146, 1)"
+                    )
+                    for i in range(5)
+                ],
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES (4324182021466249494, {i}, 146, -1),"
+                        f"(4324182021466249494, {i}, 185, 1)"
+                    )
+                    for i in range(5)
+                ],
             )
-        yield Table(name, schema, engine, final_modifier_available)
+        yield Table(name, engine, final_modifier_available)
 
     finally:
         with Finally(f"drop the table {name}"):
@@ -171,23 +167,279 @@ def create_collapsing_table(
 
 
 @TestStep
-def create_aggregating_table(
+def create_and_populate_aggregating_table(
     self,
     name,
-    schema,
-    final_modifier_available,
+    final_modifier_available=True,
     engine="AggregatingMergeTree",
     node=None,
 ):
     if node is None:
         node = current().context.node
     try:
+
+        schema = "(key String, b UInt8, c SimpleAggregateFunction(max, UInt8))"
+
         with By(f"creating table {name}"):
-            node.query(
-                f"CREATE TABLE {name} {schema} " f"ENGINE = {engine} ORDER BY a;"
+            node.query(f"CREATE TABLE {name} {schema} ENGINE = {engine} ORDER BY key;")
+
+        with And("populating table"):
+            (
+                node.query("system stop merges"),
+                [
+                    node.query(f"INSERT INTO {name} VALUES ('a', {i}, 1);")
+                    for i in range(5)
+                ],
+                [
+                    node.query(f"INSERT INTO {name}  VALUES ('a', {i + 1}, 2);")
+                    for i in range(5)
+                ],
             )
-        yield Table(name, schema, engine, final_modifier_available)
+
+        yield Table(name, engine, final_modifier_available)
 
     finally:
+        with Finally(f"drop the table {name}"):
+            node.query(f"DROP TABLE IF EXISTS {name}")
+
+
+@TestStep
+def create_and_populate_summing_table(
+    self,
+    name,
+    final_modifier_available=True,
+    engine="SummingMergeTree",
+    node=None,
+):
+    if node is None:
+        node = current().context.node
+    try:
+        schema = "(key Int64, someCol String, eventTime DateTime)"
+
+        with By(f"creating table {name}"):
+            node.query(
+                f"CREATE TABLE {name} {schema} " f"ENGINE = {engine} ORDER BY key;"
+            )
+
+        with And("populating table"):
+            (
+                node.query("system stop merges"),
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'first', '2020-01-01 01:01:01')"
+                    )
+                    for i in range(5)
+                ],
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'second', '2020-01-01 00:00:00')"
+                    )
+                    for i in range(5)
+                ],
+            )
+
+        yield Table(name, engine, final_modifier_available)
+
+    finally:
+        with Finally(f"drop the table {name}"):
+            node.query(f"DROP TABLE IF EXISTS {name}")
+
+
+@TestStep
+def create_and_populate_merge_table(
+    self,
+    name,
+    final_modifier_available=False,
+    engine="MergeTree",
+    node=None,
+):
+    if node is None:
+        node = current().context.node
+    try:
+        schema = "(key Int64, someCol String, eventTime DateTime)"
+
+        with By(f"creating table {name}"):
+            node.query(
+                f"CREATE TABLE {name} {schema} " f"ENGINE = {engine} ORDER BY key;"
+            )
+
+        with And("populating table"):
+            (
+                node.query("system stop merges"),
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'first', '2020-01-01 01:01:01')"
+                    )
+                    for i in range(5)
+                ],
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'second', '2020-01-01 00:00:00')"
+                    )
+                    for i in range(5)
+                ],
+            )
+        yield Table(name, engine, final_modifier_available)
+
+    finally:
+        with Finally(f"drop the table {name}"):
+            node.query(f"DROP TABLE IF EXISTS {name}")
+
+
+@TestStep
+def create_and_populate_versioned_table(
+    self,
+    name,
+    final_modifier_available=True,
+    engine="VersionedCollapsingMergeTree(sign,version)",
+    node=None,
+):
+    if node is None:
+        node = current().context.node
+    try:
+        schema = "(key Int64, someCol String, Sign Int8, version UInt8)"
+
+        with By(f"creating table {name}"):
+            node.query(
+                f"CREATE TABLE {name} {schema} " f"ENGINE = {engine} ORDER BY key;"
+            )
+
+        with And("populating table"):
+            (
+                node.query("system stop merges"),
+                [
+                    node.query(f"INSERT INTO {name} VALUES ({i}, 'first', 1, 1)")
+                    for i in range(5)
+                ],
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'second', 1, 1),"
+                        f"({i+1}, 'third', -1, 2)"
+                    )
+                    for i in range(5)
+                ],
+            )
+        yield Table(name, engine, final_modifier_available)
+
+    finally:
+        with Finally(f"drop the table {name}"):
+            node.query(f"DROP TABLE IF EXISTS {name}")
+
+
+@TestStep
+def create_and_populate_stripelog_table(
+    self,
+    name,
+    final_modifier_available=False,
+    engine="StripeLog",
+    node=None,
+):
+    if node is None:
+        node = current().context.node
+    try:
+        schema = "(key Int64, someCol String, eventTime DateTime)"
+
+        with By(f"creating table {name}"):
+            node.query(f"CREATE TABLE {name} {schema} " f"ENGINE = {engine};")
+
+        with And("populating table"):
+            (
+                node.query("system stop merges"),
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'first', '2020-01-01 01:01:01')"
+                    )
+                    for i in range(5)
+                ],
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'second', '2020-01-01 00:00:00')"
+                    )
+                    for i in range(5)
+                ],
+            )
+        yield Table(name, engine, final_modifier_available)
+
+    finally:
+        with Finally(f"drop the table {name}"):
+            node.query(f"DROP TABLE IF EXISTS {name}")
+
+
+@TestStep
+def create_and_populate_tinylog_table(
+    self,
+    name,
+    final_modifier_available=False,
+    engine="TinyLog",
+    node=None,
+):
+    if node is None:
+        node = current().context.node
+    try:
+        schema = "(key Int64, someCol String, eventTime DateTime)"
+
+        with By(f"creating table {name}"):
+            node.query(f"CREATE TABLE {name} {schema} " f"ENGINE = {engine};")
+
+        with And("populating table"):
+            (
+                node.query("system stop merges"),
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'first', '2020-01-01 01:01:01')"
+                    )
+                    for i in range(5)
+                ],
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'second', '2020-01-01 00:00:00')"
+                    )
+                    for i in range(5)
+                ],
+            )
+
+        yield Table(name, engine, final_modifier_available)
+
+    finally:
+        with Finally(f"drop the table {name}"):
+            node.query(f"DROP TABLE IF EXISTS {name}")
+
+
+@TestStep
+def create_and_populate_log_table(
+    self,
+    name,
+    final_modifier_available=False,
+    engine="Log",
+    node=None,
+):
+    if node is None:
+        node = current().context.node
+    try:
+        schema = "(key Int64, someCol String, eventTime DateTime)"
+
+        with By(f"creating table {name}"):
+            node.query(f"CREATE TABLE {name} {schema} " f"ENGINE = {engine};")
+
+        with And("populating table"):
+            (
+                node.query("system stop merges"),
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'first', '2020-01-01 01:01:01')"
+                    )
+                    for i in range(5)
+                ],
+                [
+                    node.query(
+                        f"INSERT INTO {name} VALUES ({i}, 'second', '2020-01-01 00:00:00')"
+                    )
+                    for i in range(5)
+                ],
+            )
+        yield Table(name, engine, final_modifier_available)
+
+    finally:
+        pause()
         with Finally(f"drop the table {name}"):
             node.query(f"DROP TABLE IF EXISTS {name}")
