154cec12324fe76b5d94ebbaf7717e965133a2b7
AlmostIvanSidorov <vanosidorov@gmail.com>
2022-12-04 13:56:07 +0200

    core and system tables upd and basic structure fixes.

--- selects/tests/final/feature.py
+++ selects/tests/final/feature.py
@@ -1,48 +1,8 @@
 from testflows.core import *
 from selects.requirements import *
 from selects.tests.steps import *
-
-
-@TestScenario
-def select_count(self, node=None):
-    """Check select count() with `FINAL` clause equal to force_select_final select."""
-    if node is None:
-        node = current().context.node
-
-    for table in self.context.tables:
-        with Then("I check that select with force_select_final equal 'SELECT...FINAL'"):
-            assert (
-                node.query(
-                    f"SELECT count() FROM {table.name}"
-                    f"{' FINAL' if table.final_modifier_available else ''} "
-                    f" FORMAT JSONEachRow;"
-                ).output.strip()
-                == node.query(
-                    f"SELECT count() FROM {table.name}  FORMAT JSONEachRow;",
-                    settings=[("force_select_final", 1)],
-                ).output.strip()
-            )
-
-
-@TestScenario
-def select(self, node=None):
-    """Check select all data with `FINAL` clause equal to force_select_final select."""
-    if node is None:
-        node = current().context.node
-
-    for table in self.context.tables:
-        with Then("I check that select with force_select_final equal 'SELECT...FINAL'"):
-            assert (
-                node.query(
-                    f"SELECT * FROM {table.name}"
-                    f"{' FINAL' if table.final_modifier_available else ''} "
-                    f" ORDER BY key FORMAT JSONEachRow;"
-                ).output.strip()
-                == node.query(
-                    f"SELECT * FROM {table.name} ORDER BY key FORMAT JSONEachRow;",
-                    settings=[("force_select_final", 1)],
-                ).output.strip()
-            )
+import sys
+append_path(sys.path, "..")
 
 
 @TestFeature
@@ -51,8 +11,25 @@ def feature(self):
     """Check FINAL modifier."""
     self.context.tables = []
 
+    final_test_modules = ["modifier", "force_modifier"]
+
     with Given("I have set of populated tables"):
         create_and_populate_tables()
+        add_system_tables()
 
-    for scenario in loads(current_module(), Scenario):
-        scenario()
+    with And("I test all tables with `FINAL` modifier queries"):
+        for module in final_test_modules:
+            try:
+                case = load_module(f"final.{module}")
+            except ModuleNotFoundError:
+                xfail(reason=f"{module} tests are not implemented")
+                continue
+
+            for scenario in loads(case, Scenario):
+                scenario()
+
+
+
+
+
+
