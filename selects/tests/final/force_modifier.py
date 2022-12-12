d9445b1e229f85de5fe8a82b1fba7967d9459caf
AlmostIvanSidorov <vanosidorov@gmail.com>
2022-12-12 18:01:15 +0200

    upd.

--- selects/tests/final/force_modifier.py
+++ selects/tests/final/force_modifier.py
@@ -50,36 +50,41 @@ def select(self, node=None):
 @TestScenario
 def select_join_equal_table(self, node=None):
     """Check select count() with `FINAL` clause equal to force_select_final for 'JOIN' clause."""
-    xfail("doesn't work for latest clickhouse PR")
     if node is None:
         node = self.context.node
     with Given("I create duplicate table to core tables"):
         create_and_populate_core_tables(duplicate=True)
         self.context.tables.pop()
 
-    for table in self.context.tables:
-        if table.name.endswith("core"):
-            for table2 in self.context.tables:
-                if table2.name.endswith("duplicate") and table2.name.startswith(
-                    table.engine
-                ):
-                    with Then(
-                        "I check that select with force_select_final equal 'SELECT...FINAL'"
+    join_types = ["INNER JOIN", "LEFT OUTER JOIN", "RIGHT OUTER JOIN", "FULL OUTER JOIN", "CROSS JOIN",
+                  "LEFT SEMI JOIN", "RIGHT SEMI JOIN", "LEFT ANTI JOIN", "RIGHT ANTI JOIN", "LEFT ANY JOIN",
+                  "RIGHT ANY JOIN", "INNER ANY JOIN", "ASOF JOIN", "LEFT ASOF JOIN"]
+
+    for join_type in join_types:
+        for table in self.context.tables:
+            if table.name.endswith("core"):
+                for table2 in self.context.tables:
+                    if table2.name.endswith("duplicate") and table2.name.startswith(
+                            table.engine
                     ):
-                        assert (
-                            node.query(
-                                f"SELECT count() FROM {table.name}"
-                                f"{' FINAL' if table.final_modifier_available else ''}"
-                                f" inner join "
-                                f" {table2.name} on"
-                                f" {table.name}.key = {table2.name}.key"
-                            ).output.strip()
-                            == node.query(
-                                f"SELECT count() FROM {table.name} inner join"
+                        with Then(
+                                "I check that select with force_select_final equal 'SELECT...FINAL'"
+                        ):
+                            assert (
+                                    node.query(
+                                        f"SELECT count() FROM {table.name}"
+                                        f"{' FINAL' if table.final_modifier_available else ''}"
+                                        f" {join_type} "
+                                        f" {table2.name} on"
+                                        f" {table.name}.key = {table2.name}.key"
+                                    ).output.strip()
+                                    == node.query(
+                                f"SELECT count() FROM {table.name} {join_type}"
                                 f" {table2.name} on {table.name}.key = {table2.name}.key",
                                 settings=[("force_select_final", 1)],
                             ).output.strip()
-                        )
+                            )
+
 
 
 @TestFeature
