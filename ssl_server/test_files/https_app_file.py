505d9445817399a8fdb832ab50a009ab1d5c5847
MyroTk <myroslav6@gmail.com>
2023-01-27 18:48:30 +0100

    flask server files

---
+++ ssl_server/test_files/https_app_file.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import ssl
+import os
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+ciphers = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-GCM-SHA384"
+context.set_ciphers(ciphers)
+context.load_cert_chain("/https_server.crt", "/https_server.key")
+
+app = Flask("https server")
+app.secret_key = bytes(os.urandom(16))
+
+
+@app.route("/")
+def life_check():
+    """Check flask is running"""
+    return "Flask is running\n"
+
+
+@app.route("/data", methods=["GET"])
+def data():
+    """Return data."""
+    return "12345"
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True, ssl_context=context)
