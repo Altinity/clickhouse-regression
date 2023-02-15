c9fe271ab3cf7708de4e74a1b62c09b7055cf6bd
AlmostIvanSidorov <vanosidorov@gmail.com>
2023-02-15 14:40:52 +0300

    fixes.

--- selects/tests/final/force_modifier_concurrent.py
+++ selects/tests/final/force_modifier_concurrent.py
@@ -2,16 +2,12 @@ from testflows.core import *
 from selects.requirements import *
 from selects.tests.steps import *
 from helpers.common import check_clickhouse_version
+# from selects.tests.final.force_modifier import *
 
 
 @TestScenario
 def select_concurrent(self):
-    """Checking basic selects with `FINAL` clause equal to force_select_final select."""
-    with Given("I create queries with and without `FINAL`."):
-        query = define(
-            "count() query with and without FINAL",
-            "SELECT count() FROM {name} {final} FORMAT JSONEachRow;",
-        )
+    """Concurent count queries."""
 
     with Given("I exclude auxiliary and unsupported tables by the current test"):
         tables = define(
@@ -24,33 +20,18 @@ def select_concurrent(self):
             encoder=lambda tables: ", ".join([table.name for table in tables]),
         )
 
-    for table in tables:
-        with When(f"{table.name}"):
-            with When("I execute concurrent select queries"):
-                concurrent_queries(
-                    statement=query.format(name=table.name, final=""),
-                    parallel_selects=2,
-                )
-                concurrent_queries(
-                    statement=query.format(
-                        final=f"{' FINAL' if table.final_modifier_available else ''}",
-                        name=table.name,
-                    ),
-                    parallel_selects=2,
-                )
-                concurrent_queries(
-                    statement=query.format(final=f"", name=table.name),
-                    parallel_selects=2,
-                    final=1,
-                )
-                join()
+    # for table in tables:
+    #     with When(f"{table.name}"):
+    #         with When("I execute concurrent select queries"):
+    #             By("selecting data", test=select_count, parallel=True)()
 
 
 @TestModule
 @Requirements()
 @Name("force modifier concurrent")
 def feature(self):
-    """Tests for cases when the partitioning limit is exceeded."""
+    """Concurrent queries tests for force select final."""
+    xfail("doesn't ready")
     if check_clickhouse_version("<22.11")(self):
         skip(
             reason="force_select_final is only supported on ClickHouse version >= 22.11"
