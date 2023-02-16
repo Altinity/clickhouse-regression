cd48b7e21d16a123730a2e8382e6ca6b0bb817f4
vzakaznikov <41681088+vzakaznikov@users.noreply.github.com>
2023-02-16 10:23:28 -0500

    Update force_modifier_concurrent.py

--- selects/tests/final/force_modifier_concurrent.py
+++ selects/tests/final/force_modifier_concurrent.py
@@ -6,131 +6,36 @@ from selects.tests.select_steps import *
 from selects.tests.concurrent_query_steps import *
 
 
-@TestScenario
-def select_sanity_concurrent(self):
-    """Sanity test for concurrent queries with inserts, deletes and updates."""
-
-    with Given("I chose tables for testing"):
-        tables = define(
-            "tables",
-            [
-                table
-                for table in self.context.tables
-                if table.name.endswith("core")
-                and (
-                    table.name.startswith("ReplacingMergeTree_table")
-                    or table.name.startswith("MergeTree_table")
-                )
-            ],
-            encoder=lambda tables: ", ".join([table.name for table in tables]),
-        )
-
-    with And("I choose 'SELECT' steps for loop testing"):
-        statements = [select_count_query, select_limit_query, select_limit_by_query, select_group_by_query,
-                      select_distinct_query, select_prewhere_query, select_where_query]
-
+@TestOutline
+def parallel(self, tables, selects, inserts, updates, deletes, iterations=10):
+    """Execute specified selects, inserts, updates, and deletes in parallel."""  
     for table in tables:
-        with When(f"{table.name}"):
-            with When("I execute concurrent select, insert, delete, update queries"):
-                for statement in statements:
-                    for i in range(2):
-                        By(
-                            f"{select_count_query.name} loop iteration {i}",
-                            test=statement,
-                            parallel=True,
-                        )(
-                            name=table.name,
-                            final_manual=True,
-                            final_force=1,
-                            final_modifier_available=table.final_modifier_available,
-                        )
+        with Scenario(f"{table.name}"):
+            for i in range(iterations):
+                for select in selects:
+                    By(f"{select.name}", test=select, parallel=True)(name=table.name)
 
-                By(
-                    f"{select_count_query.name} manual 1",
-                    test=select_count_query,
-                    parallel=True,
-                )(
-                    name=table.name,
-                    final_manual=False,
-                    final_force=1,
-                    final_modifier_available=table.final_modifier_available,
-                )
+                for insert in inserts:
+                    By(f"{insert.name}", test=insert, parallel=True)(name=table.name)
 
-                By(
-                    f"{select_limit_query.name} manual 1",
-                    test=select_limit_query,
-                    parallel=True,
-                )(
-                    name=table.name,
-                    final_manual=False,
-                    final_force=0,
-                    final_modifier_available=table.final_modifier_available,
-                )
+                for update in updates:
+                    By(f"{update.name}", test=update, parallel=True)(name=table.name)
 
-                By(
-                    f"{select_count_query.name} manual 2",
-                    test=select_count_query,
-                    parallel=True,
-                )(
-                    name=table.name,
-                    final_manual=True,
-                    final_force=0,
-                    final_modifier_available=table.final_modifier_available,
-                )
+                for delete in deletes:
+                    By(f"{delete.name}", test=delete, parallel=True)(name=table.name)    
 
-                By(f"inserts,updates deletes", test=concurrent_queries, parallel=True,)(
-                    table_name=table.name,
-                    parallel_runs=1,
-                    parallel_selects=1,
-                    parallel_deletes=1,
-                    first_delete_id=1,
-                    last_delete_id=4,
-                    parallel_updates=2,
-                    first_update_id=1,
-                    last_update_id=4,
-                    parallel_inserts=10,
-                    first_insert_id=1,
-                    last_insert_id=4,
-                )
-
-            join()
-
-    with Then("I check data consistency in tested tables"):
-        for table in tables:
-            with When(f"{table.name}"):
-                By(select_count_query.name, test=select_count_query, parallel=False)(
-                    name=table.name,
-                    final_manual=False,
-                    final_force=1,
-                    final_modifier_available=table.final_modifier_available,
-                    check_results=True,
-                    final_manual_check=True,
-                    final_force_check=1,
-                )
-
-                By(select_count_query.name, test=select_count_query, parallel=False)(
-                    name=table.name,
-                    final_modifier_available=table.final_modifier_available,
-                    check_results=True,
-                )
-
-                By(f"{select_count_query.name} negative", test=select_count_query, parallel=False)(
-                    name=table.name,
-                    final_manual=False,
-                    final_force=0,
-                    final_modifier_available=table.final_modifier_available,
-                    check_results=True,
-                    final_manual_check=False,
-                    final_force_check=1,
-                    negative=True
-                )
-
-
+#FIXME: add all scenarios
+# same query runs in parallel
+# all queries in parallel
+# same query runs in parallel with inserts, updates and delete
+# ...
+                    
+                                      
 @TestModule
 @Requirements()
-@Name("force modifier concurrent")
+@Name("force modifier ")
 def feature(self):
-    """Concurrent queries tests for force select final."""
+    """Parallel queries tests for force select final."""
     if check_clickhouse_version("<22.11")(self):
         skip(
             reason="force_select_final is only supported on ClickHouse version >= 22.11"
