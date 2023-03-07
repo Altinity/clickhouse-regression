ea44411ad35ef53aab3cc5642e4fda620e681b04
MyroTk <myroslav6@gmail.com>
2023-03-07 18:31:43 +0100

    docker processing sh

--- .github/docker_package_processing.sh
+++ .github/docker_package_processing.sh
@@ -4,9 +4,10 @@ set -x
 echo "clickhouse_binary_path=$clickhouse_binary_path:$version" >> $GITHUB_ENV
 echo "get specific ClickHouse version $version"
 docker_image=${{ env.clickhouse_binary_path }}
+echo $docker_image
 docker pull $docker_image
 if [[ $version == 'latest' ]]; then
-    pid=$(docker run -d $(echo $clickhouse_binary_path))
+    pid=$(docker run -d $(echo ${{ env.clickhouse_binary_path }}))
     echo $pid
     ../retry.sh 60 2 "docker exec $pid clickhouse-client -q \"SELECT version()\""
     echo "version=$(docker exec $pid clickhouse-client -q 'SELECT version()')" >> $GITHUB_ENV
