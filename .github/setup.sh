4e0dab01593769059abb47cf4037606d06fb58b5
MyroTk <myroslav6@gmail.com>
2023-03-07 23:44:19 +0100

    adding all suites

--- .github/setup.sh
+++ .github/setup.sh
@@ -4,10 +4,13 @@ set -x
 sudo rm -rf /var/lib/apt/lists/*
 sudo rm -rf /var/cache/debconf
 sudo rm -rf /tmp/*
+
 sudo apt-get clean
 sudo pip install -r pip_requirements.txt
 sudo apt-get update
+
 mkdir $SUITE/_instances
+
 env
 uname -i
 python3 -c "import platform; print('Platform machine:', platform.machine())"
