06cfc46c7dd15c63e78d38411cfdb274c530e533
MyroTk <myroslav6@gmail.com>
2023-03-17 17:57:54 +0100

    drop timeout

--- ontime_benchmark/tests/benchmark.py
+++ ontime_benchmark/tests/benchmark.py
@@ -232,7 +232,7 @@ def benchmark(self, table_name, table_settings, nodes=None, format=None):
         with Finally("I drop the table"):
             for node in nodes:
                 with When(f"on {node}", flags=TE):
-                    node.query(f"DROP TABLE IF EXISTS {table_name} SYNC")
+                    node.query(f"DROP TABLE IF EXISTS {table_name} SYNC", timeout=420)
 
 
 @TestScenario
