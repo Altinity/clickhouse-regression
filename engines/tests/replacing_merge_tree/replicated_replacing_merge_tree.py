f5431c7a4a3869480bde29b7674b153b973b9e7a
AlmostIvanSidorov <vanosidorov@gmail.com>
2023-03-17 13:26:22 +0300

    distr upd.

--- engines/tests/replacing_merge_tree/replicated_replacing_merge_tree.py
+++ engines/tests/replacing_merge_tree/replicated_replacing_merge_tree.py
@@ -199,6 +199,78 @@ def with_is_deleted_distributed(self):
             node1.query(f"DROP TABLE IF EXISTS distr_{name} ON CLUSTER {cluster_name}")
 
 
+@TestScenario
+def update_distributed(self):
+    """Check updating a row by inserting a row with (arbitrary) greater version or delete old one for the new
+    ReplicateReplacingMergeTree engine on 2 shard cluster with one shard on 2 replicas and distributed table on it."""
+
+    insert_values_local = (
+        " ('data1','adsf', 1, 0),"
+        " ('data1','adsf', 2, 0),"
+        " ('data1', 'a', 3, 0),"
+        " ('data1', 'b', 1, 1),"
+        " ('data1', 'c', 2, 1)"
+    )
+
+    insert_values_update = " ('data1', 'a', 3, 1)" " ('data1', 'fdasd', 3, 0),"
+
+    node1 = self.context.cluster.node("clickhouse1")
+
+    name = f"update_{getuid()}"
+
+    cluster_name = "sharded_replicated_cluster"
+
+    try:
+        with Given(
+            "I create table with is_deleted column and clean_deleted_rows='Always'"
+        ):
+            node1.query(
+                f"CREATE TABLE IF NOT EXISTS {name} ON CLUSTER '{cluster_name}'"
+                f"(id String, some_data String, version UInt32, is_deleted UInt8)"
+                " ENGINE = ReplicatedReplacingMergeTree('/clickhouse"
+                f"/tables/{name}/',"
+                " '{replica}', version, is_deleted) ORDER BY id"
+            )
+
+        with When("I insert data in this table"):
+            node1.query(
+                f"INSERT INTO {name} VALUES {insert_values_local}",
+            )
+
+        with And("I create distributed table"):
+            node1.query(
+                f"CREATE TABLE IF NOT EXISTS distr_{name} ON CLUSTER '{cluster_name}'"
+                f"(id String, some_data String, version UInt32, is_deleted UInt8)"
+                f" ENGINE = Distributed('{cluster_name}','default','{name}')"
+            )
+
+        with Then(
+            "I select all data from the table with --final and expect to see all the latest "
+            "version not deleted data"
+        ):
+            node1.query(
+                f"SELECT * FROM distr_{name} FORMAT JSONEachRow;",
+                message='{"id":"data1","some_data":"a","version":3,"is_deleted":0}\n'
+                        '{"id":"data1","some_data":"a","version":3,"is_deleted":0}',
+                settings=[("final", 1)],
+            )
+
+        with And("I insert data in this table"):
+            node1.query(f"INSERT INTO {name} VALUES {insert_values_update}")
+
+        with And("I check data that data has been updated"):
+            node1.query(
+                f"SELECT * FROM distr_{name} FORMAT JSONEachRow;",
+                message='{"id":"data1","some_data":"fdasd","version":3,"is_deleted":0}\n'
+                        '{"id":"data1","some_data":"fdasd","version":3,"is_deleted":0}',
+                settings=[("final", 1)],
+            )
+    finally:
+        with Finally("I drop table"):
+            node1.query(f"DROP TABLE IF EXISTS {name} ON CLUSTER {cluster_name}")
+            node1.query(f"DROP TABLE IF EXISTS distr_{name} ON CLUSTER {cluster_name}")
+
+
 @TestModule
 @Requirements(RQ_SRS_035_ClickHouse_ReplicatedReplacingMergeTree("1.0"),
               RQ_SRS_035_ClickHouse_ReplicatedReplacingMergeTree_Distributed("1.0"))
