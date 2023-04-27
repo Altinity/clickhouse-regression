765c70de23eba134edc645eb4a49bdb4d937ee25
AlmostIvanSidorov <vanosidorov@gmail.com>
2023-04-27 17:34:31 +0200

    3 mixed keeper cluster all ssl upd.

---
+++ ssl_keeper/tests/keeper_ssl_cluster.py
@@ -0,0 +1,80 @@
+
+import time
+
+from ssl_keeper.requirements import *
+from ssl_keeper.tests.steps import *
+from helpers.common import *
+
+
+@TestScenario
+def mixed_keepers_3(self):
+    """Check that 3 nodes Clickhouse Keeper Cluster work in write mode
+    with 1 node down and in read mode only with 2 nodes down.
+    """
+    cluster = self.context.cluster
+    try:
+        with Given("Receive UID"):
+            uid = getuid()
+
+        with And("I create some replicated table"):
+            table_name = f"test{uid}"
+            create_simple_table(table_name=table_name)
+
+        with And("I stop maximum available Keeper nodes for such configuration"):
+            cluster.node("clickhouse3").stop_clickhouse()
+
+        with And("I check that table in write mode"):
+            retry(cluster.node("clickhouse1").query, timeout=500, delay=1)(
+                f"insert into {table_name} values (1,1)", exitcode=0
+            )
+
+        with And("I stop one more Keeper node"):
+            cluster.node("clickhouse2").stop_clickhouse()
+
+        if check_clickhouse_version(">23")(self):
+            with And("I check that table in read only mode"):
+                retry(cluster.node("clickhouse1").query, timeout=300, delay=10)(
+                    f"insert into {table_name} values (1,2)",
+                    exitcode=242,
+                    message="DB::Exception: Table is in readonly mode",
+                    steps=False,
+                    settings=[("insert_keeper_max_retries", 0)],
+                )
+
+        else:
+            with And("I check that table in read only mode"):
+                retry(cluster.node("clickhouse1").query, timeout=300, delay=10)(
+                    f"insert into {table_name} values (1,2)",
+                    exitcode=242,
+                    message="DB::Exception: Table is in readonly mode",
+                    steps=False,
+                )
+
+        with And("I start dropped nodes"):
+            for name in cluster.nodes["clickhouse"][1:3]:
+                cluster.node(name).start_clickhouse(wait_healthy=False)
+
+        with And(f"I check that ruok returns imok"):
+            for name in cluster.nodes["clickhouse"][0:3]:
+                retry(cluster.node("bash-tools").cmd, timeout=500, delay=1)(
+                    f"echo ruok | nc {name} 9281",
+                    exitcode=0,
+                    message="F",
+                )
+
+        with And("I check clean ability"):
+            table_insert(table_name=table_name, node_name="clickhouse1")
+
+    finally:
+        with Finally("I clean up"):
+            clean_coordination_on_all_nodes()
+
+
+@TestFeature
+@Name("keeper_ssl_cluster")
+def feature(self):
+    """Check 2N+1 cluster configurations for
+    clickhouse-keeper and zookeeper.
+    """
+    for scenario in loads(current_module(), Scenario):
+        scenario()
--- ssl_keeper/tests/sanity.py
+++ ssl_keeper/tests/sanity.py
@@ -10,10 +10,7 @@ def select_1(self):
 
         with Check(f"{name}"):
             with When("I try to execute SELECT 1 query"):
-                r = node.query("SELECT 1")
-
-            with Then("it should work"):
-                assert r.output == "1", error()
+                retry(node.query, timeout=300, delay=10)("SELECT 1", message="1", exitcode=0)
 
 
 @TestFeature
