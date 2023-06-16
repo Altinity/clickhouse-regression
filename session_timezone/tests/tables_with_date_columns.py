1aea61a3097903fc38ab0dcbb15e96ee156e3c93
AlmostIvanSidorov <vanosidorov@gmail.com>
2023-06-16 16:50:25 +0200

    upd.

--- session_timezone/tests/tables_with_date_columns.py
+++ session_timezone/tests/tables_with_date_columns.py
@@ -4,6 +4,48 @@ from testflows.asserts import error
 from session_timezone.tests.steps import *
 
 
+@TestOutline
+def create_date_type_table(self, table_name=None, with_data=False):
+    """Creates a table and optionally insert data into it"""
+    node = self.context.cluster.node("clickhouse1")
+    if table_name is None:
+        table_name = f"test_tz_{getuid()}"
+
+    try:
+        with Given("I create table with Date, DateTime, DateTime64 columns"):
+            node.query(
+                f"CREATE TABLE IF NOT EXISTS {table_name} (d Date, d2 DateTime, d3 DateTime64) Engine=MergeTree()"
+                f" ORDER BY d;"
+            )
+
+        if with_data:
+            with When("I insert data with enabled 'session_timezone' setting "):
+                node.query(f"INSERT INTO {table_name} VALUES ('2000-01-01','2000-01-01','2000-01-01'),"
+                           f"('2000-01-02','2000-01-02','2000-01-02'),"
+                           f"('2000-01-03','2000-01-03','2000-01-03')")
+
+        yield
+    finally:
+        node.query(f"DROP TABLE IF EXISTS {table_name};")
+
+
+@TestScenario
+def session_tz_select(self):
+    """Session time zone select from table."""
+    node = self.context.cluster.node("clickhouse1")
+
+    table_name = f"test_tz_{getuid()}"
+
+    with Given("I create table with Date, DateTime, DateTime64 columns and inserted data"):
+        create_date_type_table(table_name=table_name, with_data=True)
+
+    with Then("I check inserted data with session time zone setting"):
+        node.query(
+            f"SELECT * FROM {table_name} SETTINGS session_timezone='UTC' FORMAT CSV",
+            message='"2000-01-01","1999-12-31 23:00:00","1999-12-31 23:00:00.000"'
+        )
+
+
 @TestFeature
 @Requirements(
     RQ_SRS_037_ClickHouse_SessionTimezone_ParsingOfDateTimeTypes_Insert("1.0")
