2e077884c5e4382675c4771fcbaad867909f5bdc
vzakaznikov <vzakaznikov@protonmail.com>
2023-06-19 08:00:13 -0400

    First fixes to get broken suite working.

--- parquet/regression.py
+++ parquet/regression.py
@@ -118,50 +118,50 @@ def regression(
                 )
 
         with Pool(2) as executor:
+            # Feature(
+            #     run=load("parquet.tests.file", "feature"),
+            #     parallel=True,
+            #     executor=executor,
+            # )
+            # Feature(
+            #     run=load("parquet.tests.query", "feature"),
+            #     parallel=True,
+            #     executor=executor,
+            # )
+            # Feature(
+            #     run=load("parquet.tests.int_list_multiple_chunks", "feature"),
+            #     parallel=True,
+            #     executor=executor,
+            # )
+            # Feature(
+            #     run=load("parquet.tests.url", "feature"),
+            #     parallel=True,
+            #     executor=executor,
+            # )
+            # Feature(
+            #     run=load("parquet.tests.mysql", "feature"),
+            #     parallel=True,
+            #     executor=executor,
+            # )
+            # Feature(
+            #     run=load("parquet.tests.postgresql", "feature"),
+            #     parallel=True,
+            #     executor=executor,
+            # )
+            # Feature(
+            #     run=load("parquet.tests.remote", "feature"),
+            #     parallel=True,
+            #     executor=executor,
+            # )
+            # Feature(
+            #     run=load("parquet.tests.chunked_array", "feature"),
+            #     parallel=True,
+            #     executor=executor,
+            # )
             Feature(
-                run=load("parquet.tests.file", "feature"),
+                run=load("parquet.tests.broken", "feature"),
                 parallel=True,
                 executor=executor,
-            )
-            Feature(
-                run=load("parquet.tests.query", "feature"),
-                parallel=True,
-                executor=executor,
-            )
-            Feature(
-                run=load("parquet.tests.int_list_multiple_chunks", "feature"),
-                parallel=True,
-                executor=executor,
-            )
-            Feature(
-                run=load("parquet.tests.url", "feature"),
-                parallel=True,
-                executor=executor,
-            )
-            Feature(
-                run=load("parquet.tests.mysql", "feature"),
-                parallel=True,
-                executor=executor,
-            )
-            Feature(
-                run=load("parquet.tests.postgresql", "feature"),
-                parallel=True,
-                executor=executor,
-            )
-            Feature(
-                run=load("parquet.tests.remote", "feature"),
-                parallel=True,
-                executor=executor,
-            )
-            Feature(
-                run=load("parquet.tests.chunked_array", "feature"),
-                parallel=True,
-                executor=executor,
-            )
-            Feature(
-                run=load("parquet.tests.read_broken_bigint", "feature"),
-                parallel=True,
-                executor=executor
             )
 
             join()
