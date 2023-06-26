f1e66de40bd94524577313e3651013da3435b934
selfeer <selfeers@gmail.com>
2023-06-27 00:47:17 +0400

    Moving outline to the separate outline.py file | Adding snapshot_id into import_export function

--- parquet/tests/complex_datatypes.py
+++ parquet/tests/complex_datatypes.py
@@ -4,59 +4,9 @@ from testflows.core import *
 from testflows.asserts import snapshot, values
 from parquet.requirements import *
 from helpers.common import *
+from parquet.tests.outline import import_export
 
-
-@TestOutline
-def import_export(self, snapshot_name, import_file):
-    node = self.context.node
-    table_name = "table_" + getuid()
-    path_to_export = f"/var/lib/clickhouse/user_files/{table_name}.parquet"
-
-    with And("I save file structure"):
-        import_column_structure = node.query(f"DESCRIBE TABLE file('{import_file}')")
-
-    with Check("import"):
-        with When(f"I try to import the Parquet file into the table"):
-            node.query(
-                f"""
-                CREATE TABLE {table_name}
-                ENGINE = MergeTree
-                ORDER BY tuple() AS SELECT * FROM file('{import_file}', Parquet)
-                """
-            )
-
-        with And("I read the contents of the created table"):
-            import_read = node.query(f"SELECT * FROM {table_name}")
-
-        with Then("I check the output is correct"):
-            with values() as that:
-                assert that(
-                    snapshot(
-                        import_column_structure.output.strip(),
-                        name=snapshot_name,
-                    )
-                ), error()
-
-    with Check("export"):
-        with When("I export the table back into a new parquet file"):
-            node.query(
-                f"SELECT * FROM {table_name} INTO OUTFILE '{path_to_export}' COMPRESSION 'none' FORMAT Parquet"
-            )
-
-        with And("I check the exported Parquet file's contents"):
-            read = node.query(f"SELECT * FROM file('{path_to_export}', Parquet)")
-
-        with Then("output must match the snapshot"):
-            assert read.output.strip() == import_read.output.strip(), error()
-
-        with And("I check that table structure matches ..."):
-            export_columns_structure = node.query(
-                f"DESCRIBE TABLE file('{path_to_export}')"
-            )
-            assert (
-                import_column_structure.output.strip()
-                == export_columns_structure.output.strip()
-            ), error()
+snapshot_id = "complex_datatypes"
 
 
 @TestScenario
@@ -68,7 +18,7 @@ def array(self):
     with Given("I have a Parquet file with the array datatype"):
         import_file = os.path.join("arrow", "list_columns.parquet")
 
-    import_export(snapshot_name="array_structure", import_file=import_file)
+    import_export(snapshot_name="array_structure", import_file=import_file, snapshot_id=snapshot_id)
 
 
 @TestScenario
@@ -80,7 +30,7 @@ def nested_array(self):
     with Given("I have a Parquet file with the nested array datatype"):
         import_file = os.path.join("arrow", "nested_lists.snappy.parquet")
 
-    import_export(snapshot_name="nested_array_structure", import_file=import_file)
+    import_export(snapshot_name="nested_array_structure", import_file=import_file, snapshot_id=snapshot_id)
 
 
 @TestScenario
@@ -92,7 +42,7 @@ def nested_map(self):
     with Given("I have a Parquet file with the nested array datatype"):
         import_file = os.path.join("arrow", "nested_maps.snappy.parquet")
 
-    import_export(snapshot_name="nested_map_structure", import_file=import_file)
+    import_export(snapshot_name="nested_map_structure", import_file=import_file, snapshot_id=snapshot_id)
 
 
 @TestScenario
@@ -105,7 +55,7 @@ def nested_struct(self):
     with Given("I have a Parquet file with the nested array datatype"):
         import_file = os.path.join("arrow", "nested_structs.rust.parquet")
 
-    import_export(snapshot_name="nested_struct_structure", import_file=import_file)
+    import_export(snapshot_name="nested_struct_structure", import_file=import_file, snapshot_id=snapshot_id)
 
 
 @TestScenario
@@ -120,7 +70,7 @@ def complex_null(self):
     ):
         import_file = os.path.join("arrow", "nullable.impala.parquet")
 
-    import_export(snapshot_name="complex_null_structure", import_file=import_file)
+    import_export(snapshot_name="complex_null_structure", import_file=import_file, snapshot_id=snapshot_id)
 
 
 @TestScenario
@@ -133,7 +83,7 @@ def tuple_of_nulls(self):
     with Given("I have a Parquet file with the tuple of nulls datatype"):
         import_file = os.path.join("arrow", "nulls.snappy.parquet")
 
-    import_export(snapshot_name="tuple_of_nulls_structure", import_file=import_file)
+    import_export(snapshot_name="tuple_of_nulls_structure", import_file=import_file, snapshot_id=snapshot_id)
 
 
 @TestScenario
@@ -147,7 +97,7 @@ def big_tuple_with_nulls(self):
         import_file = os.path.join("arrow", "repeated_no_annotation.parquet")
 
     import_export(
-        snapshot_name="big_tuple_with_nulls_structure", import_file=import_file
+        snapshot_name="big_tuple_with_nulls_structure", import_file=import_file, snapshot_id=snapshot_id
     )
 
 
