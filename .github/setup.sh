568c8397f91dc6aa43b30060cb3cdb81470c075d
MyroTk <44327070+MyroTk@users.noreply.github.com>
2023-07-07 11:34:29 -0700

    Update setup.sh

--- .github/setup.sh
+++ .github/setup.sh
@@ -16,6 +16,8 @@ uname -i
 python3 -c "import platform; print('Platform machine:', platform.machine())"
 
 if [[ $clickhouse_binary_path == "docker"* ]]; then
+    echo "login to docker"
+    echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
     echo "clickhouse_binary_path=$clickhouse_binary_path:$version" >> $GITHUB_ENV
     echo "get specific ClickHouse version $version"
     docker_image=$(echo $clickhouse_binary_path | cut -c10- | cut -d: -f1):$version
