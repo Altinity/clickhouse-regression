1b33c3cc51937434b5a22a29dce90ebb8ef7f8e7
AlmostIvanSidorov <vanosidorov@gmail.com>
2023-07-28 11:24:37 +0200

    standalone non ssl fixes

--- clickhouse_keeper/tests/servers_start_up.py
+++ clickhouse_keeper/tests/servers_start_up.py
@@ -55,7 +55,7 @@ def mixed_start_up(
                     cluster_nodes=cluster_nodes,
                     check_preprocessed=False,
                     restart=False,
-                    modify=False,
+                    modify=True,
                 )
 
             with And("I create mixed 3 nodes Keeper server config file"):
@@ -63,7 +63,7 @@ def mixed_start_up(
                     nodes=control_nodes,
                     check_preprocessed=False,
                     restart=False,
-                    modify=False,
+                    modify=True,
                 )
 
             with And("I start mixed ClickHouse server nodes"):
@@ -83,134 +83,6 @@ def mixed_start_up(
         finally:
             with Finally("I clean up"):
                 clean_coordination_on_all_nodes()
-
-
-@TestScenario
-@Requirements(
-    RQ_SRS_024_ClickHouse_Keeper_Configurations_StandaloneKeeperCluster("1.0")
-)
-def standalone_start_up(self):
-    """Check 9 nodes ClickHouse server and standalone 3 nodes Keeper start up and simple synchronization"""
-    if check_clickhouse_version("<23.3")(self):
-        if self.context.ssl == "true":
-            clean_coordination_on_all_nodes()
-            cluster = self.context.cluster
-            try:
-                with Given("I stop all ClickHouse server nodes"):
-                    for name in cluster.nodes["clickhouse"][:12]:
-                        retry(cluster.node(name).stop_clickhouse, timeout=100, delay=1)(
-                            safe=False
-                        )
-
-                with And("I clean ClickHouse Keeper server nodes"):
-                    clean_coordination_on_all_nodes()
-
-                with Given("I create config 3 nodes Keeper server"):
-                    create_keeper_cluster_configuration_ssl(
-                        nodes=cluster.nodes["clickhouse"][9:12]
-                    )
-
-                with And("I create server openSSL config"):
-                    create_open_ssl()
-
-                with And("I create client openSSL config"):
-                    create_client_ssl()
-
-                with And("I add Keeper server configuration file"):
-                    create_config_section_ssl(
-                        control_nodes=cluster.nodes["clickhouse"][9:12],
-                        cluster_nodes=cluster.nodes["clickhouse"][:12],
-                        check_preprocessed=False,
-                    )
-
-                with Given("I start all standalone Keepers nodes"):
-                    start_keepers(
-                        standalone_keeper_nodes=cluster.nodes["clickhouse"][9:12],
-                        manual_cleanup=True,
-                    )
-
-                with And("I start ClickHouse server"):
-                    time.sleep(15)
-                    for name in cluster.nodes["clickhouse"][:9]:
-                        cluster.node(name).start_clickhouse()
-
-                with And("I create simple table"):
-                    create_simple_table(manual_cleanup=True)
-
-                with And("I make simple synchronization check"):
-                    simple_synchronization_check()
-
-            finally:
-                with Finally("I clean up"):
-                    with By("Dropping table if exists", flags=TE):
-                        self.context.cluster.node("clickhouse1").query(
-                            f"DROP TABLE IF EXISTS test ON CLUSTER "
-                            "'Cluster_3shards_with_3replicas' SYNC"
-                        )
-
-                    with By("I start clickhouse servers", flags=TE):
-                        stop_keepers(cluster_nodes=cluster.nodes["clickhouse"][9:12])
-                        for name in cluster.nodes["clickhouse"][9:12]:
-                            self.context.cluster.node(name).start_clickhouse(
-                                wait_healthy=False
-                            )
-                            time.sleep(5)
-
-                    clean_coordination_on_all_nodes()
-        else:
-            clean_coordination_on_all_nodes()
-            cluster = self.context.cluster
-            try:
-                with Given("I create config 3 nodes Keeper server"):
-                    create_keeper_cluster_configuration(
-                        nodes=cluster.nodes["clickhouse"][9:12]
-                    )
-
-                with Given("I start all standalone Keepers nodes"):
-                    for name in cluster.nodes["clickhouse"][:12]:
-                        cluster.node(name).stop_clickhouse()
-                    start_keepers(
-                        standalone_keeper_nodes=cluster.nodes["clickhouse"][9:12],
-                        manual_cleanup=True,
-                    )
-
-                with And("I add Keeper server configuration file"):
-                    create_config_section(
-                        control_nodes=cluster.nodes["clickhouse"][9:12],
-                        cluster_nodes=cluster.nodes["clickhouse"][:12],
-                        check_preprocessed=False,
-                    )
-
-                with And("I start ClickHouse server"):
-                    time.sleep(3)
-                    for name in cluster.nodes["clickhouse"][:9]:
-                        cluster.node(name).start_clickhouse()
-
-                with And("I create simple table"):
-                    create_simple_table(manual_cleanup=True)
-
-                with And("I make simple synchronization check"):
-                    simple_synchronization_check()
-
-            finally:
-                with Finally("I clean up"):
-                    with By("Dropping table if exists", flags=TE):
-                        self.context.cluster.node("clickhouse1").query(
-                            f"DROP TABLE IF EXISTS test ON CLUSTER "
-                            "'Cluster_3shards_with_3replicas' SYNC"
-                        )
-
-                    with By("I start clickhouse servers", flags=TE):
-                        stop_keepers(cluster_nodes=cluster.nodes["clickhouse"][9:12])
-                        for name in cluster.nodes["clickhouse"][9:12]:
-                            self.context.cluster.node(name).start_clickhouse(
-                                wait_healthy=False
-                            )
-                            time.sleep(5)
-
-                    clean_coordination_on_all_nodes()
-    else:
-        xfail("test doesn't work from 23.3")
 
 
 @TestScenario
@@ -345,60 +217,6 @@ def different_shared_start_up(self):
     else:
         xfail("test doesn't work from 23.3")
 
-
-# @TestScenario
-# @Requirements(RQ_SRS_024_ClickHouse_Keeper_CommandLineOptions_NoDaemon("1.0"))
-# def standalone_start_up_no_daemon(self):
-#     """Check standalone 3 nodes Keeper cluster start up and synchronization
-#     when one node in no daemon mode
-#     """
-#     cluster = self.context.cluster
-#     time.sleep(30)
-#     try:
-#         with Given("I create 3 nodes Keeper server config section"):
-#             create_keeper_cluster_configuration(nodes=cluster.nodes["clickhouse"][9:12])
-#             for name in cluster.nodes["clickhouse"][9:12]:
-#                 cluster.node(name).stop_clickhouse()
-#
-#         with Given(
-#             "I start stanalone keeper servers except the last on in daemon mode"
-#         ):
-#             for name in cluster.nodes["clickhouse"][9:11]:
-#                 node = cluster.node(name)
-#                 with When(f"I start keeper {name} in daemon mode"):
-#                     with By("starting keeper process"):
-#                         node.cmd("rm -rf /tmp/clickhouse-keeper.pid")
-#                         node.cmd(
-#                             "clickhouse keeper --config /etc/clickhouse-server/config.xml"
-#                             " --pidfile=/tmp/clickhouse-keeper.pid --daemon",
-#                             exitcode=0,
-#                         )
-#
-#                     with And("checking that keeper pid file was created"):
-#                         node.cmd(
-#                             "ls /tmp/clickhouse-keeper.pid",
-#                             exitcode=0,
-#                             message="/tmp/clickhouse-keeper.pid",
-#                         )
-#
-#         with Then(f"I check that I can start last keeper {name} in no daemon mode"):
-#             name = cluster.nodes["clickhouse"][11]
-#             node = cluster.node(name)
-#             node.cmd("rm -rf /tmp/clickhouse-keeper.pid")
-#             with node.cmd(
-#                 "clickhouse keeper --config /etc/clickhouse-server/config.xml"
-#                 " --pidfile=/tmp/clickhouse-keeper.pid",
-#                 no_checks=True,
-#                 asynchronous=True,
-#             ) as keeper_process:
-#                 keeper_process.app.expect("Quorum initialized")
-#     finally:
-#         with Finally("I clean up"):
-#             stop_keepers(cluster_nodes=cluster.nodes["clickhouse"][9:11])
-#             time.sleep(20)
-#             for name in cluster.nodes["clickhouse"][9:12]:
-#                 cluster.node(name).start_clickhouse(wait_healthy=False)
-#             clean_coordination_on_all_nodes()
 
 
 @TestFeature
