9fc867021bc113fa6c98d041cc24904a692bbf05
selfeer <selfeers@gmail.com>
2023-08-23 15:28:56 +0400

    Fix argparser descriptions

--- parquet/performance/argparsers.py
+++ parquet/performance/argparsers.py
@@ -18,7 +18,7 @@ def argparser(parser):
         "--from-year",
         type=str,
         dest="from_year",
-        help="Used to determine the starting year for downloading the large ontime dataset from the Bureau of Transportation Statistics",
+        help="determines the starting year for downloading the large ontime dataset from the Bureau of Transportation Statistics",
         metavar="value",
         default=1987,
     )
@@ -27,7 +27,7 @@ def argparser(parser):
         "--to-year",
         type=str,
         dest="to_year",
-        help="Used to determine the end year for downloading the large ontime dataset from the Bureau of Transportation Statistics",
+        help="determines the end year for downloading the large ontime dataset from the Bureau of Transportation Statistics",
         metavar="value",
         default=2015,
     )
@@ -36,7 +36,7 @@ def argparser(parser):
         "--threads",
         type=str,
         dest="threads",
-        help="Used to determine the number of threads used in creating a parquet file with large dataset",
+        help="determines the number of threads used in creating a parquet file with large dataset",
         metavar="value",
         default=20,
     )
@@ -45,7 +45,8 @@ def argparser(parser):
         "--max-memory-usage",
         type=str,
         dest="max_memory_usage",
-        help="Used to determine The maximum amount of RAM to use for running a query on a single server (values used in bytes)",
+        help="sets the maximum amount of RAM to use for running a query on a single server, 0 sets it to "
+        "unlimited (values used in bytes)",
         metavar="value",
         default=0,
     )
@@ -54,7 +55,7 @@ def argparser(parser):
         "--compression",
         type=str,
         dest="compression",
-        help="Used to determine compression used for inserting into outfile",
+        help="determines compression used for inserting into a parquet file",
         metavar="value",
         default=None,
     )
@@ -63,7 +64,7 @@ def argparser(parser):
         "--rerun-queries",
         type=str,
         dest="rerun_queries",
-        help="Used to determine the number of time to rerun each query for performance test",
+        help="determines the number of times each query in the steps file will be run",
         metavar="value",
         default=3,
     )
@@ -72,7 +73,7 @@ def argparser(parser):
         "--filename",
         type=str,
         dest="filename",
-        help="Used to determine the name of the csv file",
+        help="determines the name of the csv file that contains the report of the test run",
         metavar="value",
         default="query.csv",
     )
