9111252718e198d4a60efe16ebb596ec0fbd0f03
selfeer <selfeers@gmail.com>
2023-09-22 16:16:05 +0400

    Update

--- alter/requirements/requirements.py
+++ alter/requirements/requirements.py
@@ -8,21 +8,6 @@ from testflows.core import Requirement
 
 Heading = Specification.Heading
 
-RQ_SRS_032_ClickHouse_Alter = Requirement(
-    name="RQ.SRS-032.ClickHouse.Alter",
-    version="1.0",
-    priority=None,
-    group=None,
-    type=None,
-    uid=None,
-    description=(
-        "[ClickHouse] SHALL support the usage of the `ALTER` statement.\n" "\n"
-    ),
-    link=None,
-    level=2,
-    num="1.1",
-)
-
 RQ_SRS_032_ClickHouse_Alter_ReplacePartition = Requirement(
     name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition",
     version="1.0",
@@ -35,11 +20,11 @@ RQ_SRS_032_ClickHouse_Alter_ReplacePartition = Requirement(
     ),
     link=None,
     level=3,
-    num="1.2.1",
+    num="3.1.1",
 )
 
-RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition = Requirement(
-    name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition",
+RQ_SRS_032_ClickHouse_Alter_ReplacePartition_ReplaceData = Requirement(
+    name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.ReplaceData",
     version="1.0",
     priority=None,
     group=None,
@@ -59,7 +44,28 @@ RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition = Requirement(
     ),
     link=None,
     level=3,
-    num="1.2.2",
+    num="3.1.2",
+)
+
+RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_NewData = Requirement(
+    name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.NewData",
+    version="1.0",
+    priority=None,
+    group=None,
+    type=None,
+    uid=None,
+    description=(
+        "[ClickHouse] SHALL support replace an existing partition with new data.\n"
+        "\n"
+        "For example,\n"
+        "```sql\n"
+        "ALTER TABLE my_table REPLACE PARTITION 202301 WITH ATTACH 'path_to_new_data';\n"
+        "```\n"
+        "\n"
+    ),
+    link=None,
+    level=3,
+    num="3.1.3",
 )
 
 RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_KeepTable = Requirement(
@@ -75,7 +81,7 @@ RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_KeepTable = Require
     ),
     link=None,
     level=3,
-    num="1.2.3",
+    num="3.1.4",
 )
 
 RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_TemporaryTable = Requirement(
@@ -91,7 +97,7 @@ RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_TemporaryTable = Re
     ),
     link=None,
     level=3,
-    num="1.2.4",
+    num="3.1.5",
 )
 
 RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_Conditions = Requirement(
@@ -111,10 +117,14 @@ RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_Conditions = Requir
         "\n"
         "\n"
         "[ClickHouse]: https://clickhouse.com\n"
+        "[GitHub Repository]: https://github.com/Altinity/clickhouse-regression/blob/main/alter/requirements/requirements.md\n"
+        "[Revision History]: https://github.com/Altinity/clickhouse-regression/commits/main/alter/requirements/requirements.md\n"
+        "[Git]: https://git-scm.com/\n"
+        "[GitHub]: https://github.com\n"
     ),
     link=None,
     level=3,
-    num="1.2.5",
+    num="3.1.6",
 )
 
 SRS032_ClickHouse_Alter_statement = Specification(
@@ -134,37 +144,43 @@ SRS032_ClickHouse_Alter_statement = Specification(
     parent=None,
     children=None,
     headings=(
-        Heading(name="Working with ALTER statement", level=1, num="1"),
-        Heading(name="RQ.SRS-032.ClickHouse.Alter", level=2, num="1.1"),
-        Heading(name="REPLACE PARTITION", level=2, num="1.2"),
+        Heading(name="Revision History", level=1, num="1"),
+        Heading(name="Introduction", level=1, num="2"),
+        Heading(name="Working with ALTER statement", level=1, num="3"),
+        Heading(name="REPLACE PARTITION", level=2, num="3.1"),
         Heading(
-            name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition", level=3, num="1.2.1"
+            name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition", level=3, num="3.1.1"
         ),
         Heading(
-            name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition",
+            name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.ReplaceData",
             level=3,
-            num="1.2.2",
+            num="3.1.2",
+        ),
+        Heading(
+            name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.NewData",
+            level=3,
+            num="3.1.3",
         ),
         Heading(
             name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.KeepTable",
             level=3,
-            num="1.2.3",
+            num="3.1.4",
         ),
         Heading(
             name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.TemporaryTable",
             level=3,
-            num="1.2.4",
+            num="3.1.5",
         ),
         Heading(
             name="RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.Conditions",
             level=3,
-            num="1.2.5",
+            num="3.1.6",
         ),
     ),
     requirements=(
-        RQ_SRS_032_ClickHouse_Alter,
         RQ_SRS_032_ClickHouse_Alter_ReplacePartition,
-        RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition,
+        RQ_SRS_032_ClickHouse_Alter_ReplacePartition_ReplaceData,
+        RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_NewData,
         RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_KeepTable,
         RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_TemporaryTable,
         RQ_SRS_032_ClickHouse_Alter_ReplacePartition_AddNewPartition_Conditions,
@@ -175,21 +191,32 @@ SRS032_ClickHouse_Alter_statement = Specification(
 
 ## Table of Contents
 
-* 1 [Working with ALTER statement](#working-with-alter-statement)
-  * 1.1 [RQ.SRS-032.ClickHouse.Alter](#rqsrs-032clickhousealter)
-  * 1.2 [REPLACE PARTITION](#replace-partition)
-    * 1.2.1 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition](#rqsrs-032clickhousealterreplacepartition)
-    * 1.2.2 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition](#rqsrs-032clickhousealterreplacepartitionaddnewpartition)
-    * 1.2.3 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.KeepTable](#rqsrs-032clickhousealterreplacepartitionaddnewpartitionkeeptable)
-    * 1.2.4 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.TemporaryTable](#rqsrs-032clickhousealterreplacepartitionaddnewpartitiontemporarytable)
-    * 1.2.5 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.Conditions](#rqsrs-032clickhousealterreplacepartitionaddnewpartitionconditions)
+* 1 [Revision History](#revision-history)
+* 2 [Introduction](#introduction)
+* 3 [Working with ALTER statement](#working-with-alter-statement)
+  * 3.1 [REPLACE PARTITION](#replace-partition)
+    * 3.1.1 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition](#rqsrs-032clickhousealterreplacepartition)
+    * 3.1.2 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.ReplaceData](#rqsrs-032clickhousealterreplacepartitionreplacedata)
+    * 3.1.3 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.NewData](#rqsrs-032clickhousealterreplacepartitionaddnewpartitionnewdata)
+    * 3.1.4 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.KeepTable](#rqsrs-032clickhousealterreplacepartitionaddnewpartitionkeeptable)
+    * 3.1.5 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.TemporaryTable](#rqsrs-032clickhousealterreplacepartitionaddnewpartitiontemporarytable)
+    * 3.1.6 [RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.Conditions](#rqsrs-032clickhousealterreplacepartitionaddnewpartitionconditions)
 
 
+## Revision History
+
+This document is stored in an electronic form using [Git] source control management software
+hosted in a [GitHub Repository].
+All the updates are tracked using the [Revision History].
+
+## Introduction
+
+This software requirements specification covers requirements for `ALTER` statement in [ClickHouse].
+
+The documentation used:
+- https://clickhouse.com/docs/en/sql-reference/statements/alter/partition#replace-partition
 
 ## Working with ALTER statement
-
-### RQ.SRS-032.ClickHouse.Alter
-version: 1.0
 
 [ClickHouse] SHALL support the usage of the `ALTER` statement.
 
@@ -200,7 +227,7 @@ version: 1.0
 
 [ClickHouse] SHALL support the usage of the `REPLACE PARTITION`.
 
-#### RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition
+#### RQ.SRS-032.ClickHouse.Alter.ReplacePartition.ReplaceData
 version: 1.0
 
 [ClickHouse] SHALL support the ability to add a new partition to a table, replacing an existing one.
@@ -211,6 +238,16 @@ This query copies the data partition from the `table1` to `table2` and replaces 
 
 ```sql
 ALTER TABLE table2 [ON CLUSTER cluster] REPLACE PARTITION partition_expr FROM table1
+```
+
+#### RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.NewData
+version: 1.0
+
+[ClickHouse] SHALL support replace an existing partition with new data.
+
+For example,
+```sql
+ALTER TABLE my_table REPLACE PARTITION 202301 WITH ATTACH 'path_to_new_data';
 ```
 
 #### RQ.SRS-032.ClickHouse.Alter.ReplacePartition.AddNewPartition.KeepTable
@@ -235,5 +272,9 @@ version: 1.0
 
 
 [ClickHouse]: https://clickhouse.com
+[GitHub Repository]: https://github.com/Altinity/clickhouse-regression/blob/main/alter/requirements/requirements.md
+[Revision History]: https://github.com/Altinity/clickhouse-regression/commits/main/alter/requirements/requirements.md
+[Git]: https://git-scm.com/
+[GitHub]: https://github.com
 """,
 )
