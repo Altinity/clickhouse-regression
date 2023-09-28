401a62af49cc4ca56adcece4d708d739ed6e07e6
Vitaliy Zakaznikov <vzakaznikov@altinity.com>
2023-09-28 23:11:49 +0000

    Applying black fomatter to .github/get-deb-url.py

--- .github/get-deb-url.py
+++ .github/get-deb-url.py
@@ -45,17 +45,15 @@ if __name__ == "__main__":
     github_env = args.github_env
 
     arch = platform.uname()[-1]
-    if arch == 'x86_64':
-        build_url = 'build_urls_package_release/build_urls_package_release.json'
-    elif arch == 'aarch64':
-        build_url = 'build_urls_package_aarch64/build_urls_package_aarch64.json'
+    if arch == "x86_64":
+        build_url = "build_urls_package_release/build_urls_package_release.json"
+    elif arch == "aarch64":
+        build_url = "build_urls_package_aarch64/build_urls_package_aarch64.json"
     else:
         raise Exception("Only x86_64 and ARM are supported.")
-    
+
     with open(
-        os.path.join(
-            reports_path, build_url
-        ),
+        os.path.join(reports_path, build_url),
         "r",
         encoding="utf-8",
     ) as file_handler:
