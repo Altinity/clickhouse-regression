9cd1563a2d2a04e45211f795003326f3a3a39c07
selfeer <selfeers@gmail.com>
2023-10-11 17:34:25 +0400

    Update alter suite

--- alter/regression.py
+++ alter/regression.py
@@ -8,11 +8,58 @@ from testflows.core import *
 
 append_path(sys.path, "..")
 
+from helpers.cluster import Cluster
+from s3.regression import argparser
+from alter.table.requirements.replace_partition import *
+from helpers.datatypes import *
+
+
+xfails = {}
+
+xflags = {}
+
+ffails = {}
+
 
 @TestModule
-@Name("Alter")
-def regression(self):
-    pass
+@ArgumentParser(argparser)
+@XFails(xfails)
+@XFlags(xflags)
+@FFails(ffails)
+@Requirements(RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition("1.0"))
+@Name("alter")
+def regression(
+    self,
+    local,
+    clickhouse_version,
+    clickhouse_binary_path,
+    collect_service_logs,
+    storages,
+    stress,
+    minio_uri,
+    gcs_uri,
+    aws_s3_region,
+    aws_s3_bucket,
+    minio_root_user,
+    minio_root_password,
+    aws_s3_access_key,
+    aws_s3_key_id,
+    gcs_key_secret,
+    gcs_key_id,
+    node="clickhouse1",
+):
+    """Alter regression."""
+    nodes = {"clickhouse": ("clickhouse1", "clickhouse2", "clickhouse3")}
+
+    self.context.clickhouse_version = clickhouse_version
+
+    with Cluster(
+        local,
+        clickhouse_binary_path,
+        collect_service_logs=collect_service_logs,
+        nodes=nodes,
+    ) as cluster:
+        pass
 
 
 if main():
