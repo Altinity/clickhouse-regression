9984f8dc604a538026a0aed762aaabc174660e9e
selfeer <selfeers@gmail.com>
2023-10-17 15:19:38 +0400

    update parts.py

--- alter/table/tests/replace_partition/parts.py
+++ alter/table/tests/replace_partition/parts.py
@@ -8,8 +8,8 @@ from time import sleep
 
 
 @TestCheck
-def check_parts(self, part_type_1, part_type_2):
-    """Check that it is possible to use the repalce partition command between partitions with different types of parts."""
+def check_partition_replace_capability(self, destination_table, source_table):
+    """Check that it is possible to use the replace partition command between partitions with all types of parts."""
     node = self.context.node
     table1 = "table1_" + getuid()
     table2 = "table2_" + getuid()
@@ -38,7 +38,7 @@ def check_parts(self, part_type_1, part_type_2):
                 Column(name="p", datatype=UInt8()),
                 Column(name="i", datatype=UInt64()),
             ],
-            query_settings=part_types[part_type_1]["settings"],
+            query_settings=part_types[destination_table]["settings"],
         )
 
     with And("Create a new table with the same structure as the table_1"):
@@ -51,58 +51,57 @@ def check_parts(self, part_type_1, part_type_2):
                 Column(name="p", datatype=UInt8()),
                 Column(name="i", datatype=UInt64()),
             ],
-            query_settings=part_types[part_type_2]["settings"],
+            query_settings=part_types[source_table]["settings"],
         )
 
     with When(
         "I insert the data into table_1 based on the type of part we need to get"
     ):
-        if part_type_1 == "compact and wide":
+        if destination_table == "compact and wide":
             node.query(f"INSERT INTO {table1} {part_types['compact']['values']}")
             node.query(f"INSERT INTO {table1} {part_types['wide']['values']}")
 
         else:
-            node.query(f"INSERT INTO {table1} {part_types[part_type_1]['values']}")
+            node.query(
+                f"INSERT INTO {table1} {part_types[destination_table]['values']}"
+            )
 
     with And("I insert the data into table_2 based on the type of part we need to get"):
-        if part_type_2 == "compact and wide":
+        if source_table == "compact and wide":
             node.query(f"INSERT INTO {table2} {part_types['compact']['values']}")
             node.query(f"INSERT INTO {table2} {part_types['wide']['values']}")
         else:
-            node.query(f"INSERT INTO {table2} {part_types[part_type_2]['values']}")
+            node.query(f"INSERT INTO {table2} {part_types[source_table]['values']}")
 
     with Then(
         "I use the replace partition clause to replace the partition from table_2 into table_1 and wait for the process to finish"
     ):
         node.query(f"ALTER TABLE {table1} REPLACE PARTITION 1 FROM {table2}")
-        sleep(3)
-    with And("I select and save the partition values from the source table_2"):
+
+    with And("I select and save the part type values from the source table_2"):
         partition_values_2 = node.query(
             f"SELECT part_type FROM system.parts WHERE table = '{table2}' AND partition = '1' ORDER "
             "BY part_type"
         )
 
-    with Check("I check that the partition for table_1 was replaced from the table_2"):
+    with Check("I check that the part type for table_1 was replaced from the table_2"):
         partition_values_1 = node.query(
             f"SELECT part_type FROM system.parts WHERE table = '{table1}' AND partition = '1' ORDER "
             "BY part_type"
         )
-        note(
-            f"table11: {partition_values_1.output.strip()} \n table22: {partition_values_2.output.strip()}"
-        )
+
         assert (
             partition_values_1.output.strip() == partition_values_2.output.strip()
         ), error()
 
 
 @TestSketch(Scenario)
-@Flags(TE)
 def test_parts(self):
     values = {"wide", "compact", "compact and wide"}
 
-    check_parts(
-        part_type_1=either(*values, i="part_type_table_1"),
-        part_type_2=either(*values, i="part_type_table_2"),
+    check_partition_replace_capability(
+        destination_table=either(*values, i="destination_table"),
+        source_table=either(*values, i="source_table"),
     )
 
 
@@ -113,4 +112,4 @@ def feature(self, node="clickhouse1"):
     """Check that it is possible to use the replace partition between different part types."""
     self.context.node = self.context.cluster.node(node)
 
-    test_parts()
+    Scenario(run=test_parts)
