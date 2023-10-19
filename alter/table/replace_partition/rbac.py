e75d35dda8d220def8952c1d4e57bd27dc569e0f
selfeer <selfeers@gmail.com>
2023-10-19 20:16:32 +0400

    update

--- alter/table/replace_partition/rbac.py
+++ alter/table/replace_partition/rbac.py
@@ -139,7 +139,7 @@ def check_replace_partition_with_privileges(self):
 @Requirements(RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_RBAC("1.0"))
 @Name("rbac")
 def feature(self, node="clickhouse1"):
-    """Check that it is possible to use the replace partition as a user with different privileges."""
+    """Check that it is possible replace partition as a user with different privileges."""
     self.context.node = self.context.cluster.node(node)
 
     Scenario(run=check_replace_partition_with_privileges)
