fa1627e4e382d5e73af0cea6e4363f8a87306e86
Selfeer <davit.mnatobishvili.1@btu.edu.ge>
2023-10-26 19:16:21 +0400

    Change table_functions into prohibited_actions

--- alter/table/replace_partition/prohibited_actions.py
+++ alter/table/replace_partition/prohibited_actions.py
@@ -2,28 +2,26 @@ from testflows.core import *
 from testflows.asserts import *
 from alter.table.replace_partition.requirements.requirements import *
 from helpers.common import getuid
-from helpers.tables import (
-    create_table_partitioned_by_column,
-    insert_into_table_random_uint64,
+from alter.table.replace_partition.common import (
+    create_two_tables_partitioned_by_column_with_data,
 )
 
 
 @TestScenario
+@Requirements(
+    RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited_TableFunctions("1.0")
+)
 def table_functions(self):
-    """Checking that the usage of table functions with replace partition outputs an expected error and does not crash the ClickHouse."""
+    """Checking that the usage of table functions after from clause outputs an expected error and does not crash the
+    ClickHouse."""
     node = self.context.node
     destination_table = "destination_" + getuid()
     source_table = "source_" + getuid()
 
-    with Given("I have two tables with the same structure"):
-        create_table_partitioned_by_column(table_name=source_table)
-        create_table_partitioned_by_column(table_name=destination_table)
-
-    with And("I insert data into both tables"):
-        insert_into_table_random_uint64(
-            table_name=destination_table, number_of_values=10
+    with Given("I have two tables with the same structure filled with random data"):
+        create_two_tables_partitioned_by_column_with_data(
+            destination_table=destination_table, source_table=source_table
         )
-        insert_into_table_random_uint64(table_name=source_table, number_of_values=10)
 
     with Check("I check that the usage of table functions outputs an expected error"):
         node.query(
@@ -34,10 +32,8 @@ def table_functions(self):
 
 
 @TestFeature
-@Requirements(
-    RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited_TableFunctions("1.0")
-)
-@Name("table functions")
+@Requirements(RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited("1.0"))
+@Name("prohibited actions")
 def feature(self, node="clickhouse1"):
     """Check that the ClickHouse doesn't crash when using table functions with replace partition clause."""
     self.context.node = self.context.cluster.node(node)
