21df793be3b4f4fb15a8623fb7cc027c1c90002d
Selfeer <davit.mnatobishvili.1@btu.edu.ge>
2023-10-26 23:56:33 +0400

    add prohibited actions

--- alter/table/replace_partition/prohibited_actions.py
+++ alter/table/replace_partition/prohibited_actions.py
@@ -1,10 +1,19 @@
 from testflows.core import *
 from testflows.asserts import *
-from alter.table.replace_partition.requirements.requirements import *
 from helpers.common import getuid
+from helpers.tables import create_table_partitioned_by_column, create_table
+from alter.table.replace_partition.requirements.requirements import *
 from alter.table.replace_partition.common import (
     create_two_tables_partitioned_by_column_with_data,
+    create_merge_tree_and_memory_tables,
 )
+
+
+def io_error_message(exitcode=62, message="Syntax error"):
+    return (
+        exitcode,
+        f"DB::Exception: {message}",
+    )
 
 
 @TestScenario
@@ -17,18 +26,209 @@ def table_functions(self):
     node = self.context.node
     destination_table = "destination_" + getuid()
     source_table = "source_" + getuid()
+    exitcode, message = io_error_message()
 
     with Given("I have two tables with the same structure filled with random data"):
         create_two_tables_partitioned_by_column_with_data(
             destination_table=destination_table, source_table=source_table
         )
 
-    with Check("I check that the usage of table functions outputs an expected error"):
+    with Check(
+        "I check that the usage of table function after from clause outputs an expected error"
+    ):
         node.query(
-            f"ALTER TABLE {destination_table} REPLACE PARTITION 2 FROM {source_table} file('file.parquet')",
-            exitcode=62,
-            message="DB::Exception: Syntax error",
+            f"ALTER TABLE {destination_table} REPLACE PARTITION 2 FROM file('file.parquet')",
+            exitcode=exitcode,
+            message=message,
         )
+
+
+@TestScenario
+@Requirements(RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited_Join("1.0"))
+def join(self):
+    """Checking that the usage of JOIN does output an expected error and does not crash the ClickHouse."""
+    node = self.context.node
+    destination_table = "destination_" + getuid()
+    source_table = "source_" + getuid()
+    exitcode, message = io_error_message()
+
+    with Given(
+        "I have two tables with a MergeTree engine, with the same structure partitioned by the same column"
+    ):
+        create_two_tables_partitioned_by_column_with_data(
+            destination_table=destination_table, source_table=source_table
+        )
+
+    with Check(
+        "I check that the usage of join after from clause outputs an expected error"
+    ):
+        node.query(
+            f"ALTER TABLE {destination_table} REPLACE PARTITION 2 FROM JOIN table_2 ON table_1.p",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+@Requirements(RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited_Join("1.0"))
+def subquery(self):
+    """Checking that the usage of subquery does output an expected error and does not crash the ClickHouse."""
+    node = self.context.node
+    destination_table = "destination_" + getuid()
+    source_table = "source_" + getuid()
+    exitcode, message = io_error_message()
+
+    with Given(
+        "I have two tables with a MergeTree engine, with the same structure partitioned by the same column"
+    ):
+        create_two_tables_partitioned_by_column_with_data(
+            destination_table=destination_table, source_table=source_table
+        )
+
+    with Check(
+        "I check that the usage of subquery after from clause outputs an expected error"
+    ):
+        node.query(
+            f"ALTER TABLE {destination_table} REPLACE PARTITION 2 FROM (SELECT 1)",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+@Requirements(
+    RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited_OrderAndPartition(
+        "1.0"
+    )
+)
+def order_by_partition_by(self):
+    """Checking that the usage of order by and partition by does output an expected error and does not crash the ClickHouse."""
+    node = self.context.node
+    destination_table = "destination_" + getuid()
+    source_table = "source_" + getuid()
+    exitcode, message = io_error_message()
+
+    with Given(
+        "I have two tables with a MergeTree engine, with the same structure partitioned by the same column"
+    ):
+        create_two_tables_partitioned_by_column_with_data(
+            destination_table=destination_table, source_table=source_table
+        )
+
+    with Check(
+        "I check that the usage of subquery after from clause outputs an expected error"
+    ):
+        node.query(
+            f"ALTER TABLE {destination_table} REPLACE PARTITION 2 FROM {source_table} ORDER BY p PARTITION BY p",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+@Requirements(RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited("1.0"))
+def non_mergetree_table(self):
+    """Checking that it is not possible to replace partition on a destination table from a table that is not a MergeTree table."""
+    node = self.context.node
+    destination_table = "destination_" + getuid()
+    source_table = "source_" + getuid()
+    exitcode, message = io_error_message(
+        exitcode=48, message="Cannot replace partition from table"
+    )
+
+    with Given(
+        "I create a destination table with a MergeTree engine partitioned by a column and a memory table that has no partitions"
+    ):
+        create_merge_tree_and_memory_tables(
+            merge_tree_table=destination_table, memory_table=source_table
+        )
+
+    with Check(
+        "That it is not possible to replace a partition on a mergetree table from a memory table"
+    ):
+        node.query(
+            f"ALTER TABLE {destination_table} REPLACE PARTITION 2 FROM {source_table}",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with But("Check that partition was not replaced on the destination table"):
+        destination_data = node.query(
+            f"SELECT * FROM {destination_table} WHERE p = 2 ORDER BY p"
+        )
+        source_data = node.query(f"SELECT * FROM {source_table} WHERE p = 2 ORDER BY p")
+
+        assert destination_data.output.strip() != source_data.output.strip()
+
+
+@TestScenario
+@Requirements(
+    RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited_View_Normal("1.0")
+)
+def view(self):
+    node = self.context.node
+    destination_table = "destination_" + getuid()
+    view_name = "view_" + getuid()
+    exitcode, message = io_error_message(
+        exitcode=48, message="Cannot replace partition from table"
+    )
+
+    with Given("I have a MergeTree table partitioned by a column"):
+        create_table_partitioned_by_column(table_name=destination_table)
+
+    with When("I create a normal view"):
+        node.query(f"CREATE VIEW {view_name} AS SELECT 1;")
+
+    with Check(
+        "I check that replacing partition on the destination table from the normal view outputs an error"
+    ):
+        node.query(
+            f"ALTER TABLE {destination_table} REPLACE PARTITION 1 FROM {view_name}",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+@Requirements(
+    RQ_SRS_032_ClickHouse_Alter_Table_ReplacePartition_Prohibited_View_Materialized(
+        "1.0"
+    )
+)
+def materialized_view(self):
+    node = self.context.node
+    destination_table = "destination_" + getuid()
+    view_name = "view_" + getuid()
+    exitcode, message = io_error_message(
+        exitcode=48, message="Cannot replace partition from table"
+    )
+
+    with Given("I have a MergeTree table partitioned by a column"):
+        create_table_partitioned_by_column(table_name=destination_table)
+
+    with When("I create a materialized view of the destination table"):
+        node.query(
+            f"CREATE MATERIALIZED VIEW {view_name} TO {destination_table} AS SELECT * FROM {destination_table};"
+        )
+
+    with Check(
+        "I check that replacing partition on the destination table from the normal view outputs an error"
+    ):
+        node.query(
+            f"ALTER TABLE {destination_table} REPLACE PARTITION 1 FROM {view_name}",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestSuite
+def from_clause(self):
+    Scenario(run=table_functions)
+    Scenario(run=join)
+    Scenario(run=subquery)
+    Scenario(run=non_mergetree_table)
+    Scenario(run=view)
+    Scenario(run=materialized_view)
 
 
 @TestFeature
@@ -38,4 +238,5 @@ def feature(self, node="clickhouse1"):
     """Check that the ClickHouse doesn't crash when using table functions with replace partition clause."""
     self.context.node = self.context.cluster.node(node)
 
-    Scenario(run=table_functions)
+    Feature(run=from_clause)
+    Scenario(run=order_by_partition_by)
