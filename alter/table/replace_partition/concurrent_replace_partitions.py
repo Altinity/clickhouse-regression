76ea6141288a6e442470b9d1c0c66c6bcd26fa76
Selfeer <davit.mnatobishvili.1@btu.edu.ge>
2023-11-06 21:30:22 +0400

    update description

--- alter/table/replace_partition/concurrent_replace_partitions.py
+++ alter/table/replace_partition/concurrent_replace_partitions.py
@@ -1,3 +1,7 @@
+import random
+
+from time import sleep
+
 from testflows.core import *
 from testflows.asserts import *
 from alter.table.replace_partition.requirements.requirements import *
@@ -7,7 +11,6 @@ from alter.table.replace_partition.common import (
 )
 from helpers.alter import *
 from helpers.tables import create_table_partitioned_by_column
-import random
 
 
 @TestStep(Given)
@@ -54,21 +57,29 @@ def source_table_with_partitions(self, table_name, number_of_partitions):
 
 @TestStep(Given)
 def create_two_tables(
-    self, destination_table_name, source_table_name, number_of_partitions=100
+    self, destination_table=None, source_table=None, number_of_partitions=None
 ):
     """Create two tables with the same structure having the specified number of partitions.
     The destination table will be used as the table where replace partition command will be
     replacing the partition and the source table will be used as the table from which partitions will be taken.
     """
+    if destination_table is None:
+        destination_table = self.context.destination_table
+
+    if source_table is None:
+        source_table = self.context.source_table
+
+    if number_of_partitions is None:
+        number_of_partitions = self.context.number_of_partitions
 
     with By("creating destination table"):
         destination_table_with_partitions(
-            table_name=destination_table_name, number_of_partitions=number_of_partitions
+            table_name=destination_table, number_of_partitions=number_of_partitions
         )
 
     with And("creating source table"):
         source_table_with_partitions(
-            table_name=source_table_name, number_of_partitions=number_of_partitions
+            table_name=source_table, number_of_partitions=number_of_partitions
         )
 
 
@@ -78,10 +89,26 @@ def replace_partition_and_validate_data(
     partition_to_replace,
     destination_table=None,
     source_table=None,
+    delay_before=None,
+    delay_after=None,
+    validate=True,
 ):
     """
     Replace partition and validate that the data on the destination table is the same data as on the source table.
     Also check that the data on the source table was not lost during replace partition.
+
+     Args:
+        partition_to_replace (str): The partition identifier that needs to be replaced in the destination table.
+        destination_table (str, optional): The name of the destination table where the partition is to be replaced.
+        Defaults to None, in which case the table name is taken from the test context.
+        source_table (str, optional): The name of the source table from which to take the replacement partition.
+        Defaults to None, in which case the table name is taken from the test context.
+        delay_before (float, optional): The delay in seconds to wait before performing the partition replacement.
+        Defaults to None, which causes a random delay.
+        delay_after (float, optional): The delay in seconds to wait after performing the partition replacement.
+        Defaults to None, which causes a random delay.
+        validate (bool, optional): A flag determining whether to perform validation checks after the partition replacement.
+        Defaults to True.
     """
     node = self.context.node
 
@@ -91,6 +118,12 @@ def replace_partition_and_validate_data(
     if source_table is None:
         source_table = self.context.source_table
 
+    if delay_before is None:
+        delay_before = random.random()
+
+    if delay_after is None:
+        delay_after = random.random()
+
     with By(
         "saving the data from the source table before replacing partitions on the destination table"
     ):
@@ -99,19 +132,24 @@ def replace_partition_and_validate_data(
         )
 
     with And("replacing partition on the destination table"):
+        sleep(delay_before)
+
         replace_partition(
             destination_table=destination_table,
             source_table=source_table,
             partition=partition_to_replace,
         )
 
-    with Then("checking that the partition was replaced on the destination table"):
-        check_partition_was_replaced(
-            destination_table=destination_table,
-            source_table=source_table,
-            source_table_before_replace=source_data_before,
-            partition=partition_to_replace,
-        )
+        sleep(delay_after)
+
+    if validate:
+        with Then("checking that the partition was replaced on the destination table"):
+            check_partition_was_replaced(
+                destination_table=destination_table,
+                source_table=source_table,
+                source_table_before_replace=source_data_before,
+                partition=partition_to_replace,
+            )
 
 
 @TestScenario
@@ -121,9 +159,19 @@ def concurrent_replace(
     number_of_concurrent_queries=None,
     destination_table=None,
     source_table=None,
+    delay_before=None,
+    delay_after=None,
 ):
     """Concurrently run multiple replace partitions on the destination table and
     validate that the data on both destination and source tables is the same."""
+
+    validate = self.context.validate
+
+    if delay_before is None:
+        delay_before = self.context.delay_before
+
+    if delay_after is None:
+        delay_after = self.context.delay_after
 
     if number_of_partitions is None:
         number_of_partitions = self.context.number_of_partitions
@@ -137,14 +185,31 @@ def concurrent_replace(
     if source_table is None:
         source_table = self.context.source_table
 
+    list_of_partitions_replaced = []
+
     for i in range(number_of_concurrent_queries):
         partition_to_replace = random.randrange(1, number_of_partitions)
+        list_of_partitions_replaced.append(partition_to_replace)
 
         Check(
             name=f"replace partition #{i} partition {partition_to_replace}",
             test=replace_partition_and_validate_data,
             parallel=True,
-        )(partition_to_replace=partition_to_replace)
+        )(
+            partition_to_replace=partition_to_replace,
+            validate=validate,
+            delay_before=delay_before,
+            delay_after=delay_after,
+        )
+
+    if not validate:
+        with Then("checking that the partition was replaced on the destination table"):
+            for i in list_of_partitions_replaced:
+                check_partition_was_replaced(
+                    destination_table=destination_table,
+                    source_table=source_table,
+                    partition=i,
+                )
 
 
 @TestFeature
@@ -155,7 +220,13 @@ def concurrent_replace(
 )
 @Name("concurrent replace partitions")
 def feature(
-    self, node="clickhouse1", number_of_partitions=100, number_of_concurrent_queries=100
+    self,
+    node="clickhouse1",
+    number_of_partitions=100,
+    number_of_concurrent_queries=100,
+    delay_before=None,
+    delay_after=None,
+    validate=True,
 ):
     """
     Concurrently execute replace partition on a destination table with
@@ -163,11 +234,13 @@ def feature(
     using a randomly picked partition.
     """
     self.context.node = self.context.cluster.node(node)
-
+    self.context.delay_before = delay_before
+    self.context.delay_after = delay_after
     self.context.destination_table = "destination_" + getuid()
     self.context.source_table = "source_" + getuid()
     self.context.number_of_partitions = number_of_partitions
     self.context.number_of_concurrent_queries = number_of_concurrent_queries
+    self.context.validate = validate
 
     with Given(
         "I create destination and source tables for the replace partition command"
