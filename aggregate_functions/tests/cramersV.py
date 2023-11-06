156e9662d87f240410b28f9e327da0ccd1d82819
Alsu Giliazova <74852806+alsugiliazova@users.noreply.github.com>
2023-11-06 17:03:20 +0100

    add tests for three aggregate fuctions (#14)
    * add tests for three more aggregate fuctions

---
+++ aggregate_functions/tests/cramersV.py
@@ -0,0 +1,21 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Miscellaneous_CramersV,
+)
+
+from aggregate_functions.tests.steps import get_snapshot_id
+from aggregate_functions.tests.covarPop import scenario as checks
+
+
+@TestScenario
+@Name("cramersV")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Miscellaneous_CramersV("1.0"))
+def scenario(self, func="cramersV({params})", table=None):
+    """Check cramersV aggregate function by using the same checks as for covarPop."""
+    self.context.snapshot_id = get_snapshot_id()
+
+    if table is None:
+        table = self.context.table
+
+    checks(func=func, table=table, decimal=False, extended_precision=False, snapshot_id=self.context.snapshot_id)
---
+++ aggregate_functions/tests/cramersVBiasCorrected.py
@@ -0,0 +1,26 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Miscellaneous_CramersVBiasCorrected,
+)
+
+from aggregate_functions.tests.steps import *
+from aggregate_functions.tests.covarPop import scenario as checks
+
+
+@TestScenario
+@Name("cramersVBiasCorrected")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Miscellaneous_CramersVBiasCorrected("1.0"))
+def scenario(self, func="cramersVBiasCorrected({params})", table=None):
+    """Check cramersVBiasCorrected aggregate function by using the same checks as for covarPop."""
+    self.context.snapshot_id = get_snapshot_id()
+
+    if table is None:
+        table = self.context.table
+
+    checks(func=func, table=table, decimal=False, extended_precision=False, snapshot_id=self.context.snapshot_id)
+
+    with Check("example_2"):
+        execute_query(
+            f"SELECT {func.format(params='a,b')} FROM (SELECT number % 10 AS a, number % 4 AS b FROM numbers(150))"
+        )
