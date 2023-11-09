88aa8f046ad5c721496d1d30ed6e899fdb013ded
alsugiliazova <alsugilyazova1@mail.ru>
2023-11-09 15:20:48 +0100

    add snapshots for histogram aggregate function

--- aggregate_functions/tests/histogram.py
+++ aggregate_functions/tests/histogram.py
@@ -1,27 +1,15 @@
-from testflows.core import *
+from helpers.tables import is_numeric
 
+from aggregate_functions.tests.steps import *
 from aggregate_functions.requirements import (
     RQ_SRS_031_ClickHouse_AggregateFunctions_Parametric_Histogram
-)
-
-from helpers.datatypes import *
-from helpers.tables import (
-    is_numeric,
-    is_unsigned_integer,
-    is_low_cardinality,
-    is_nullable,
-)
-from aggregate_functions.tests.steps import (
-    get_snapshot_id,
-    execute_query,
-    permutations_with_replacement,
 )
 
 
 @TestScenario
 @Name("histogram")
 @Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Parametric_Histogram("1.0"))
-def scenario(self, func="histogram({params})({values})", table=None):
+def scenario(self, func="histogram({params})", table=None, decimal=False):
     """Check histogram aggregate function"""
 
     self.context.snapshot_id = get_snapshot_id()
@@ -30,35 +18,54 @@ def scenario(self, func="histogram({params})({values})", table=None):
         table = self.context.table
 
     with Check("constant"):
-        execute_query(f"SELECT {func.format(params='5', values='1')}")
+        execute_query(
+            f"SELECT {func.format(params='5)(1')}"
+        )
 
     with Check("zero rows"):
         execute_query(
-            f"SELECT {func.format(params='5', values='number')} FROM numbers(0)"
+            f"SELECT {func.format(params='5)(number')} FROM numbers(0)"
         )
 
     with Check("single row"):
         execute_query(
-            f"SELECT {func.format(params='5', values='number')} FROM numbers(1)"
+            f"SELECT {func.format(params='5)(number')} FROM numbers(1)"
         )
 
     with Check("with group by"):
         execute_query(
-            f"SELECT number % 2 AS even, {func.format(params='5', values='number')} FROM numbers(10) GROUP BY even"
+            f"SELECT number % 2 AS even, {func.format(params='5)(number')} FROM numbers(10) GROUP BY even"
         )
 
     with Check("NULL value handling"):
         execute_query(
-            f"SELECT {func.format(params='5', values='x')}  FROM values('x Nullable(UInt8)', 0, 1, NULL, 3, 4, 5)"
+            f"SELECT {func.format(params='5)(x')}  FROM values('x Nullable(UInt8)', 0, 1, NULL, 3, 4, 5)"
         )
 
     with Check("single NULL value"):
         execute_query(
-            f"SELECT {func.format(params='5', values='x')}  FROM values('x Nullable(UInt8), w Nullable(UInt8)', (NULL,NULL) )"
+            f"SELECT {func.format(params='5)(x')}  FROM values('x Nullable(UInt8), w Nullable(UInt8)', (NULL,NULL) )"
         )
 
     with Check("return type"):
         execute_query(
-            f"SELECT toTypeName({func.format(params='5', values='number')}) FROM numbers(1, 10)"
+            f"SELECT toTypeName({func.format(params='5)(number')}) FROM numbers(1, 10)"
         )
+
+    with Check("different number of bins"):
+        for k in range(1, 10):
+            with When(f"{k}"):
+                execute_query(
+                    f"SELECT {func.format(params=f'{k})(number')} FROM numbers(100)"
+                )
+
+    for column in table.columns:
+        column_name, column_type = column.name, column.datatype.name
+
+        if not is_numeric(column.datatype, decimal=decimal):
+            continue
+
+        with Check(f"{column_type}"):
+            execute_query(f"SELECT {func.format(params=f'5)({column_name}')} FROM {table.name}")
+    
 
