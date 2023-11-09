012bcde0d234cc93cbdc82d0a8ad55769df0d15f
Alsu Giliazova <74852806+alsugiliazova@users.noreply.github.com>
2023-11-09 15:35:26 +0100

    test for maxIntersections and maxIntersectionsPosition (#17)
    * add tests for other aggregate function

---
+++ aggregate_functions/tests/maxIntersectionsPosition.py
@@ -0,0 +1,21 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Miscellaneous_MaxIntersectionsPosition,
+)
+
+from aggregate_functions.tests.steps import get_snapshot_id
+from aggregate_functions.tests.maxIntersections import scenario as checks
+
+
+@TestScenario
+@Name("maxIntersectionsPosition")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Miscellaneous_MaxIntersectionsPosition("1.0"))
+def scenario(self, func="maxIntersectionsPosition({params})", table=None):
+    """Check maxIntersectionsPosition aggregate function by using the same tests as for maxIntersections."""
+    self.context.snapshot_id = get_snapshot_id()
+
+    if table is None:
+        table = self.context.table
+
+    checks(func=func, table=table, snapshot_id=self.context.snapshot_id)
