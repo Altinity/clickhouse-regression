69bf6192e4960b036cb2f309f6fc6724fbda7eb8
Stuart <146047128+strtgbb@users.noreply.github.com>
2023-11-15 09:59:38 -0500

    Fix time zone exit code check

--- session_timezone/tests/steps.py
+++ session_timezone/tests/steps.py
@@ -4,7 +4,12 @@ from helpers.common import getuid
 
 @TestStep
 def clickhouse_local(
-    self, query="select timezone()", node=None, timezone="UTC", message=""
+    self,
+    query="select timezone()",
+    node=None,
+    timezone="UTC",
+    message="",
+    exitcode=0,
 ):
     """Step to enable clickhouse local query"""
     if node is None:
@@ -12,5 +17,7 @@ def clickhouse_local(
 
     with When("I make clickhouse local query"):
         node.command(
-            f'TZ={timezone} clickhouse local -q  "{query}" --multiquery', message=f"{message}"
+            f'TZ={timezone} clickhouse local -q  "{query}" --multiquery',
+            message=f"{message}",
+            exitcode=exitcode,
         )
