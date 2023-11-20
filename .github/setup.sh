ffdabffacdf28f995638ff835622a8eeba8e251c
vzakaznikov <41681088+vzakaznikov@users.noreply.github.com>
2023-11-20 11:32:12 -0500

    Update setup.sh

--- .github/setup.sh
+++ .github/setup.sh
@@ -18,6 +18,7 @@ sudo apt-get update
 
 echo "Install docker-compose..."
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 mkdir $SUITE/_instances
 
