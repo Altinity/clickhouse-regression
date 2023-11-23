1c62b9071376e129f77c4c1cf37c9dc57c012ca6
Stuart <146047128+strtgbb@users.noreply.github.com>
2023-11-23 15:23:50 -0500

    Increase timeout on tiered storage adding_another_volume

--- tiered_storage/tests/background_move/adding_another_volume.py
+++ tiered_storage/tests/background_move/adding_another_volume.py
@@ -141,7 +141,7 @@ def scenario(self, cluster, node="clickhouse1"):
                     with And(
                         "that the first two (oldest) parts were moved to 'external'"
                     ):
-                        for attempt in retries(timeout=60, delay=1):
+                        for attempt in retries(timeout=180, delay=1):
                             with attempt:
                                 assert used_disks[0] == "external", error()
 
