9eb2e75d812daa5ea4c2740bbf930ab901355ef1
Alsu Giliazova <74852806+alsugiliazova@users.noreply.github.com>
2023-11-29 20:02:08 +0100

    Add groupBitmapAnd, groupBitmapOr, groupBitmapXor and retention aggregate functions (#24)
    * add tests for groupBitmapAnd

---
+++ aggregate_functions/tests/groupBitmapXor.py
@@ -0,0 +1,25 @@
+from testflows.core import *
+
+from helpers.tables import *
+from aggregate_functions.tests.steps import *
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_GroupBitmapXor,
+)
+from aggregate_functions.tests.groupBitmapAnd import scenario as checks
+
+
+@TestScenario
+@Name("groupBitmapXor")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_GroupBitmapXor("1.0"))
+def scenario(
+    self,
+    func="groupBitmapXor({params})",
+    snapshot_id=None,
+):
+    """Check groupBitmapXor aggregate function by using the same tests as for groupBitmapAnd."""
+    self.context.snapshot_id = get_snapshot_id(snapshot_id=snapshot_id)
+
+    if "Merge" in self.name:
+        skip(reason=f"Does not support -Merge combinator")
+
+    checks(func=func, snapshot_id=self.context.snapshot_id)
