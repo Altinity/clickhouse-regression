3409fbdf811d155861c5f31c90907c1c7b537c4a
alsu <alsugilyazova1@mail.ru>
2023-12-04 02:09:17 +0100

    add snapshots because of change from 23.10

--- aggregate_functions/tests/groupBitmapAnd.py
+++ aggregate_functions/tests/groupBitmapAnd.py
@@ -76,7 +76,7 @@ def scenario(
     snapshot_id=None,
 ):
     """Check groupBitmapAnd aggregate function."""
-    self.context.snapshot_id = get_snapshot_id(snapshot_id=snapshot_id)
+    self.context.snapshot_id = get_snapshot_id(snapshot_id=snapshot_id, clickhouse_version=">=23.10")
 
     if "Merge" in self.name:
         skip(reason=f"Does not support -Merge combinator")
