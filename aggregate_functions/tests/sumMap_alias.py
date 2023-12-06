dfd92bae5dc8f1216f9a9e92ad878bc83abc3772
alsugiliazova <alsugilyazova1@mail.ru>
2023-12-06 13:05:42 +0000

    ad tests for sumMap aggregate function

---
+++ aggregate_functions/tests/sumMap_alias.py
@@ -0,0 +1,24 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_SumMap,
+)
+
+from aggregate_functions.tests.steps import get_snapshot_id
+from aggregate_functions.tests.maxMappedArrays import scenario as checks
+
+
+@TestScenario
+@Name("sumMap_alias")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_SumMap("1.0"))
+def scenario(self, func="sumMap({params})", table=None, snapshot_id=None):
+    """Check sumMap(sumMappedArrays) aggregate function by using the same tests as for maxMap(maxMappedArrays)."""
+    self.context.snapshot_id = get_snapshot_id(snapshot_id=snapshot_id)
+
+    if "Merge" in self.name:
+        return self.context.snapshot_id, func.replace("({params})", "")
+
+    if table is None:
+        table = self.context.table
+
+    checks(func=func, table=table, snapshot_id=self.context.snapshot_id)
