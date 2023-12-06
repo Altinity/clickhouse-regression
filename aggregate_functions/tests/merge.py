3fb61f7e1a07b833b32740c0a0bccb6a074090e8
alsu <alsugilyazova1@mail.ru>
2023-12-06 13:59:12 +0100

    apply black

--- aggregate_functions/tests/merge.py
+++ aggregate_functions/tests/merge.py
@@ -154,6 +154,8 @@ def feature(self):
         "uniqExact",  # problem on 23.8 aarch
         "welchTTest",  # problem on 22.8 aarch
         "studentTTest",
+        "sequenceCount",
+        "sequenceMatch"
     ]
     parametric = ['histogram', 'windowFunnel', 'uniqUpTo', 'sumMapFiltered', 'exponentialMovingAverage',
                   'groupArraySample', 'meanZTest', 'quantilesBFloat16', 'quantilesBFloat16Weighted',
--- aggregate_functions/tests/minMap_alias.py
+++ aggregate_functions/tests/minMap_alias.py
@@ -13,7 +13,9 @@ from aggregate_functions.tests.maxMappedArrays import scenario as checks
 @Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_MinMap("1.0"))
 def scenario(self, func="minMap({params})", table=None, snapshot_id=None):
     """Check minMap(minMappedArrays) aggregate function by using the same tests as for maxMap(maxMappedArrays)."""
-    self.context.snapshot_id = get_snapshot_id(snapshot_id=snapshot_id, clickhouse_version=">=23.11")
+    self.context.snapshot_id = get_snapshot_id(
+        snapshot_id=snapshot_id, clickhouse_version=">=23.11"
+    )
 
     if "Merge" in self.name:
         return self.context.snapshot_id, func.replace("({params})", "")
