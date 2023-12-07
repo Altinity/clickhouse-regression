750d4c0b10975f40bb66531bf7339ebd28a70907
alsugiliazova <alsugilyazova1@mail.ru>
2023-12-07 18:09:46 +0000

    add tests for corrMatrix, covarPopMatrix and covarSampMatrix aggregate functions and arm snapshots for them

---
+++ aggregate_functions/tests/covarPopMatrix.py
@@ -0,0 +1,24 @@
+from testflows.core import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_CovarPopMatrix
+)
+
+from aggregate_functions.tests.steps import get_snapshot_id
+from aggregate_functions.tests.corrMatrix import scenario as checks
+
+
+@TestScenario
+@Name("covarPopMatrix")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_CovarPopMatrix("1.0"))
+def scenario(self, func="covarPopMatrix({params})", table=None, snapshot_id=None):
+    """Check covarPopMatrix aggregate function by using the same checks as for corrMatrix."""
+    self.context.snapshot_id = get_snapshot_id(snapshot_id=snapshot_id)
+
+    if "Merge" in self.name:
+        return self.context.snapshot_id, func.replace("({params})", "")
+
+    if table is None:
+        table = self.context.table
+
+    checks(func=func, table=table, snapshot_id=self.context.snapshot_id)
