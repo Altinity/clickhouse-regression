7f1d22426ce6a3f627b5e4821d33f163ee7c79d2
alsu <alsugilyazova1@mail.ru>
2023-12-08 13:16:11 +0100

    add snapshots and apply black

--- aggregate_functions/tests/groupArrayLast.py
+++ aggregate_functions/tests/groupArrayLast.py
@@ -13,13 +13,9 @@ from aggregate_functions.tests.any import scenario as checks
 @Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_GroupArrayLast("1.0"))
 def scenario(self, func="groupArrayLast({params})", table=None, snapshot_id=None):
     """Check groupArrayLast aggregate function by using the same tests as for any."""
-    self.context.snapshot_id = get_snapshot_id(
-        snapshot_id=snapshot_id
-    )
+    self.context.snapshot_id = get_snapshot_id(snapshot_id=snapshot_id)
 
-    _func = func.replace(
-        "({params})", f"(5)({{params}})"
-    )
+    _func = func.replace("({params})", f"(5)({{params}})")
 
     if "Merge" in self.name:
         return self.context.snapshot_id, _func.replace("({params})", "")
@@ -28,5 +24,3 @@ def scenario(self, func="groupArrayLast({params})", table=None, snapshot_id=None
         table = self.context.table
 
     checks(func=_func, table=table, snapshot_id=self.context.snapshot_id)
-
-    
