580a329cf48b7c0bb427d947a305b203fdbddf72
Stuart <146047128+strtgbb@users.noreply.github.com>
2023-12-13 12:05:33 -0500

    Switch Cluster for create_cluster in ldap

--- ldap/role_mapping/regression.py
+++ ldap/role_mapping/regression.py
@@ -5,7 +5,7 @@ from testflows.core import *
 
 append_path(sys.path, "..", "..")
 
-from helpers.cluster import Cluster
+from helpers.cluster import create_cluster
 from helpers.argparser import argparser
 from ldap.role_mapping.requirements import *
 from helpers.common import check_clickhouse_version
@@ -92,21 +92,23 @@ def regression(
     if stress is not None:
         self.context.stress = stress
 
-    with Cluster(
-        local,
-        clickhouse_binary_path,
-        collect_service_logs=collect_service_logs,
-        nodes=nodes,
-    ) as cluster:
+    with Given("docker-compose cluster"):
+        cluster = create_cluster(
+            local=local,
+            clickhouse_binary_path=clickhouse_binary_path,
+            collect_service_logs=collect_service_logs,
+            nodes=nodes,
+            configs_dir=current_dir(),
+        )
         self.context.cluster = cluster
 
-        Scenario(
-            run=load("ldap.authentication.tests.sanity", "scenario"), name="ldap sanity"
-        )
-        Feature(run=load("ldap.role_mapping.tests.server_config", "feature"))
-        Feature(run=load("ldap.role_mapping.tests.mapping", "feature"))
-        Feature(run=load("ldap.role_mapping.tests.user_dn_detection", "feature"))
-        Feature(run=load("ldap.role_mapping.tests.cluster_secret", "feature"))
+    Scenario(
+        run=load("ldap.authentication.tests.sanity", "scenario"), name="ldap sanity"
+    )
+    Feature(run=load("ldap.role_mapping.tests.server_config", "feature"))
+    Feature(run=load("ldap.role_mapping.tests.mapping", "feature"))
+    Feature(run=load("ldap.role_mapping.tests.user_dn_detection", "feature"))
+    Feature(run=load("ldap.role_mapping.tests.cluster_secret", "feature"))
 
 
 if main():
