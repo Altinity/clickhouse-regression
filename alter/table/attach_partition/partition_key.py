60c8b8ee8f6460982084092b48650e6431f575fe
alsu <alsugilyazova1@mail.ru>
2024-01-02 16:48:47 +0100

    update attach partition from partition keys

--- alter/table/attach_partition/partition_key.py
+++ alter/table/attach_partition/partition_key.py
@@ -12,133 +12,430 @@ from alter.table.replace_partition.engines import (
 )
 
 
-def columns():
-    columns = [
-        Column(name="a", datatype=UInt16()),
-        Column(name="b", datatype=UInt16()),
-        Column(name="i", datatype=UInt64()),
-    ]
-    return columns
-
-
 def valid_partition_key_pair(source_partition_key, destination_partition_key):
-    subset = {
-        "a": ["tuple()"],
-        "b": ["tuple()"],
-        "(a,b)": ["a", "b", "tuple()", "(a,b)"],
-        "(b,a)": ["a", "b", "tuple()", "(a,b)"],
-        "(a,b,c)": ["a", "b", "(a,b)", "(b,a)", "tuple()", "(a,c,b)"],
-        "(a,c,b)": ["a", "b", "(a,b)", "(b,a)", "tuple()", "(a,b,c)"],
+
+    """Validates if pair source partition key - destination partition key is valid for `attach partition from` statement."""
+    different = {
+        "(intDiv(a,2),intDiv(b,2))": [
+            "(a%2,b%2)",
+            "(b%2,a%2)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(intDiv(b,2),intDiv(a,2))": [
+            "(a%2,b%2)",
+            "(b%2,a%2)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
     }
     not_subset = {
         "tuple()": [
             "a",
-            "(a%2)",
+            "a%2",
+            "a%3",
             "intDiv(a,2)",
+            "intDiv(a,3)",
             "b",
-            "(b%2)",
+            "b%2",
             "intDiv(b,2)",
             "(a,b)",
-            "(b,a)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
             "(a%2,b%2)",
-            "(b%2,a%2)",
             "(intDiv(a,2),intDiv(b,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
             "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
             "(a,b,c)",
             "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
         ],
         "a": [
             "b",
-            "(b%2)",
+            "b%2",
             "intDiv(b,2)",
             "(a,b)",
-            "(b,a)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
             "(a%2,b%2)",
-            "(b%2,a%2)",
             "(intDiv(a,2),intDiv(b,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
             "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
             "(a,b,c)",
             "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
         ],
         "a%2": [
             "b",
-            "(b%2)",
+            "b%2",
             "intDiv(b,2)",
             "(a,b)",
-            "(b,a)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
             "(a%2,b%2)",
-            "(b%2,a%2)",
             "(intDiv(a,2),intDiv(b,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
             "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
             "(a,b,c)",
             "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "a%3": [
+            "b",
+            "b%2",
+            "intDiv(b,2)",
+            "(a,b)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
+            "(a%2,b%2)",
+            "(intDiv(a,2),intDiv(b,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
+            "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
         ],
         "intDiv(a,2)": [
             "b",
-            "(b%2)",
+            "b%2",
             "intDiv(b,2)",
             "(a,b)",
-            "(b,a)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
             "(a%2,b%2)",
-            "(b%2,a%2)",
             "(intDiv(a,2),intDiv(b,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
             "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
             "(a,b,c)",
             "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "intDiv(a,3)": [
+            "b",
+            "b%2",
+            "intDiv(b,2)",
+            "(a,b)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
+            "(a%2,b%2)",
+            "(intDiv(a,2),intDiv(b,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
+            "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
         ],
         "b": [
             "a",
-            "(a%2)",
+            "a%2",
+            "a%3",
             "intDiv(a,2)",
+            "intDiv(a,3)",
             "(a,b)",
-            "(b, a)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
             "(a%2,b%2)",
-            "(b%2,a%2)",
             "(intDiv(a,2),intDiv(b,2))",
-            "(intDiv(b, 2),intDiv(a,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
+            "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
             "(a,b,c)",
             "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
         ],
         "b%2": [
             "a",
-            "(a%2)",
+            "a%2",
+            "a%3",
             "intDiv(a,2)",
+            "intDiv(a,3)",
             "(a,b)",
-            "(b,a)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
             "(a%2,b%2)",
-            "(b%2,a%2)",
             "(intDiv(a,2),intDiv(b,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
             "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
             "(a,b,c)",
             "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
         ],
         "intDiv(b,2)": [
             "a",
-            "(a%2)",
+            "a%2",
+            "a%3",
             "intDiv(a,2)",
+            "intDiv(a,3)",
             "(a,b)",
-            "(b,a)",
+            "(a,b%2)",
+            "(intDiv(a,2), b)",
             "(a%2,b%2)",
-            "(b%2,a%2)",
             "(intDiv(a,2),intDiv(b,2))",
+            "(a,intDiv(b,2))",
+            "(b,a)",
+            "(b%2,a%2)",
             "(intDiv(b,2),intDiv(a,2))",
+            "(b,c)",
+            "(a,c)",
             "(a,b,c)",
             "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
         ],
-        "(a,b)": ["(a,b,c)", "(a,c,b)"],
-        "(a%2,b%2)": ["(a,b,c)", "(a,c,b)"],
-        "(intDiv(a, 2),intDiv(b,2))": ["(a,b,c)", "(a,c,b)"],
-        "(b,a)": ["(a,b,c)", "(a,c,b)"],
-        "(b%2,a%2)": ["(a,b,c)", "(a,c,b)"],
-        "(intDiv(b,2),intDiv(a,2))": ["(a,b,c)", "(a,c,b)"],
+        "(a,b)": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(a,b%2)": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(intDiv(a,2), b)": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(a%2,b%2)": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(intDiv(a,2),intDiv(b,2))": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(a,intDiv(b,2))": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(b,a)": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(b%2,a%2)": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
+        "(intDiv(b,2),intDiv(a,2))": [
+            "(b,c)",
+            "(a,c)",
+            "(a,b,c)",
+            "(a,c,b)",
+            "(b,a,c)",
+            "(b,c,a)",
+            "(c,a,b)",
+            "(c,b,a)",
+        ],
     }
+
     not_monotonic = {
-        "a": ["a%2"],
-        "intDiv(a,2)": ["a%2"],
+        "a": ["a%2", "a%3"],
+        "a%2": ["a%3"],
+        "a%3": ["a%2"],
+        "intDiv(a,2)": ["a%2", "a%3"],
+        "intDiv(a,3)": ["a%2", "a%3"],
         "b": ["b%2"],
         "intDiv(b,2)": ["b%2"],
-        "(a,b)": ["(a%2,b%2)"],
-        "(b,a)": ["(b%2,a%2)"],
+        "(a,b)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(a,b%2)": [
+            "a%2",
+            "a%3",
+            "(a%2,b%2)",
+            "(b%2,a%2)",
+        ],
+        "(intDiv(a,2), b)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(a%2,b%2)": ["a%3", "(a,b%2)"],
+        "(intDiv(a,2),intDiv(b,2))": [
+            "a%2",
+            "a%3",
+            "b%2",
+            "(a,b%2)",
+            "(a%2,b%2)",
+            "(b%2,a%2)",
+        ],
+        "(a,intDiv(b,2))": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(b,a)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(b%2,a%2)": ["a%3", "(a,b%2)"],
+        "(b,c)": ["b%2"],
+        "(a,c)": ["b%2"],
+        "(intDiv(b,2),intDiv(a,2))": [
+            "a%2",
+            "a%3",
+            "b%2",
+            "(a,b%2)",
+            "(a%2,b%2)",
+            "(b%2,a%2)",
+        ],
+        "(a,b,c)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(a,c,b)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(b,a,c)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(b,c,a)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(c,a,b)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+        "(c,b,a)": ["a%2", "a%3", "b%2", "(a,b%2)", "(a%2,b%2)", "(b%2,a%2)"],
+    }
+
+    partially_different = {
+        "a%2": ["a", "intDiv(a,2)", "intDiv(a,3)"],
+        "a%3": ["a", "intDiv(a,3)"],
+        "intDiv(a,2)": ["a", "intDiv(a,3)"],
+        "intDiv(a,3)": ["a", "intDiv(a,2)"],
+        "b%2": ["b", "intDiv(b,2)"],
+        "intDiv(b,2)": ["b"],
+        "(intDiv(a,2),intDiv(b,2))": [
+            "a",
+            "b",
+            "(a,b)",
+            "(b,a)",
+            "intDiv(a,3)",
+            "(a,intDiv(b,2))",
+            "(intDiv(a,2), b)",
+        ],
+        "(a%2,b%2)": [
+            "a",
+            "b",
+            "intDiv(a,2)",
+            "intDiv(a,3)",
+            "intDiv(b,2)",
+            "(a,b)",
+            "(b,a)",
+            "(intDiv(a,2),intDiv(b,2))",
+            "(intDiv(b,2),intDiv(a,2))",
+            "(a,intDiv(b,2))",
+            "(intDiv(a,2), b)",
+        ],
+        "(intDiv(b,2),intDiv(a,2))": [
+            "a",
+            "b",
+            "(a,b)",
+            "(b,a)",
+            "intDiv(a,3)",
+            "(a,intDiv(b,2))",
+            "(intDiv(a,2), b)",
+        ],
+        "(b%2,a%2)": [
+            "a",
+            "b",
+            "intDiv(a,2)",
+            "intDiv(a,3)",
+            "intDiv(b,2)",
+            "(a,b)",
+            "(b,a)",
+            "(intDiv(a,2),intDiv(b,2))",
+            "(intDiv(b,2),intDiv(a,2))",
+            "(a,intDiv(b,2))",
+            "(intDiv(a,2), b)",
+        ],
     }
 
     if destination_partition_key in not_monotonic.get(source_partition_key, ""):
@@ -146,6 +443,12 @@ def valid_partition_key_pair(source_partition_key, destination_partition_key):
 
     if destination_partition_key in not_subset.get(source_partition_key, ""):
         return False, "not subset"
+
+    if destination_partition_key in different.get(source_partition_key, ""):
+        return False, "different"
+
+    if destination_partition_key in partially_different.get(source_partition_key, ""):
+        return False, "partially different"
 
     return True, ""
 
@@ -190,9 +493,11 @@ def check_attach_partition_from(
         )
 
     with And("I attach partition from source table into destination table"):
-        partition_ids = node.query(
-            f"SELECT partition_id FROM system.parts WHERE table='{source_table_name}'"
-        ).output.split()
+        partition_ids = set(
+            node.query(
+                f"SELECT partition_id FROM system.parts WHERE table='{source_table_name}'"
+            ).output.split()
+        )
         valid, reason = valid_partition_key_pair(
             source_partition_key, destination_partition_key
         )
@@ -211,56 +516,69 @@ def check_attach_partition_from(
 
                 assert source_partition_data == destination_partition_data
         else:
+            exitcode = None
+            message = None
+
             if reason == "not monotonic":
-                for partition_id in partition_ids:
-                    query = f"ALTER TABLE {destination_table_name} ATTACH PARTITION ID '{partition_id}' FROM {source_table_name}"
-                    node.query(
-                        query,
-                        exitcode=36,
-                        message="DB::Exception: Destination table partition expression is not monotonically increasing.",
-                    )
+                exitcode = 36
+                message = "DB::Exception: Destination table partition expression is not monotonically increasing."
             elif reason == "not subset":
-                for partition_id in partition_ids:
-                    query = f"ALTER TABLE {destination_table_name} ATTACH PARTITION ID '{partition_id}' FROM {source_table_name}"
-                    node.query(
-                        query,
-                        exitcode=36,
-                        message="DB::Exception: Destination table partition expression columns must be a subset of source table partition expression columns.",
-                    )
+                exitcode = 36
+                message = "DB::Exception: Destination table partition expression columns must be a subset of source table partition expression columns."
+            elif reason == "different":
+                exitcode = 248
+                message = "DB::Exception: Can not create the partition. A partition can not contain values that have different partition ids."
+            elif reason == "partially different":
+                skip("to do")
+
+            for partition_id in partition_ids:
+                query = f"ALTER TABLE {destination_table_name} ATTACH PARTITION ID '{partition_id}' FROM {source_table_name}"
+                node.query(
+                    query,
+                    exitcode=exitcode,
+                    message=message,
+                )
 
 
 @TestSketch(Scenario)
 @Flags(TE)
 def attach_partition_from(self):
-    """Run test check with different table engines to see if replace partition is possible."""
-    values = {
-        create_empty_partitioned_table,
-        create_partitioned_table_with_data,
-    }
+    """Run test check with different partition keys for both source and destination tables to see if `attach partition from` is possible."""
 
     partition_keys = {
         "tuple()",
         "a",
         "a%2",
+        "a%3",
         "intDiv(a,2)",
+        "intDiv(a,3)",
         "b",
         "b%2",
         "intDiv(b,2)",
         "(a,b)",
+        "(b,c)",
+        "(a,c)",
         "(a%2,b%2)",
+        "(a,intDiv(b,2))",
+        "(a,b%2)",
+        "(intDiv(a,2), b)",
         "(intDiv(a,2),intDiv(b,2))",
         "(b,a)",
         "(b%2,a%2)",
         "(intDiv(b,2),intDiv(a,2))",
         "(a,b,c)",
         "(a,c,b)",
+        "(b,a,c)",
+        "(b,c,a)",
+        "(c,a,b)",
+        "(c,b,a)",
     }
 
     engines = {
         "MergeTree",
-        #     "ReplacingMergeTree",
-        #     "AggregatingMergeTree",
-        #     "SummingMergeTree",
+        # "ReplacingMergeTree",
+        # "AggregatingMergeTree",
+        # "SummingMergeTree",
         #     # "CollapsingMergeTree",
         #     # "VersionedCollapsingMergeTree",
         #     # "GraphiteMergeTree",
