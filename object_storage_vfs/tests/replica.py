f76538f326548eb945de84a2526189de5d62272b
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-01-12 22:14:10 -0500

    VFS add/remove replica test

--- object_storage_vfs/tests/replica.py
+++ object_storage_vfs/tests/replica.py
@@ -141,6 +141,137 @@ def drop_replica(self):
         assert_row_count(node=nodes[1], table_name=table_name, rows=1000000)
 
 
+@TestScenario
+@Requirements(RQ_SRS_038_DiskObjectStorageVFS_Replica_Remove("1.0"))
+def parallel_add_remove(self):
+    table_name = "vfs_add_remove_replicas"
+    nodes = self.context.ch_nodes
+    rows_per_insert = 100_000_000
+    retry_settings = {
+        "timeout": 120,
+        "initial_delay": 5,
+        "delay": 2,
+    }
+
+    if self.context.stress:
+        rows_per_insert = 500_000_000
+        retry_settings["timeout"] = 300
+        retry_settings["delay"] = 5
+
+    with Given("I enable vfs"):
+        enable_vfs()
+
+    try:
+        with Given("I have a replicated table on one node"):
+            create_one_replica(node=nodes[0], table_name=table_name)
+
+        When(
+            "I start parallel inserts on the first node",
+            test=insert_random,
+            parallel=True,
+        )(
+            node=nodes[0],
+            table_name=table_name,
+            columns="d UInt64",
+            rows=rows_per_insert,
+        )
+
+        And(
+            "I replicate the table on the second node in parallel",
+            test=create_one_replica,
+            parallel=True,
+        )(node=nodes[1], table_name=table_name)
+
+        join()
+
+        with Then("I wait for the second node to sync by watching the row count"):
+            retry(assert_row_count, **retry_settings)(
+                node=nodes[1], table_name=table_name, rows=rows_per_insert
+            )
+
+        And(
+            "I start parallel inserts on the second node",
+            test=insert_random,
+            parallel=True,
+        )(
+            node=nodes[1],
+            table_name=table_name,
+            columns="d UInt64",
+            rows=rows_per_insert,
+        )
+
+        And(
+            "I delete the replica on the first node",
+            test=delete_one_replica,
+            parallel=True,
+        )(node=nodes[0], table_name=table_name)
+
+        And(
+            "I replicate the table on the third node in parallel",
+            test=create_one_replica,
+            parallel=True,
+        )(node=nodes[2], table_name=table_name)
+
+        join()
+
+        with And("I wait for the third node to sync by watching the row count"):
+            retry(assert_row_count, **retry_settings)(
+                node=nodes[2], table_name=table_name, rows=rows_per_insert * 2
+            )
+
+        with Then("I also check the row count on the second node"):
+            assert_row_count(
+                node=nodes[1], table_name=table_name, rows=rows_per_insert * 2
+            )
+
+        Given(
+            "I start parallel inserts on the second node in parallel",
+            test=insert_random,
+            parallel=True,
+        )(
+            node=nodes[1],
+            table_name=table_name,
+            columns="d UInt64",
+            rows=rows_per_insert,
+        )
+        And(
+            "I start parallel inserts on the third node in parallel",
+            test=insert_random,
+            parallel=True,
+        )(
+            node=nodes[2],
+            table_name=table_name,
+            columns="d UInt64",
+            rows=rows_per_insert,
+        )
+
+        And(
+            "I replicate the table on the first node again in parallel",
+            test=create_one_replica,
+            parallel=True,
+        )(node=nodes[0], table_name=table_name)
+
+        join()
+
+        with Then("I wait for the first node to sync by watching the row count"):
+            retry(assert_row_count, **retry_settings)(
+                node=nodes[0], table_name=table_name, rows=rows_per_insert * 4
+            )
+
+        with And("I check the row count on the other nodes"):
+            assert_row_count(
+                node=nodes[1], table_name=table_name, rows=rows_per_insert * 4
+            )
+            assert_row_count(
+                node=nodes[2], table_name=table_name, rows=rows_per_insert * 4
+            )
+
+    finally:
+        with Finally("I drop the table on each node"):
+            for node in nodes:
+                node.query(f"DROP TABLE IF EXISTS {table_name} SYNC")
+
+
 @TestFeature
 @Name("replica")
 def feature(self):
