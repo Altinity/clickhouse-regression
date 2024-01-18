f29834e41af37e7be18751cfa77a2e746492dc7c
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-01-17 20:40:09 -0500

    Refactor vfs random replica test

--- object_storage_vfs/tests/replica.py
+++ object_storage_vfs/tests/replica.py
@@ -4,6 +4,7 @@ import random
 import json
 
 from testflows.core import *
+from testflows.combinatorics import product, combinations
 
 from object_storage_vfs.tests.steps import *
 from object_storage_vfs.requirements import *
@@ -273,41 +274,85 @@ def parallel_add_remove(self):
                 node.query(f"DROP TABLE IF EXISTS {table_name} SYNC")
 
 
-@TestScenario
+@TestOutline(Scenario)
 @Requirements(RQ_SRS_038_DiskObjectStorageVFS_Replica_Remove("1.0"))
-def random_add_remove(self, allow_vfs=True):
-    """
-    Randomly perform actions on replicas and check that they all agree.
-    """
-
+def add_remove_replicas(self, allow_vfs=True):
     table_name = "vfs_random_add_remove_replicas"
     nodes = self.context.ch_nodes
     rows_per_insert = 1_000_000
-    random.seed(table_name)
 
     loop_sleep = 10
     n_loops = 10
+    n_parallel = 5
 
-    actions = {
-        "CREATE": lambda _, node: create_one_replica(
+    @TestStep(When)
+    def add_table_replica(self, node):
+        create_one_replica(
             node=node, table_name=table_name, replica_name=getuid(), no_checks=True
-        ),
-        "DELETE": lambda _, node: delete_one_replica(node=node, table_name=table_name),
-        "INSERT": lambda _, node: insert_random(
+        )
+
+    @TestStep(When)
+    def remove_replica(self, node):
+        delete_one_replica(node=node, table_name=table_name)
+
+    @TestStep(When)
+    def insert_data(self, node):
+        insert_random(
             node=node,
             table_name=table_name,
             columns="d UInt64",
             rows=rows_per_insert,
             no_checks=True,
-        ),
-        "OPTIMIZE": lambda _, node: node.query(
-            f"OPTIMIZE TABLE {table_name}", no_checks=True
-        ),
-    }
-    for a, f in actions.items():
-        actions[a] = TestStep(When)(f)
+        )
 
-    action_items = list(actions.items())
+    @TestStep(When)
+    def optimize_table(self, node):
+        node.query(f"OPTIMIZE TABLE {table_name}", no_checks=True)
+
+    @TestStep(When)
+    def select_count(self, node):
+        r = node.query(f"SELECT count() FROM {table_name}", no_checks=True)
+
+    @TestStep(When)
+    def get_row_count(self, node):
+        r = node.query(
+            f"SELECT count() FROM {table_name} FORMAT JSON",
+            exitcode=0,
+        )
+        return int(json.loads(r.output)["data"][0]["count()"])
+
+    @TestStep(Then)
+    def check_consistency(self):
+        with When("I check which nodes have the table"):
+            active_nodes = [
+                n for n in nodes if table_name in n.query("SHOW TABLES").output
+            ]
+            if not active_nodes: return
+
+        with When("I make sure all nodes are synced"):
+            for node in active_nodes:
+                node.query(
+                    f"SYSTEM SYNC REPLICA {table_name}", timeout=60, no_checks=True
+                )
+
+        with And("I query all nodes for their row counts"):
+            row_counts = {}
+            for node in active_nodes:
+                row_counts[node.name] = get_row_count(node=node)
+
+        with Then("All replicas should have the same state"):
+            for n1, n2 in combinations(active_nodes, 2):
+                assert row_counts[n1.name] == row_counts[n2.name], error()
+
+    actions = [
+        add_table_replica,
+        insert_data,
+        optimize_table,
+        select_count,
+        remove_replica,
+    ]
+
+    action_pairs = list(product(nodes, actions))
 
     try:
         if allow_vfs:
@@ -315,22 +360,26 @@ def random_add_remove(self, allow_vfs=True):
                 enable_vfs()
 
         for _ in range(n_loops):
-            for node in nodes:
-                action_name, action_func = random.choice(action_items)
+            action_node, action_func = random.choice(action_pairs)
 
+            for _ in range(n_parallel):
                 When(
-                    f"I {action_name} on {node.name}",
+                    f"I {action_func.name} on {action_node.name}",
                     test=action_func,
                     parallel=True,
-                )(node=node)
-            time.sleep(loop_sleep)
+                )(node=action_node)
+
+            join()
+
+            with Then("I check that the replicas are consistent"):
+                check_consistency()
 
         with When("I wait for all tasks to finish"):
             join()
 
         with And("I make sure all nodes are replicating"):
             for node in nodes:
-                actions["CREATE"](node=node)
+                add_table_replica(node=node)
 
         with When("I make sure all nodes are synced"):
             for node in nodes:
@@ -341,17 +390,7 @@ def random_add_remove(self, allow_vfs=True):
         with And("I query all nodes for their row counts"):
             row_counts = []
             for node in nodes:
-                r = node.query(
-                    f"SELECT count() FROM {table_name} FORMAT JSON",
-                    exitcode=0,
-                )
-                row_counts.append(int(json.loads(r.output)["data"][0]["count()"]))
-
-        with Then("All replicas should have the same state"):
-            assert row_counts[0] == row_counts[1] == row_counts[2], error()
-
-        with Then("There should be more than zero rows"):
-            assert row_counts[0] > 0, error()
+                row_counts.append(get_row_count(node=node))
 
     finally:
         with Finally("I drop the table on each node"):
@@ -360,18 +399,31 @@ def random_add_remove(self, allow_vfs=True):
 
 
 @TestScenario
+def random_add_remove(self):
+    """
+    Randomly perform actions on replicas and check that they all agree.
+    """
+    add_remove_replicas(allow_vfs=True)
+
+
+@TestScenario
 def random_add_remove_no_vfs(self):
     """
     To isolate issues, run the same test without vfs
     """
-    random_add_remove(allow_vfs=False)
+    add_remove_replicas(allow_vfs=False)
 
 
 @TestFeature
 @Name("replica")
 def feature(self):
+    # Use the same seed for all tests, but a different seed each run
+    random_seed = random.random()
+    note(f"Using seed {random_seed}")
+
     with Given("I have S3 disks configured"):
         s3_config()
 
     for scenario in loads(current_module(), Scenario):
+        random.seed(random_seed)
         scenario()
