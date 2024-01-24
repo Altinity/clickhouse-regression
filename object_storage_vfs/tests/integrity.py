492def86e21792d07b8d21b0ebef3a01f637a386
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-01-24 11:32:10 -0500

    Add vfs migration test

--- object_storage_vfs/tests/integrity.py
+++ object_storage_vfs/tests/integrity.py
@@ -2,6 +2,7 @@
 import time, datetime
 
 from testflows.core import *
+from testflows.combinatorics import permutations
 
 from object_storage_vfs.tests.steps import *
 from object_storage_vfs.requirements import *
@@ -336,6 +337,50 @@ def ttl_move(self):
         )
 
 
+@TestOutline(Scenario)
+@Requirements(RQ_SRS_038_DiskObjectStorageVFS_Integrity_Migration("1.0"))
+@Examples("source destination", permutations(["replicated", "zero-copy", "vfs"], 2))
+def migration(self, source, destination):
+    node = self.context.node
+
+    with Given("I have a replicated table"):
+        _, replicated_table_name = replicated_table_cluster(
+            storage_policy="external_no_vfs"
+        )
+
+    with And("I have a zero-copy table"):
+        _, zero_copy_table_name = replicated_table_cluster(
+            storage_policy="external_no_vfs", allow_zero_copy=True
+        )
+
+    with And("I have a vfs table"):
+        _, vfs_table_name = replicated_table_cluster(storage_policy="external_vfs")
+
+    table_names = {
+        "replicated": replicated_table_name,
+        "zero-copy": zero_copy_table_name,
+        "vfs": vfs_table_name,
+    }
+
+    with And(f"I select {source} as the source table"):
+        source_table_name = table_names[source]
+
+    with And(f"I select {destination} as the destination table"):
+        dest_table_name = table_names[destination]
+
+    with And("I insert data to the source table"):
+        insert_random(node=node, table_name=source_table_name, rows=1000000)
+
+    with When("I copy the source table to the destination table"):
+        node.query(f"INSERT INTO {dest_table_name} SELECT * from {source_table_name}")
+
+    with And("I delete the source table"):
+        node.query(f"DROP TABLE {source_table_name}")
+
+    with Then("the data should be in the destination table"):
+        assert_row_count(node=node, table_name=dest_table_name, rows=1000000)
+
+
 @TestScenario
 @Requirements(
     RQ_SRS_038_DiskObjectStorageVFS_Integrity_ConnectionInterruption_FaultInjection(
@@ -380,7 +425,6 @@ def fault_injection(self):
         )
 
 
-# RQ_SRS_038_DiskObjectStorageVFS_Integrity_Migration
 # RQ_SRS_038_DiskObjectStorageVFS_Integrity_ConnectionInterruption
 
 
