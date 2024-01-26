7b65474c00e71358123c82a9042284eac0ea9ef7
alsu <alsugilyazova1@mail.ru>
2024-01-26 22:44:31 +0100

    add attach part support to corrupted parts test

--- alter/table/attach_partition/corrupted_partitions.py
+++ alter/table/attach_partition/corrupted_partitions.py
@@ -7,14 +7,18 @@ from helpers.common import (
     attach_partition,
     detach_partition,
     attach_partition_from,
+    attach_part,
+    detach_part,
 )
 from helpers.tables import create_table_partitioned_by_column
 
 one_part = ["1_1_1_0"]
+second_part = ["1_2_2_0"]
 multiple_parts = ["1_1_1_0", "1_2_2_0"]
 all_parts = ["1_1_1_0", "1_2_2_0", "1_3_3_0"]
 
 after_attach = {"1_1_1_0": "1_4_4_0", "1_2_2_0": "1_5_5_0", "1_3_3_0": "1_6_6_0"}
+after_attach_detached_part = {"1_1_1_0": "1_4_4_0", "1_2_2_0": "1_2_2_0"}
 
 
 @TestStep(When)
@@ -196,10 +200,10 @@ def check_attach_partition_detached_with_corrupted_parts(self, corrupt):
             f"SELECT partition, part_type, name, active, rows FROM system.parts WHERE table = '{table}' ORDER BY tuple(*)"
         )
 
-    with When("I change some bit values of the part on one of or both tables"):
+    with When("I change some bit values of the partition"):
         corrupt(table_name=table)
 
-    with Then("I attach partition from the detached folder"):
+    with Then("I attach part from the detached folder"):
         parts_before_attach = node.query(
             f"SELECT partition, part_type, name, active, rows FROM system.parts WHERE table = '{table}' ORDER BY tuple(*)"
         )
@@ -239,7 +243,7 @@ def check_attach_partition_detached_with_corrupted_parts(self, corrupt):
     with And(
         "I check that data was attached to the destination table",
         description="this allows us to validate that the partitions were attached by validating that the inside the "
-        "system.parts table the data for destination table was updated.",
+        "system.parts table the data for the table was updated.",
     ):
 
         assert parts_before_attach.output.strip() != parts_after_attach.output.strip()
@@ -255,7 +259,105 @@ def check_attach_partition_detached_with_corrupted_parts(self, corrupt):
                     f"ALTER TABLE {table} DETACH PART '{after_attach_detached[part]}'"
                 )
 
-        for retry in retries(timeout=1):
+        for retry in retries(timeout=10):
+            with retry:
+                node.query(
+                    f"SELECT * FROM {table}",
+                )
+
+
+@TestCheck
+def check_attach_corrupted_part(self, corrupt):
+    """Check attach part from detached folder when parts are corrupted."""
+    node = self.context.node
+    table = getuid()
+
+    self.context.parts = []
+
+    with Given(
+        "I create table",
+        description=f"""
+            table with: {corrupt.__name__}    
+        """,
+    ):
+        create_table_partitioned_by_column(
+            table_name=table,
+        )
+
+    with And(
+        "I populate the table with data to create a partition with a set number of parts",
+        description="""this will create a partition with three parts [
+            "1_1_1_0",
+            "1_2_2_0",
+            "1_3_3_0",
+        ]""",
+    ):
+        partition = 1
+        for i in range(3):
+            node.query(
+                f"INSERT INTO {table} (p, i) SELECT {partition}, {i} FROM numbers({i+1});"
+            )
+
+    node.query(
+        f"SELECT partition, part_type, name, active, rows FROM system.parts WHERE table = '{table}' ORDER BY tuple(*)"
+    )
+    with And("I detach part from table"):
+        detach_part(table=table, part="1_1_1_0")
+        node.query(
+            f"SELECT * FROM {table}",
+        )
+        node.query(
+            f"SELECT partition, part_type, name, active, rows FROM system.parts WHERE table = '{table}' ORDER BY tuple(*)"
+        )
+
+    with When("I change some bit values of the parts"):
+        corrupt(table_name=table)
+
+    with Then("I attach part from the detached folder"):
+        parts_before_attach = node.query(
+            f"SELECT partition, part_type, name, active, rows FROM system.parts WHERE table = '{table}' ORDER BY tuple(*)"
+        )
+
+        attach_part(
+            table=table,
+            part="1_1_1_0",
+        )
+
+        parts_after_attach = node.query(
+            f"SELECT partition, part_type, name, active, rows FROM system.parts WHERE table = '{table}' ORDER BY tuple(*)"
+        )
+
+    with And("I try to read data from the table"):
+        corrupt_type = corrupt.__name__
+
+        if corrupt_type == "corrupt_no_parts":
+            message = None
+        else:
+            message = "DB::Exception:"
+
+        node.query(
+            f"SELECT * FROM {table}",
+            message=message,
+        )
+
+    with And(
+        "I check that data was attached to the destination table",
+        description="this allows us to validate that the part was attached by validating that the inside the "
+        "system.parts table the data for the table was updated.",
+    ):
+
+        assert parts_before_attach.output.strip() != parts_after_attach.output.strip()
+
+    with And(
+        "I detach all the corrupted parts and check that it is possible to read data from the table without any errors",
+    ):
+        if corrupt_type != "corrupt_no_parts":
+            for part in self.context.destination_parts:
+                node.query(
+                    f"ALTER TABLE {table} DETACH PART '{after_attach_detached_part[part]}'"
+                )
+
+        for retry in retries(timeout=10, delay=2):
             with retry:
                 node.query(
                     f"SELECT * FROM {table}",
@@ -279,6 +381,23 @@ def corrupt_one_part_detached(self, table_name):
     self.context.destination_parts = one_part
 
     corrupt_parts_on_table_partition_detached(table_name=table_name, parts=one_part)
+
+
+@TestStep(When)
+def corrupt_second_part(self, table_name):
+    """Corrupt a single part of the partition."""
+    self.context.destination_parts = second_part
+
+    corrupt_parts_on_table_partition(table_name=table_name, parts=second_part)
+
+
+@TestStep(When)
+def corrupt_two_parts(self, table_name):
+    """Corrupt a single part of the partition."""
+    self.context.destination_parts = ["1_1_1_0", "1_2_2_0"]
+
+    corrupt_parts_on_table_partition_detached(table_name=table_name, parts=["1_1_1_0"])
+    corrupt_parts_on_table_partition(table_name=table_name, parts=["1_2_2_0"])
 
 
 @TestStep(When)
@@ -365,6 +484,30 @@ def attach_partition_detached_with_corrupted_parts(self):
     )
 
 
+@TestSketch(Scenario)
+@Flags(TE)
+def attach_corrupted_part(self):
+    """
+    Check attach partition with different amounts of parts being corrupted.
+
+    Combinations:
+    Attach non-corrupted to non-corrupted table
+    Attach corrupted part to non-corrupted table
+    Attach non-corrupted part to corrupted table
+    Attach corrupted part to corrupted table
+    """
+    values = {
+        corrupt_no_parts,
+        corrupt_one_part_detached,
+        corrupt_second_part,
+        corrupt_two_parts,
+    }
+
+    check_attach_corrupted_part(
+        corrupt=either(*values),
+    )
+
+
 @TestFeature
 @Requirements(RQ_SRS_034_ClickHouse_Alter_Table_AttachPartition_CorruptedParts("1.0"))
 @Name("corrupted partitions")
@@ -382,3 +525,4 @@ def feature(self, node="clickhouse1"):
 
     Scenario(run=attach_partition_from_with_corrupted_parts)
     Scenario(run=attach_partition_detached_with_corrupted_parts)
+    Scenario(run=attach_corrupted_part)
