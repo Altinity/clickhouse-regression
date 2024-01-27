54fa510db7057ecf57aeb2c88eb617370705722d
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-01-26 21:49:31 -0500

    Add vfs support to s3 backup, disk, invalid disk

--- s3/tests/disk.py
+++ s3/tests/disk.py
@@ -42,11 +42,14 @@ def define_s3_disk_storage_configuration(
             },
         }
 
+        if self.context.object_storage_mode == "vfs":
+            disks[disk_name]["allow_vfs"] = "1"
+
         if hasattr(self.context, "s3_options"):
-            disks["external"].update(self.context.s3_options)
+            disks[disk_name].update(self.context.s3_options)
 
         if options:
-            disks["external"].update(options)
+            disks[disk_name].update(options)
 
         yield disks
 
