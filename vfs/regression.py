cf3f5ea079f3c5d2084c7ba3c67e55786c068213
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-02-14 11:44:09 -0500

    update vfs xfails

--- vfs/regression.py
+++ vfs/regression.py
@@ -17,9 +17,6 @@ xfails = {
     ":/replica/command combinations/*": [(Error, "some combos time out")],
     ":/parallel replica/add remove commands/*": [(Fail, "WIP"), (Error, "WIP")],
     ":/settings/disable vfs with vfs table/access:": [(Fail, "not supported")],
-    ":/system/optimize/table_settings='min_bytes_for_wide_part=:": [
-        (Fail, "needs investigation")
-    ],
 }
 
 ffails = {
@@ -28,9 +25,8 @@ ffails = {
         "vfs not supported on < 24",
         check_clickhouse_version("<24"),
     ),
-    ":/alter/move": (XFail, "Clickhouse crashes"),
+    ":/alter/move": (XFail, "Fix pending"),
     ":/replica/add remove one node": (XFail, "Fix pending"),
-    ":/settings/incompatible with send metadata": (XFail, "Clickhouse crashes"),
 }
 
 # RQ_SRS_038_DiskObjectStorageVFS_Providers_Configuration
