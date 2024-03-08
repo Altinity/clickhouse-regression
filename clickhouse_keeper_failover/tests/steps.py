dad2ac829bc2a869cc236404fa764a748b9103aa
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-03-08 14:07:02 -0500

    keeper failover: test step for swapping configs

--- clickhouse_keeper_failover/tests/steps.py
+++ clickhouse_keeper_failover/tests/steps.py
@@ -26,9 +26,25 @@ def get_external_ports(self, internal_port):
 @TestStep
 def get_current_leader(self):
     cluster = self.context.cluster
-    
+
     for node, port in self.context.keeper_ports.items():
         r = cluster.command(None, f"curl 'http://localhost:{port}/ready'")
         is_leader = json.loads(r.output)["details"]["role"] == "leader"
         if is_leader:
             return node
+
+
+@TestStep
+def set_keeper_config(self, nodes, config_file_name):
+    source_path = "/etc/clickhouse-keeper-configs/"
+    dest_file = "/etc/clickhouse-keeper/keeper_config.xml"
+
+    source_file = source_path + config_file_name
+
+    cmd = f"ln -s -f {source_file} {dest_file}"
+
+    for node in nodes:
+        node.command(
+            "echo '||||||INSERTING NEW CONFIG' >> /var/log/clickhouse-keeper/clickhouse-keeper.log"
+        )
+        node.command(cmd)
