1352311afc0b50767844dab9fac8a3bba8328dfd
alsu <alsugilyazova1@mail.ru>
2024-03-08 16:01:47 +0100

    fix writing snapshots

--- alter/table/attach_partition/common.py
+++ alter/table/attach_partition/common.py
@@ -1208,6 +1208,11 @@ def execute_query(
     snapshot_name=None,
     node=None,
     path=None,
+    format="JSONEachRow",
+    use_file=False,
+    hash_output=False,
+    timeout=None,
+    settings=None,
 ):
     """Execute SQL query and compare the output to the snapshot."""
     if snapshot_name is None:
@@ -1217,11 +1222,19 @@ def execute_query(
         node = current().context.node
 
     with When("I execute query", description=sql):
+        note(sql)
+        if format and not "FORMAT" in sql:
+            sql += " FORMAT " + format
+
         r = node.query(
             sql,
             exitcode=exitcode,
             message=message,
             no_checks=no_checks,
+            use_file=use_file,
+            hash_output=hash_output,
+            timeout=timeout,
+            settings=settings,
         )
         if no_checks:
             return r
@@ -1233,18 +1246,16 @@ def execute_query(
         else:
             with Then("I check output against snapshot"):
                 with values() as that:
-                    for attempt in retries(timeout=30, delay=5):
-                        with attempt:
-                            assert that(
-                                snapshot(
-                                    "\n" + r.output.strip() + "\n",
-                                    "tests." + current_cpu(),
-                                    name=snapshot_name,
-                                    path=path,
-                                    encoder=str,
-                                    mode=snapshot.CHECK,
-                                )
-                            ), error()
+                    assert that(
+                        snapshot(
+                            "\n" + r.output.strip() + "\n",
+                            "tests." + current_cpu(),
+                            name=snapshot_name,
+                            path=path,
+                            encoder=str,
+                            mode=snapshot.CHECK,  # snapshot.REWRITE | snapshot.CHECK | snapshot.UPDATE
+                        )
+                    ), error()
 
 
 # MergeTree
