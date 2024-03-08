a0c11f784f9395fb309eb52bff4b541894d09fae
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-03-08 14:07:40 -0500

    keeper failover: manual test WIP

--- clickhouse_keeper_failover/tests/manual_failover.py
+++ clickhouse_keeper_failover/tests/manual_failover.py
@@ -3,21 +3,28 @@ from testflows.core import *
 
 from clickhouse_keeper_failover.tests.steps import *
 
+
 @TestFeature
 @Name("manual failover")
 def feature(self):
     """Test keeper manual failover."""
 
-    with Given("I know the current leader"):
-        current_leader = retry(get_current_leader, timeout=10, delay=2)()
+    with Given("I check that the leader exists"):
+        current_leader = retry(
+            get_current_leader, timeout=10, delay=1, initial_delay=2
+        )()
 
-    with And("I split the nodes into ensembles for PR and DR"):
+    with Given("I split the nodes into ensembles for PR and DR"):
         pr_ensemble = self.context.keeper_nodes[:3]
         dr_ensemble = self.context.keeper_nodes[3:]
 
     with When("I stop the PR ensemble"):
         for node in pr_ensemble:
-            node.stop()
+            node.command("kill 1")
 
+    with Given("I enable leadership on DR ensemble"):
+        set_keeper_config(
+            nodes=dr_ensemble, config_file_name="keeper_config_manual_failover.xml"
+        )
 
-        
+    pause()
