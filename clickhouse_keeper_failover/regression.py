b36a93af5c2a72bbbba9abdd1bb9fff567871ccc
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-03-12 10:41:17 -0400

    keeper failover: use fresh cluster for each feature and small fixes

--- clickhouse_keeper_failover/regression.py
+++ clickhouse_keeper_failover/regression.py
@@ -16,6 +16,60 @@ xfails = {}
 
 ffails = {}
 
+@TestModule
+def run_feature(
+    self,
+    local,
+    clickhouse_binary_path,
+    collect_service_logs,
+    feature_file_name,
+):
+    nodes = {
+        "clickhouse": [],
+        "keeper": [f"keeper-{i}" for i in range(1, 7)],
+    }
+
+    try:
+        with Given("docker-compose cluster"):
+            cluster = create_cluster(
+                local=local,
+                clickhouse_binary_path=clickhouse_binary_path,
+                collect_service_logs=collect_service_logs,
+                nodes=nodes,
+                configs_dir=current_dir(),
+            )
+            self.context.cluster = cluster
+            self.context.keeper_nodes = [
+                cluster.node(n) for n in cluster.nodes["keeper"]
+            ]
+
+        with And("I set the keeper config file"):
+            set_keeper_config(
+                nodes=self.context.keeper_nodes,
+                config_file_name="keeper_config_6node.xml",
+                restart=True,
+            )
+
+        with And("I know which ports are in use"):
+            self.context.keeper_ports = get_external_ports(internal_port=9182)
+
+        with And("I wait for a leader to be elected"):
+            current_leader = retry(
+                get_current_leader, timeout=30, delay=5, initial_delay=10
+            )()
+
+        Feature(
+            run=load(f"clickhouse_keeper_failover.tests.{feature_file_name}", "feature")
+        )
+
+    finally:
+        with Finally("I reset the config"):
+            set_keeper_config(
+                nodes=self.context.keeper_nodes,
+                config_file_name="keeper_config_6node.xml",
+                restart=False,
+            )
+
 
 @TestModule
 @ArgumentParser(argparser)
@@ -32,10 +86,6 @@ def regression(
     allow_vfs=False,
 ):
     """Clickhouse-keeper failover testing."""
-    nodes = {
-        "clickhouse": [],
-        "keeper": [f"keeper-{i}" for i in range(1, 7)],
-    }
 
     self.context.clickhouse_version = clickhouse_version
 
@@ -45,42 +95,15 @@ def regression(
     if check_clickhouse_version("<21.4")(self):
         skip(reason="only supported on ClickHouse version >= 21.4")
 
-    with Given("docker-compose cluster"):
-        cluster = create_cluster(
+    features = ["manual_failover", "dynamic_reconfiguration_failover"]
+
+    for feature in features:
+        run_feature(
             local=local,
             clickhouse_binary_path=clickhouse_binary_path,
             collect_service_logs=collect_service_logs,
-            nodes=nodes,
-            configs_dir=current_dir(),
+            feature_file_name=feature,
         )
-        self.context.cluster = cluster
-        self.context.keeper_nodes = [cluster.node(n) for n in cluster.nodes["keeper"]]
-
-    with And("I set the keeper config file"):
-        set_keeper_config(
-            nodes=self.context.keeper_nodes,
-            config_file_name="keeper_config_6node.xml",
-            restart=True,
-        )
-
-    with And("I know which ports are in use"):
-        self.context.keeper_ports = get_external_ports(internal_port=9182)
-
-    try:
-        Feature(run=load("clickhouse_keeper_failover.tests.manual_failover", "feature"))
-        Feature(
-            run=load(
-                "clickhouse_keeper_failover.tests.dynamic_reconfiguration_failover",
-                "feature",
-            )
-        )
-    finally:
-        with Finally("I reset the config"):
-            set_keeper_config(
-                nodes=self.context.keeper_nodes,
-                config_file_name="keeper_config_6node.xml",
-                restart=False,
-            )
 
 
 if main():
