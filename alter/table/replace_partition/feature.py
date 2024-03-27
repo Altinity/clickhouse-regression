0642fbc0a9358670369e3633e0e39bb275bc33fb
alsu <alsugilyazova1@mail.ru>
2024-03-27 12:35:29 +0100

    add partition key test to replace partition suite

--- alter/table/replace_partition/feature.py
+++ alter/table/replace_partition/feature.py
@@ -86,4 +86,10 @@ def feature(self):
                 "feature",
             )
         )
+        Feature(
+            run=load(
+                "alter.table.replace_partition.partition_keys",
+                "feature",
+            )
+        )
         join()
