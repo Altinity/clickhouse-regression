e93f774130666b41b8ab7adb0e5ed63b9763b3a0
alsu <alsugilyazova1@mail.ru>
2024-03-27 14:20:24 +0100

    update alter xfails

--- alter/regression.py
+++ alter/regression.py
@@ -86,6 +86,9 @@ xfails = {
     "/alter/attach partition/partition key datetime/*": [
         (Fail, "Need to investigate", check_clickhouse_version("<=24.2"))
     ],
+    "/alter/replace partition/partition keys/replace partition partition key/*": [
+        (Fail, "Under discussion", check_clickhouse_version(">24.3"))
+    ],
     "/alter/attach partition/small test/*": [(Fail, "Should fail")],
 }
 
