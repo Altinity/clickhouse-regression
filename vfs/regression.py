8fd183b6b78972ec5ed5aa881364f3983c16ef94
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-03-29 09:31:31 -0400

    Enable ZookeeperNode in vfs tests

--- vfs/regression.py
+++ vfs/regression.py
@@ -57,6 +57,7 @@ def minio(
             clickhouse_binary_path=clickhouse_binary_path,
             collect_service_logs=collect_service_logs,
             nodes=nodes,
+            use_zookeeper_nodes=True,
             configs_dir=current_dir(),
             environ={
                 "MINIO_ROOT_PASSWORD": root_password,
@@ -143,6 +144,7 @@ def aws_s3(
             clickhouse_binary_path=clickhouse_binary_path,
             collect_service_logs=collect_service_logs,
             nodes=nodes,
+            use_zookeeper_nodes=True,
             configs_dir=current_dir(),
             environ={
                 "S3_AMAZON_ACCESS_KEY": access_key,
@@ -213,6 +215,7 @@ def gcs(
             clickhouse_binary_path=clickhouse_binary_path,
             collect_service_logs=collect_service_logs,
             nodes=nodes,
+            use_zookeeper_nodes=True,
             configs_dir=current_dir(),
             environ={"GCS_KEY_SECRET": access_key, "GCS_KEY_ID": key_id},
         )
