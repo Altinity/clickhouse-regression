4681d590bb1ea951f7bb95d526f7b15e86e8e0f7
alsu <alsugilyazova1@mail.ru>
2024-04-03 17:02:32 +0200

    small fix

--- alter/table/attach_partition/part_level.py
+++ alter/table/attach_partition/part_level.py
@@ -201,7 +201,7 @@ def part_levels_user_example(self):
 
     with And("I get part name and construct new part name with too high level"):
         part_names = node.query(
-            f"SELECT name FROM system.parts WHERE table = '{destination_table}'"
+            f"SELECT name FROM system.parts WHERE table = '{destination_table}' and active"
         ).output.split("\n")
         part_name = random.choice(part_names)
         new_part_name = "_".join(part_name.split("_")[:-1]) + "_4294967295"
@@ -377,6 +377,55 @@ def check_part_level_reset_replicated(self, engine):
                 f"SELECT name FROM system.parts WHERE table = '{source_table}' AND active"
             ).output
             assert part_name == "all_1_1_0", f"Unexpected part name: {part_name}"
+
+
+@TestScenario
+def test(self):
+    source_table = "source_" + getuid()
+    destination_table = "destination_" + getuid()
+
+    with Given("I have a table"):
+        create_MergeTree_table(table_name=source_table, order_by="id")
+
+    with And("I insert data into the table"):
+        self.context.node.query(f"INSERT INTO {source_table} (id) VALUES (1)")
+
+    with And("I optimize the table"):
+        optimize_table(table_name=source_table)
+
+    with And("I get part name"):
+        part_name = self.context.node.query(
+            f"SELECT name FROM system.parts WHERE table = '{source_table}'"
+        ).output
+
+    with And("I detach the part"):
+        detach_part(table_name=source_table, part_name=part_name)
+
+    with And("I rename the detached part"):
+        new_part_name = "_".join(part_name.split("_")[:-1]) + "_4294967294"
+        rename_detached_part(
+            table_name=source_table,
+            part_name=part_name,
+            new_part_name=new_part_name,
+        )
+
+    with And("I attach the part"):
+        attach_part(table_name=source_table, part_name=new_part_name)
+
+    with And("I create a destination table"):
+        create_MergeTree_table(table_name=destination_table, order_by="id")
+
+    with And("I attach a partition from the source table to the destination table"):
+        attach_partition_from(
+            source_table=source_table,
+            destination_table=destination_table,
+            partition="tuple()",
+        )
+
+    with And("I get part name"):
+        part_name = self.context.node.query(
+            f"SELECT name FROM system.parts WHERE table = '{destination_table}'"
+        ).output
 
 
 @TestScenario
