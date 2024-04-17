f73b984c9e9ad01415bac89afdc2ee24ecdc5550
alsu <alsugilyazova1@mail.ru>
2024-04-17 13:58:32 +0200

    add tests for approx_top_k aggregate function

---
+++ aggregate_functions/tests/approx_top_k.py
@@ -0,0 +1,34 @@
+from testflows.core import *
+from aggregate_functions.tests.steps import *
+
+from aggregate_functions.requirements import (
+    RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_Approx_Top_K,
+)
+
+from aggregate_functions.tests.topK import scenario as checks
+
+
+@TestScenario
+@Name("approx_top_k")
+@Requirements(RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_Approx_Top_K("1.0"))
+def scenario(self, func="approx_top_k({params})", table=None, snapshot_id=None):
+    """Check approx_top_k, aggregate function by using the same checks as for topK."""
+
+    self.context.snapshot_id = get_snapshot_id(
+        snapshot_id=snapshot_id,
+    )
+
+    if table is None:
+        table = self.context.table
+
+    params = "({params})"
+
+    _func = func.replace(params, f"(3){params}")
+    if "Merge" in self.name:
+        return self.context.snapshot_id, _func.replace("({params})", "")
+
+    checks(
+        func=func,
+        table=table,
+        snapshot_id=self.context.snapshot_id,
+    )
