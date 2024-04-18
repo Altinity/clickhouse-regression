7bb251ce0b88b86e0ae8be7f5a28bee292a0af0f
alsu <alsugilyazova1@mail.ru>
2024-04-18 11:54:57 +0200

    update versions in attach partition suite

--- alter/table/attach_partition/partition_key_datetime.py
+++ alter/table/attach_partition/partition_key_datetime.py
@@ -236,11 +236,11 @@ def check_attach_partition_from(
     """Check `attach partition from` with different types of source and destination tables."""
 
     if (
-        check_clickhouse_version("<24.3")(self)
+        check_clickhouse_version("<24.4")(self)
         and source_partition_key != destination_partition_key
     ):
         skip(
-            "`attach partition from` with tables that have different partition keys are not supported before 24.3"
+            "`attach partition from` with tables that have different partition keys are not supported before 24.4"
         )
 
     self.context.source_engine = source_table.__name__.split("_")[-1]
@@ -271,7 +271,7 @@ def check_attach_partition_from(
             node=self.context.node_1,
         )
 
-    if check_clickhouse_version(">=24.3")(self):
+    if check_clickhouse_version(">=24.4")(self):
         with And(
             "I add setting to allow alter partition with different partition keys"
         ):
@@ -574,12 +574,12 @@ def attach_partition_from(self):
             source_partition_key, destination_partition_key = partition_keys
             source_table, destination_table = tables
 
-            source_partition_key_str = source_partition_key.replace("(", "_")
-            source_partition_key_str = source_partition_key_str.replace(")", "_")
-            destination_partition_key_str = destination_partition_key.replace("(", "_")
-            destination_partition_key_str = destination_partition_key_str.replace(
+            source_partition_key_str = source_partition_key.replace("(", "_").replace(
                 ")", "_"
             )
+            destination_partition_key_str = destination_partition_key.replace(
+                "(", "_"
+            ).replace(")", "_")
 
             Scenario(
                 f"combination partition keys {source_partition_key_str} {destination_partition_key_str}  tables  {source_table.__name__} {destination_table.__name__}",
