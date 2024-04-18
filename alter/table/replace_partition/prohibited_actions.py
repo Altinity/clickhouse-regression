94c23dbbc07af5d96574489fc51ef7bb120908fa
alsu <alsugilyazova1@mail.ru>
2024-04-18 11:49:40 +0200

    update version in replace partition suite

--- alter/table/replace_partition/prohibited_actions.py
+++ alter/table/replace_partition/prohibited_actions.py
@@ -293,7 +293,7 @@ def partition_by(self):
     destination_table = "destination_" + getuid()
     source_table = "source_" + getuid()
 
-    if check_clickhouse_version(">=24.3")(self):
+    if check_clickhouse_version(">=24.4")(self):
         exitcode, message = io_error_message(
             exitcode=36,
             message="Destination table partition expression columns must be a subset of source table partition expression columns",
