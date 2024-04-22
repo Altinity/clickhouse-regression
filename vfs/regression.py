93c16c51fb84479b9e955ca641522c87587568bf
alsu <alsugilyazova1@mail.ru>
2024-04-22 16:43:41 +0200

    analyzer off tiered storage and vfs

--- vfs/regression.py
+++ vfs/regression.py
@@ -282,6 +282,20 @@ def regression(
     if storages is None:
         storages = ["minio"]
 
+    with Given("I disable experimental analyzer if needed"):
+        if check_clickhouse_version(">=24.3")(self):
+            if not allow_experimental_analyzer:
+                default_query_settings = getsattr(
+                    current().context, "default_query_settings", []
+                )
+                default_query_settings.append(("allow_experimental_analyzer", 0))
+        else:
+            if allow_experimental_analyzer:
+                default_query_settings = getsattr(
+                    current().context, "default_query_settings", []
+                )
+                default_query_settings.append(("allow_experimental_analyzer", 1))
+
     if "aws_s3" in storages:
         Module(test=aws_s3)(
             local=local,
