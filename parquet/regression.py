b3edb599eec0040698d1b31086eff1d0dd357423
alsu <alsugilyazova1@mail.ru>
2024-04-22 16:34:39 +0200

    analyzer off parquet

--- parquet/regression.py
+++ parquet/regression.py
@@ -385,6 +385,20 @@ def regression(
         )
         self.context.cluster = cluster
 
+    with And("I disable experimental analyzer if needed"):
+        if check_clickhouse_version(">=24.3")(self):
+            if not allow_experimental_analyzer:
+                default_query_settings = getsattr(
+                    current().context, "default_query_settings", []
+                )
+                default_query_settings.append(("allow_experimental_analyzer", 0))
+        else:
+            if allow_experimental_analyzer:
+                default_query_settings = getsattr(
+                    current().context, "default_query_settings", []
+                )
+                default_query_settings.append(("allow_experimental_analyzer", 1))
+
     with Given("I have a Parquet table definition"):
         columns = (
             cluster.node("clickhouse1")
