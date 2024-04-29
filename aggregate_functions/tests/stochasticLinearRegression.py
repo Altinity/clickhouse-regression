d642925a9fc74b58508bcecad956dabf364ee73e
alsugiliazova <alsugilyazova1@mail.ru>
2024-04-29 22:04:51 +0000

    add new aggregate functions snapshots for 24.3

--- aggregate_functions/tests/stochasticLinearRegression.py
+++ aggregate_functions/tests/stochasticLinearRegression.py
@@ -5,6 +5,7 @@ from aggregate_functions.requirements import (
 )
 
 from aggregate_functions.tests.avg import scenario as checks
+from aggregate_functions.tests.steps import *
 
 
 @TestScenario
@@ -12,15 +13,29 @@ from aggregate_functions.tests.avg import scenario as checks
 @Requirements(
     RQ_SRS_031_ClickHouse_AggregateFunctions_Specific_StochasticLinearRegression("1.0")
 )
-def scenario(self, func="stochasticLinearRegression({params})", table=None):
+def scenario(
+    self, func="stochasticLinearRegression({params})", table=None, snapshot_id=None
+):
     """Check stochasticLinearRegression aggregate function by using the same tests as for avg."""
 
     if table is None:
         table = self.context.table
+
+    if check_clickhouse_version(">=24.3")(self) and check_current_cpu("aarch64")(self):
+        clickhouse_version = ">=24.3"
+    else:
+        clickhouse_version = ">=23.2"
+
+    self.context.snapshot_id = get_snapshot_id(
+        snapshot_id=snapshot_id, clickhouse_version=clickhouse_version
+    )
 
     _func = func.replace(
         "({params})",
         f"(0.1, 0.0, 5, 'SGD')({{params}}*0.1+{{params}}*0.2+7,{{params}},{{params}}*0.3)",
     )
 
-    checks(func=_func, table=table, decimal=False)
+    if "Merge" in self.name:
+        return self.context.snapshot_id, _func.replace("({params})", "")
+
+    checks(func=_func, table=table, decimal=False, snapshot_id=self.context.snapshot_id)
