15ae348a8e0e3b670d735ed4e190397f0bce40c4
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-05-02 12:37:42 -0400

    fix s3 ffails

--- s3/regression.py
+++ s3/regression.py
@@ -191,6 +191,21 @@ xfails = {
             check_clickhouse_version("<23.11"),
         )
     ],
+    ":/:/backup/:/metadata:": [(
+        Fail,
+        "Investigate",
+        check_clickhouse_version("<=22.8"),
+    )],
+    ":/:/disk/cache*": [(
+        Fail,
+        "Investigate",
+        check_clickhouse_version("<=22.8"),
+    )],
+    ":/:/invalid disk/cache*": [(
+        Fail,
+        "Investigate",
+        check_clickhouse_version("<=22.8"),
+    )],
 }
 
 ffails = {
@@ -230,17 +245,17 @@ ffails = {
     ":/:/backup/:/metadata:": (
         XFail,
         "Under development for 22.8 and newer.",
-        check_clickhouse_version(">=22.8"),
+        check_clickhouse_version("<=22.8"),
     ),
     ":/:/disk/cache*": (
         XFail,
         "Under development for 22.8 and newer.",
-        check_clickhouse_version(">=22.8"),
+        check_clickhouse_version("<=22.8"),
     ),
     ":/:/invalid disk/cache*": (
         XFail,
         "Under development for 22.8 and newer.",
-        check_clickhouse_version(">=22.8"),
+        check_clickhouse_version("<=22.8"),
     ),
     ":/vfs": (Skip, "vfs not supported on < 24", check_clickhouse_version("<24")),
     ":/:/disk/no restart": (
