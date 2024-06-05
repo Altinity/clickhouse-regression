9977bf83272e5d7d71f6caa7e7c888dbdf29fbd8
alsu <alsugilyazova1@mail.ru>
2024-06-05 23:19:10 +0200

    fix to bash script

--- .github/add_link_to_logs.sh
+++ .github/add_link_to_logs.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+sudo chown -R $(whoami):$(whoami) $SUITE/
+
+if [[ $artifacts == 'internal' ]]; then
+    artifact_s3_bucket_path="altinity-internal-test-reports"
+    artifact_s3_dir="clickhouse/$version/$GITHUB_RUN_ID/testflows"
+    confidential="--confidential"
+elif [[ $artifacts == 'public' ]]; then
+    artifact_s3_bucket_path="altinity-test-reports"
+    artifact_s3_dir="clickhouse/$version/$GITHUB_RUN_ID/testflows"
+    confidential=""
+elif [[ $artifacts == 'builds' ]]; then
+    artifact_s3_bucket_path="altinity-build-artifacts"
+    confidential=""
+    if [[ $event_name == "pull_request" ]]; then
+        artifact_s3_dir="$pr_number/$build_sha/regression"
+    elif [[ $event_name == "release" || $event_name == "push" ]]; then
+        artifact_s3_dir="0/$build_sha/regression"
+    fi
+
+fi
 
 link="https://$artifact_s3_bucket_path.s3.amazonaws.com/index.html#$artifact_s3_dir/"
-echo $link 
+echo $link
