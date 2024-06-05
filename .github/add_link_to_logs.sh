b62b333c316e32120341aec01d3b9b40cc233999
alsu <alsugilyazova1@mail.ru>
2024-06-06 00:02:02 +0200

    add empty echo to add empty line to the output

--- .github/add_link_to_logs.sh
+++ .github/add_link_to_logs.sh
@@ -21,4 +21,5 @@ elif [[ $artifacts == 'builds' ]]; then
 fi
 
 link="Artifacts will be uploaded to https://$artifact_s3_bucket_path.s3.amazonaws.com/index.html#$artifact_s3_dir/"
+echo ""
 echo $link
