a00804690bb3b77590d27428b7f5410793ce692a
alsu <alsugilyazova1@mail.ru>
2024-06-07 22:33:55 +0200

    fix links

--- .github/add_link_to_logs.sh
+++ .github/add_link_to_logs.sh
@@ -22,5 +22,5 @@ fi
 
 echo ""
 echo "Artifacts will be uploaded to https://$artifact_s3_bucket_path.s3.amazonaws.com/index.html#$artifact_s3_dir/$(uname -i)/$SUITE$STORAGE/"
-echo "Review new fails (brisk) at https://$artifact_s3_bucket_path.s3.amazonaws.com/index.html#$artifact_s3_dir/$(uname -i)/$SUITE$STORAGE/brisk-new-fails.log.txt"
-echo "Review new fails (nice) at https://$artifact_s3_bucket_path.s3.amazonaws.com/index.html#$artifact_s3_dir/$(uname -i)/$SUITE$STORAGE/nice-new-fails.log.txt"
+echo "Review new fails (brisk) at https://$artifact_s3_bucket_path.s3.amazonaws.com/$artifact_s3_dir/$(uname -i)/$SUITE$STORAGE/brisk-new-fails.log.txt"
+echo "Review new fails (nice) at https://$artifact_s3_bucket_path.s3.amazonaws.com/$artifact_s3_dir/$(uname -i)/$SUITE$STORAGE/nice-new-fails.log.txt"
