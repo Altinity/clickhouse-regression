c1cc4128b5920c7aafe44082e72721daca40710d
selfeer <selfeers@gmail.com>
2024-06-07 13:16:16 +0400

    update docstrings

--- parquet/performance/tests/duckdb/reports.py
+++ parquet/performance/tests/duckdb/reports.py
@@ -6,11 +6,13 @@ import os
 
 
 def create_directory_if_not_exists(directory_path):
+    """Create a directory if it does not exist."""
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
 
 def sort_csv_data(data):
+    """Sort the data in the CSV file."""
     merged_data = {}
 
     for item in data:
