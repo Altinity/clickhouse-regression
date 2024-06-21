a823893643da09dc23af3e192ccb4bc835ea0657
alsu <alsugilyazova1@mail.ru>
2024-06-21 12:05:42 +0200

    add check_cascade_mv_definer_mv_mv_definer test to rbac suite

--- rbac/tests/sql_security/cascading_views.py
+++ rbac/tests/sql_security/cascading_views.py
@@ -78,7 +78,7 @@ def cascade_mv_definer_mv_definer_mv_definer(
         )
 
     with And(
-        "new user to test privileges for different operations with cascading view"
+        "new user to test required privileges for different operations with cascading view"
     ):
         new_user = "new_user_" + getuid()
         create_user(user_name=new_user)
@@ -268,7 +268,7 @@ def cascade_mv_mv_definer_mv_definer(
         )
 
     with And(
-        "new user to test privileges for different operations with cascading view"
+        "new user to test required privileges for different operations with cascading view"
     ):
         new_user = "new_user_" + getuid()
         create_user(user_name=new_user)
@@ -382,6 +382,184 @@ def cascade_mv_mv_definer_mv_definer(
 
 
 @TestScenario
+def cascade_mv_definer_mv_mv_definer(
+    self,
+    user_source_privileges,
+    definer_one_source_privileges,
+    definer_one_target_one_privileges,
+    definer_three_target_two_privileges,
+    definer_three_target_three_privileges,
+    grant_privilege,
+):
+    """
+    Test privileges for different operations on cascading view:
+    source table -> MV with definer -> MV without sql security specified -> MV with definer
+    """
+    node = self.context.node
+
+    with Given("source table for the first MV in cascade"):
+        source_table_name = "source_table_" + getuid()
+        create_simple_MergeTree_table(table_name=source_table_name)
+
+    with And("target table and definer user for the first MV"):
+        target_table_name_one = "target_table_one_" + getuid()
+        create_simple_MergeTree_table(table_name=target_table_name_one)
+        definer_user_one = "definer_one_" + getuid()
+        create_user(user_name=definer_user_one)
+
+    with And("first MV"):
+        view_name_1 = "view_one_" + getuid()
+        create_materialized_view(
+            view_name=view_name_1,
+            source_table_name=source_table_name,
+            target_table_name=target_table_name_one,
+            definer=definer_user_one,
+            sql_security="DEFINER",
+        )
+
+    with And("target table for the second MV"):
+        target_table_name_two = "target_table_two_" + getuid()
+        create_simple_MergeTree_table(table_name=target_table_name_two)
+
+    with And("second MV"):
+        view_name_2 = "view_two_" + getuid()
+        create_materialized_view(
+            view_name=view_name_2,
+            source_table_name=target_table_name_one,
+            target_table_name=target_table_name_two,
+        )
+
+    with And("target table and definer user for the third MV"):
+        target_table_name_three = "target_table_three_" + getuid()
+        create_simple_MergeTree_table(table_name=target_table_name_three)
+        definer_user_three = "definer_three_" + getuid()
+        create_user(user_name=definer_user_three)
+
+    with And("third MV"):
+        view_name_3 = "view_three_" + getuid()
+        create_materialized_view(
+            view_name=view_name_3,
+            source_table_name=target_table_name_two,
+            target_table_name=target_table_name_three,
+            definer=definer_user_three,
+            sql_security="DEFINER",
+        )
+
+    with And(
+        "new user to test required privileges for different operations with cascading view"
+    ):
+        new_user = "new_user_" + getuid()
+        create_user(user_name=new_user)
+
+    with And("I grant privileges to new user"):
+        grant_privilege(
+            privileges=user_source_privileges, object=source_table_name, user=new_user
+        )
+
+    with And(
+        "grant privileges to all three definer users on their source and target tables"
+    ):
+        grant_privilege(
+            privileges=definer_one_source_privileges,
+            object=source_table_name,
+            user=definer_user_one,
+        )
+        grant_privilege(
+            privileges=definer_one_target_one_privileges,
+            object=target_table_name_one,
+            user=definer_user_one,
+        )
+        grant_privilege(
+            privileges=definer_three_target_two_privileges,
+            object=target_table_name_two,
+            user=definer_user_three,
+        )
+        grant_privilege(
+            privileges=definer_three_target_three_privileges,
+            object=target_table_name_three,
+            user=definer_user_three,
+        )
+
+    with And(
+        """check that following set of grants allows to insert into source table:
+        - INSERT on source table for user, 
+        - SELECT on source table for definer one,
+        - SELECT on target table one for definer one,
+        - INSERT on target table one for definer one, 
+        - SELECT on target table two for definer three,
+        - INSERT on target table three for definer three
+        otherwise expect exception"""
+    ):
+        if (
+            "INSERT" in user_source_privileges
+            and "SELECT" in definer_one_source_privileges
+            and "SELECT" in definer_one_target_one_privileges
+            and "INSERT" in definer_one_target_one_privileges
+            and "SELECT" in definer_three_target_two_privileges
+            and "INSERT" in definer_three_target_three_privileges
+        ):
+            node.query(
+                f"INSERT INTO {source_table_name} VALUES (1), (2), (3)",
+                settings=[("user", new_user)],
+            )
+        else:
+            exitcode, message = 241, f"Exception:"
+            node.query(
+                f"INSERT INTO {source_table_name} VALUES (1), (2), (3)",
+                settings=[("user", new_user)],
+                exitcode=exitcode,
+                message=message,
+            )
+
+    with And("grant SELECT privilege on the third MV to new user"):
+        grant_privilege(privileges=["SELECT"], object=view_name_3, user=new_user)
+
+    with And(
+        """check that following set of grants allows user to select from third MV:
+        - SELECT on third MV for new user
+        - SELECT on second target table (third MV's source table) for definer three 
+        - SELECT on third target table for definer three
+        otherwise expect exception"""
+    ):
+        exitcode, message = 241, f"Exception:"
+        if (
+            "SELECT" in definer_three_target_two_privileges
+            and "SELECT" in definer_three_target_three_privileges
+        ):
+            node.query(f"SELECT * FROM {view_name_3}", settings=[("user", new_user)])
+        else:
+            node.query(
+                f"SELECT * FROM {view_name_3}",
+                settings=[("user", new_user)],
+                exitcode=exitcode,
+                message=message,
+            )
+
+    with And("grant INSERT privilege on the third MV to new user"):
+        grant_privilege(privileges=["INSERT"], object=view_name_3, user=new_user)
+
+    with And(
+        """check that following set of grants allows user to insert into third MV:
+        - INSERT on third MV for new user
+        - INSERT on third MV's target table for definer three
+        otherwise expect exception"""
+    ):
+        exitcode, message = 241, f"Exception:"
+        if "INSERT" in definer_three_target_three_privileges:
+            node.query(
+                f"INSERT INTO {view_name_3} VALUES (4), (5), (6)",
+                settings=[("user", new_user)],
+            )
+        else:
+            node.query(
+                f"INSERT INTO {view_name_3} VALUES (4), (5), (6)",
+                settings=[("user", new_user)],
+                exitcode=exitcode,
+                message=message,
+            )
+
+
+@TestFeature
 def check_cascade_mv_definer_mv_definer_mv_definer(self):
     """
     Run test for all possible combinations of privileges for cascading view:
@@ -488,11 +666,20 @@ def check_cascade_mv_definer_mv_definer_mv_definer(self):
         ) in privileges_combinations:
             test_name = f"user-{user_source_privileges},definer_one_source-{definer_one_source_privileges},definer_one_target_one-{definer_one_target_one_privileges},definer_two_target_one-{definer_two_target_one_privileges},definer_two_target_two-{definer_two_target_two_privileges},definer_three_target_two-{definer_three_target_two_privileges},definer_three_target_three-{definer_three_target_three_privileges},grant_privileges_option-{grant_privilege.__name__}"
             test_name = (
-                test_name.replace("(", "#")
-                .replace(")", "#")
+                test_name.replace("(", "_")
+                .replace(")", "_")
                 .replace(" ", "")
                 .replace("'", "")
             )
+            # test_name = f"{user_source_privileges}_{definer_one_source_privileges}_{definer_one_target_one_privileges}_{definer_two_target_one_privileges}_{definer_two_target_two_privileges}_{definer_three_target_two_privileges}_{definer_three_target_three_privileges}"
+            # test_name = (
+            #     test_name.replace("(", "_")
+            #     .replace(")", "_")
+            #     .replace(" ", "")
+            #     .replace("'", "")
+            #     .replace(",", "")
+            #     .replace("_", "")
+            # )
             Scenario(
                 test_name,
                 description=f"""
@@ -521,7 +708,7 @@ def check_cascade_mv_definer_mv_definer_mv_definer(self):
 
 
 @TestScenario
-def check_cascade_mv_mv_definer_mv_definer(self, node="clickhouse1"):
+def check_cascade_mv_mv_definer_mv_definer(self):
     """
     Run test for all possible combinations of privileges for cascading view:
     source table -> MV without sql security specified-> MV with definer -> MV with definer
@@ -644,6 +831,130 @@ def check_cascade_mv_mv_definer_mv_definer(self, node="clickhouse1"):
         join()
 
 
+@TestScenario
+def check_cascade_mv_definer_mv_mv_definer(self):
+    """
+    Run test for all possible combinations of privileges for cascading view:
+    source table -> MV with definer -> MV without sql security specified -> MV with definer
+    """
+    if self.context.stress:
+        with Given("choices how privileges can be granted"):
+            grant_privileges = define(
+                "grant privileges",
+                [grant_privileges_directly, grant_privileges_via_role],
+                encoder=get_name,
+            )
+
+        with And("possible table privileges"):
+            privileges = define(
+                "privileges", ("SELECT", "INSERT", "ALTER", "CREATE", "NONE")
+            )
+    else:
+        with Given("privileges are only granted directly"):
+            grant_privileges = define(
+                "grant privileges", [grant_privileges_directly], encoder=get_name
+            )
+            grant_privileges = [grant_privileges_directly]
+
+        with And("only required privileges"):
+            privileges = define("privileges", ("SELECT", "INSERT", "NONE"))
+
+    with Given(
+        "generate all possible pairs of privileges and add an option with no privileges"
+    ):
+        privilege_pairs = define(
+            "privilege pairs", list(combinations(privileges, 2)) + [("NONE",)]
+        )
+
+    with And(
+        "create dictionary for storing possible privileges for definer users and for user executing queries"
+    ):
+        combinations_dict = {}
+
+        with By(
+            "definer privileges for the user executing queries for the source table of the first MV"
+        ):
+            combinations_dict["user_source_privileges"] = privilege_pairs
+        with By(
+            "define privileges for the first definer user for the source table of the first MV"
+        ):
+            combinations_dict["definer_one_source_privileges"] = privilege_pairs
+        with By(
+            "define privileges for the first definer user for the target table of the first MV"
+        ):
+            combinations_dict["definer_one_target_one_privileges"] = privilege_pairs
+        with By(
+            "define privileges for the third definer user for the target table of the second MV (which is the source table of the third MV)"
+        ):
+            combinations_dict["definer_three_target_two_privileges"] = privilege_pairs
+        with By(
+            "define privileges for the third definer user for the target table of the third MV"
+        ):
+            combinations_dict["definer_three_target_three_privileges"] = privilege_pairs
+        with By("define the method of granting privileges"):
+            combinations_dict["grant_privilege"] = grant_privileges
+
+    with And(
+        """I use covering array with strength 6 to decrease the number of combinations.
+        The strength of a covering array specifies the number of variables for which 
+        all possible value combinations must appear at least once in the array."""
+    ):
+        covering_array = CoveringArray(combinations_dict, strength=5)
+
+    with And(
+        "I create a list of lists of privileges from the covering array with some combinations of privileges"
+    ):
+        privileges_combinations = [
+            [
+                item["user_source_privileges"],
+                item["definer_one_source_privileges"],
+                item["definer_one_target_one_privileges"],
+                item["definer_three_target_two_privileges"],
+                item["definer_three_target_three_privileges"],
+                item["grant_privilege"],
+            ]
+            for item in covering_array
+        ]
+
+    with Pool(7) as executor:
+        for (
+            user_source_privileges,
+            definer_one_source_privileges,
+            definer_one_target_one_privileges,
+            definer_three_target_two_privileges,
+            definer_three_target_three_privileges,
+            grant_privilege,
+        ) in privileges_combinations:
+            test_name = f"user-{user_source_privileges},definer_one_source-{definer_one_source_privileges},definer_one_target_one-{definer_one_target_one_privileges},definer_three_target_two-{definer_three_target_two_privileges},definer_three_target_three-{definer_three_target_three_privileges},grant_privileges_option-{grant_privilege.__name__}"
+            test_name = (
+                test_name.replace("(", "_")
+                .replace(")", "_")
+                .replace(" ", "")
+                .replace("'", "")
+            )
+            Scenario(
+                test_name,
+                description=f"""
+                User source privileges: {user_source_privileges}
+                Definer one source privileges: {definer_one_source_privileges}
+                Definer one target one privileges: {definer_one_target_one_privileges}
+                Definer three target two privileges: {definer_three_target_two_privileges}
+                Definer three target three privileges: {definer_three_target_three_privileges}
+                """,
+                test=cascade_mv_definer_mv_mv_definer,
+                parallel=True,
+                executor=executor,
+            )(
+                user_source_privileges=user_source_privileges,
+                definer_one_source_privileges=definer_one_source_privileges,
+                definer_one_target_one_privileges=definer_one_target_one_privileges,
+                definer_three_target_two_privileges=definer_three_target_two_privileges,
+                definer_three_target_three_privileges=definer_three_target_three_privileges,
+                grant_privilege=grant_privilege,
+            )
+        join()
+
+
 @TestFeature
 @Name("cascading views")
 def feature(self, node="clickhouse1"):
@@ -652,3 +963,4 @@ def feature(self, node="clickhouse1"):
 
     Scenario(run=check_cascade_mv_definer_mv_definer_mv_definer)
     Scenario(run=check_cascade_mv_mv_definer_mv_definer)
+    Scenario(run=check_cascade_mv_definer_mv_mv_definer)
