29f47728c75eed4b977a251441ab4a128fc8ea52
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-06-26 08:20:09 -0400

    modernizing tiered storage ttl moves

--- tiered_storage/tests/ttl_moves/inserts_to_volume.py
+++ tiered_storage/tests/ttl_moves/inserts_to_volume.py
@@ -11,7 +11,7 @@ from testflows.core import *
 from testflows.asserts import error
 
 
-@TestScenario
+@TestOutline(Scenario)
 @Name("inserts to volume")
 @Requirements()
 @Examples(
@@ -25,67 +25,62 @@ from testflows.asserts import error
     ],
     "%-21s | %-20s",
 )
-def scenario(self, cluster, node="clickhouse1"):
+def scenario(self, name, engine):
     """Check that when a TTL expression is set to a DateTime column
     without any interval offset then the inserted parts are immediately
     moved to the specified volume if the DateTime column value
     is less than the TLL expression and are not moved when the value is greater.
     """
-    with Given("cluster node"):
-        node = cluster.node(node)
+    cluster = self.context.cluster
+    node = cluster.node("clickhouse1")
 
-    for example in self.examples:
-        name, engine = example
-        for positive in (True, False):
-            with When(
-                f"I check that inserted parts should {'be' if positive else 'not be'} moved"
-            ):
-                with Given(f"table name='{name}', engine='{engine}'"):
+    for positive in (True, False):
+        with Check(f"inserted parts should {'be' if positive else 'not be'} moved"):
+            try:
+                with Given("table"):
                     node.query(
                         f"""
-                        CREATE TABLE {name} (
-                            p1 Int64,
-                            s1 String,
-                            d1 DateTime
-                        ) ENGINE = {engine}
-                        ORDER BY tuple()
-                        PARTITION BY p1
-                        TTL d1 TO VOLUME 'external'
-                        SETTINGS storage_policy='small_jbod_with_external'
-                    """
+                            CREATE TABLE {name} (
+                                p1 Int64,
+                                s1 String,
+                                d1 DateTime
+                            ) ENGINE = {engine}
+                            ORDER BY tuple()
+                            PARTITION BY p1
+                            TTL d1 TO VOLUME 'external'
+                            SETTINGS storage_policy='small_jbod_with_external'
+                        """
                     )
-                    try:
-                        with When("I insert data"):
-                            for p in range(2):
-                                data = []  # 10MB in total
-                                now = time.time()
-                                for i in range(10):
-                                    p1 = p
-                                    s1 = get_random_string(
-                                        cluster, 1024 * 1024, steps=False
-                                    )  # 1MB
-                                    d1 = now - 1 if i > 0 or positive else now + 300
-                                    data.append(f"({p1}, '{s1}', toDateTime({d1}))")
-                                values = ",".join(data)
-                                node.query(
-                                    f"INSERT INTO {name} (p1, s1, d1) VALUES {values}"
-                                )
 
-                        with And("I get used disks for the table"):
-                            used_disks = get_used_disks_for_table(node, name)
-                            with Then(
-                                f"parts {'should' if positive else 'should not'} have been moved"
-                            ):
-                                assert set(used_disks) == (
-                                    {"external" if positive else "jbod1"}
-                                ), error()
+                with When("I insert data"):
+                    for p in range(2):
+                        data = []  # 10MB in total
+                        now = time.time()
+                        for i in range(10):
+                            p1 = p
+                            s1 = get_random_string(
+                                cluster, 1024 * 1024, steps=False
+                            )  # 1MB
+                            d1 = now - 1 if i > 0 or positive else now + 300
+                            data.append(f"({p1}, '{s1}', toDateTime({d1}))")
+                        values = ",".join(data)
+                        node.query(f"INSERT INTO {name} (p1, s1, d1) VALUES {values}")
 
-                        with Then("number of rows should match"):
-                            r = node.query(
-                                f"SELECT count() FROM {name} FORMAT TabSeparated"
-                            ).output.strip()
-                            assert r == "20", error()
+                with And("I get used disks for the table"):
+                    used_disks = get_used_disks_for_table(node, name)
+                    with Then(
+                        f"parts {'should' if positive else 'should not'} have been moved"
+                    ):
+                        assert set(used_disks) == (
+                            {"external" if positive else "jbod1"}
+                        ), error()
 
-                    finally:
-                        with Finally("I drop the table"):
-                            node.query(f"DROP TABLE IF EXISTS {name} SYNC")
+                with Then("number of rows should match"):
+                    r = node.query(
+                        f"SELECT count() FROM {name} FORMAT TabSeparated"
+                    ).output.strip()
+                    assert r == "20", error()
+
+            finally:
+                with Finally("I drop the table"):
+                    node.query(f"DROP TABLE IF EXISTS {name} SYNC")
--- tiered_storage/tests/ttl_moves/most_recent.py
+++ tiered_storage/tests/ttl_moves/most_recent.py
@@ -12,7 +12,7 @@ from testflows.asserts import error
 from tiered_storage.requirements import *
 
 
-@TestScenario
+@TestOutline(Scenario)
 @Name("most recent")
 @Requirements(RQ_SRS_004_TTLExpressions_Selection("1.0"))
 @Examples(
@@ -26,16 +26,15 @@ from tiered_storage.requirements import *
     ],
     "%-21s | %-20s",
 )
-def scenario(self, cluster, node="clickhouse1"):
+def scenario(self, name, engine):
     """Check that when multiple TTL expressions are triggered then
     the most recent is used to move the data.
     """
-    with Given("cluster node"):
-        node = cluster.node(node)
+    cluster = self.context.cluster
+    node = cluster.node("clickhouse1")
 
-    for example in self.examples:
-        name, engine = example
-        with When(f"I create table name='{name}' with engine='{engine}'"):
+    try:
+        with Given("table"):
             node.query(
                 f"""
                 CREATE TABLE {name} (
@@ -50,58 +49,54 @@ def scenario(self, cluster, node="clickhouse1"):
                 SETTINGS storage_policy='jbods_with_external', merge_with_ttl_timeout=0
             """
             )
-            try:
-                with When("I disallow moves"):
-                    node.query("SYSTEM STOP MOVES")
 
-                with And("I insert data"):
-                    for p in range(3):
-                        data = []  # 2MB in total
-                        now = time.time()
-                        for i in range(2):
-                            s1 = get_random_string(
-                                cluster, 1024 * 1024, steps=False
-                            )  # 1MB
-                            data.append(f"({p}, '{s1}', toDateTime({now - 1}))")
-                        values = ",".join(data)
-                        node.query(f"INSERT INTO {name} (p1, s1, d1) VALUES {values}")
+        with When("I disallow moves"):
+            node.query("SYSTEM STOP MOVES")
 
-                with And("I get used disks for the table"):
+        with And("I insert data"):
+            for p in range(3):
+                data = []  # 2MB in total
+                now = time.time()
+                for i in range(2):
+                    s1 = get_random_string(cluster, 1024 * 1024, steps=False)  # 1MB
+                    data.append(f"({p}, '{s1}', toDateTime({now - 1}))")
+                values = ",".join(data)
+                node.query(f"INSERT INTO {name} (p1, s1, d1) VALUES {values}")
+
+        with And("I get used disks for the table"):
+            used_disks = get_used_disks_for_table(node, name)
+            with Then(f"parts should have been placed according to the round-robin"):
+                assert set(used_disks) == {"jbod1", "jbod2"}, error()
+
+        with And("I wait until both TTL expressions would trigger"):
+            time.sleep(30)
+
+        with And("I get used disks for the table before I resume moves"):
+            used_disks = get_used_disks_for_table(node, name)
+            with Then(f"parts should remain according to the round-robin"):
+                assert set(used_disks) == {"jbod1", "jbod2"}, error()
+
+        with And("I resume moves"):
+            node.query(f"SYSTEM START MOVES {name}")
+
+        with And("I sleep for 40 sec to allow moves to complete"):
+            time.sleep(40)
+
+        for retry in retries(count=5, delay=1):
+            with retry:
+                with When("I again get used disks for the table"):
                     used_disks = get_used_disks_for_table(node, name)
                     with Then(
-                        f"parts should have been placed according to the round-robin"
+                        f"parts should have been moved according to the most recent TTL expression"
                     ):
-                        assert set(used_disks) == {"jbod1", "jbod2"}, error()
+                        assert set(used_disks) == {"external"}, error()
 
-                with And("I wait until both TTL expressions would trigger"):
-                    time.sleep(30)
+                with Then("number of rows should match"):
+                    r = node.query(
+                        f"SELECT count() FROM {name} FORMAT TabSeparated"
+                    ).output.strip()
+                    assert r == "6", error()
 
-                with And("I get used disks for the table before I resume moves"):
-                    used_disks = get_used_disks_for_table(node, name)
-                    with Then(f"parts should remain according to the round-robin"):
-                        assert set(used_disks) == {"jbod1", "jbod2"}, error()
-
-                with And("I resume moves"):
-                    node.query(f"SYSTEM START MOVES {name}")
-
-                with And("I sleep for 40 sec to allow moves to complete"):
-                    time.sleep(40)
-
-                for retry in retries(count=5, delay=1):
-                    with retry:
-                        with When("I again get used disks for the table"):
-                            used_disks = get_used_disks_for_table(node, name)
-                            with Then(
-                                f"parts should have been moved according to the most recent TTL expression"
-                            ):
-                                assert set(used_disks) == {"external"}, error()
-
-                        with Then("number of rows should match"):
-                            r = node.query(
-                                f"SELECT count() FROM {name} FORMAT TabSeparated"
-                            ).output.strip()
-                            assert r == "6", error()
-
-            finally:
-                with Finally("I drop the table"):
-                    node.query(f"DROP TABLE IF EXISTS {name} SYNC")
+    finally:
+        with Finally("I drop the table"):
+            node.query(f"DROP TABLE IF EXISTS {name} SYNC")
