93ff7a05594aba848ac63db6a66608e0f4edacea
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-07-30 13:52:45 -0400

    replace . with _ in more test names to fix xfails

--- ssl_server/tests/dictionary.py
+++ ssl_server/tests/dictionary.py
@@ -59,7 +59,7 @@ def https_server_dictionary_checks(self):
             timeout=5,
         )
 
-    with Check(f"TLSv1.2 suite connection should work"):
+    with Check(f"TLSv1_2 suite connection should work"):
         https_server_https_dictionary_connection(
             options={
                 "requireTLSv1_2": "true",
@@ -78,7 +78,7 @@ def https_server_dictionary_checks(self):
             timeout=5,
         )
 
-    with Check("TLSv1.1 suite connection should be rejected"):
+    with Check("TLSv1_1 suite connection should be rejected"):
         https_server_https_dictionary_connection(
             options={
                 "requireTLSv1_1": "true",
@@ -88,7 +88,7 @@ def https_server_dictionary_checks(self):
             timeout=5,
         )
 
-    with Check("TLSv1.3 suite connection should be rejected"):
+    with Check("TLSv1_3 suite connection should be rejected"):
         https_server_https_dictionary_connection(
             options={
                 "requireTLSv1_3": "true",
@@ -104,13 +104,13 @@ def https_server_dictionary_checks(self):
             success=True,
         )
 
-    with Check(f"just disabling TLSv1.1 suite connection should work"):
+    with Check(f"just disabling TLSv1_1 suite connection should work"):
         https_server_https_dictionary_connection(
             options={"disableProtocols": "tlsv1_1"},
             success=True,
         )
 
-    with Check(f"just disabling TLSv1.3 suite connection should work"):
+    with Check(f"just disabling TLSv1_3 suite connection should work"):
         https_server_https_dictionary_connection(
             options={"disableProtocols": "tlsv1_3"},
             success=True,
