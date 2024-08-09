c3ae1da067cd913883db1b783f458e177712969e
Vitaliy Zakaznikov <vzakaznikov@testflows.com>
2024-08-09 12:36:01 -0500

    Updating tests/privileges/multiple_auth_methods_v2/model.py

--- rbac/tests/privileges/multiple_auth_methods_v2/model.py
+++ rbac/tests/privileges/multiple_auth_methods_v2/model.py
@@ -1,5 +1,11 @@
 import rbac.tests.privileges.multiple_auth_methods_v2.actions as actions
 
+from helpers.sql.create_user import CreateUser
+
+
+class States:
+    CreateUser = CreateUser
+
 
 class Model:
     """Multiple user authentication methods model."""
@@ -14,6 +20,10 @@ class Model:
     def expect_no_password_auth_cannot_coexist_with_others_error(self, r):
         """Check for no password authentication method coexisting with others error."""
         current = self.behavior[-1]
+
+        if not isinstance(current, States.CreateUser):
+            return
+
         auth_methods = [auth_method.method for auth_method in current.identification]
 
         if "no_password" in auth_methods and len(auth_methods) > 1:
