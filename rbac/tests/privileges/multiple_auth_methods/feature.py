cee666d82fcf6f40a37103e65a196a7de6466838
alsu <alsugilyazova1@mail.ru>
2024-08-13 16:10:14 +0300

    add syntax tests for multiple auth methods feature

--- rbac/tests/privileges/multiple_auth_methods/feature.py
+++ rbac/tests/privileges/multiple_auth_methods/feature.py
@@ -9,21 +9,21 @@ def feature(self):
     """Check that multiple authentication methods support."""
     with Pool(4) as pool:
         try:
-            # Feature(
-            #     run=load(
-            #         "rbac.tests.privileges.multiple_auth_methods.reset", "feature"
-            #     ),
-            #     parallel=True,
-            #     executor=pool,
-            # )
-            # Feature(
-            #     run=load(
-            #         "rbac.tests.privileges.multiple_auth_methods.add_identified",
-            #         "feature",
-            #     ),
-            #     parallel=True,
-            #     executor=pool,
-            # )
+            Feature(
+                run=load(
+                    "rbac.tests.privileges.multiple_auth_methods.reset", "feature"
+                ),
+                parallel=True,
+                executor=pool,
+            )
+            Feature(
+                run=load(
+                    "rbac.tests.privileges.multiple_auth_methods.add_identified",
+                    "feature",
+                ),
+                parallel=True,
+                executor=pool,
+            )
             Feature(
                 run=load(
                     "rbac.tests.privileges.multiple_auth_methods.alter_identified",
@@ -32,12 +32,28 @@ def feature(self):
                 parallel=True,
                 executor=pool,
             )
-            # Feature(
-            #     run=load(
-            #         "rbac.tests.privileges.multiple_auth_methods.create", "feature"
-            #     ),
-            #     parallel=True,
-            #     executor=pool,
-            # )
+            Feature(
+                run=load(
+                    "rbac.tests.privileges.multiple_auth_methods.create", "feature"
+                ),
+                parallel=True,
+                executor=pool,
+            )
+            Feature(
+                run=load(
+                    "rbac.tests.privileges.multiple_auth_methods.system_table",
+                    "feature",
+                ),
+                parallel=True,
+                executor=pool,
+            )
+            Feature(
+                run=load(
+                    "rbac.tests.privileges.multiple_auth_methods.syntax",
+                    "feature",
+                ),
+                parallel=True,
+                executor=pool,
+            )
         finally:
             join()
