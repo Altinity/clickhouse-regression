64210b2bc31f395ccda79918791a72b252bd43e0
alsu <alsugilyazova1@mail.ru>
2024-08-20 12:17:26 +0300

    merge two versions of create user tests with multiple auth methods

--- rbac/tests/privileges/multiple_auth_methods/create.py
+++ rbac/tests/privileges/multiple_auth_methods/create.py
@@ -1,8 +1,68 @@
-from rbac.tests.privileges.multiple_auth_methods.common import *
+from testflows.core import *
+from testflows.asserts import error
+
+from rbac.requirements import *
+from rbac.tests.privileges.multiple_auth_methods.common import (
+    create_user,
+    generate_auth_combinations,
+    authentication_methods_with_passwords,
+)
+import rbac.tests.privileges.multiple_auth_methods.actions as actions
+import rbac.tests.privileges.multiple_auth_methods.model as models
+
+from helpers.common import getuid
+
+
+# FIXME: add test to check login with another user's password
+@TestScenario
+def create_user_with_multiple_auth_methods_v2(self, auth_methods, node=None):
+    """Check that all authentication methods can be added to the user."""
+    if node is None:
+        node = self.context.node
+
+    user_name = f"user_{getuid()}"
+    self.context.behavior = []
+
+    with Given("I have client"):
+        self.context.client = actions.node_client()
+
+    with When("I create user with multiple authentication methods"):
+        user = actions.create_user(user_name=user_name, auth_methods=auth_methods)
+
+    with Then("I try to login"):
+        actions.login(user=user)
+
+    with And("I try to login with slightly wrong password"):
+        actions.login_with_wrong_password(user=user)
+
+    with And("I try to login with slightly wrong username"):
+        actions.login_with_wrong_username(user=user)
 
 
 @TestScenario
-def check_create_user_with_multiple_auth_methods(self, auth_methods, node=None):
+@Name("check create user with multiple auth methods v2")
+def check_create_user_v2(self, node=None):
+    """Run create user with multiple auth methods test with all combinations of auth methods."""
+    self.context.model = models.Model()
+
+    if node is None:
+        node = self.context.node
+
+    auth_methods_combinations = actions.create_user_auth_combinations(max_length=2)
+
+    with Pool(5) as executor:
+        for num, auth_methods in enumerate(auth_methods_combinations):
+            Scenario(
+                f"{num} {actions.names(auth_methods)}",
+                test=create_user_with_multiple_auth_methods_v2,
+                parallel=True,
+                executor=executor,
+            )(node=node, auth_methods=auth_methods)
+        join()
+
+
+@TestScenario
+def create_user_with_multiple_auth_methods(self, auth_methods, node=None):
     """Check that user can be created with multiple authentication methods."""
     if node is None:
         node = self.context.node
@@ -76,8 +136,26 @@ def check_create_user_with_multiple_auth_methods(self, auth_methods, node=None):
             assert sorted(system_auth_types) == sorted(auth_types), error()
 
 
+@TestScenario
+@Name("check create user with multiple auth methods")
+def check_create_user_with_multiple_auth_methods(self):
+    """Run create user with multiple auth methods test with all combinations of auth methods."""
+    auth_methods = generate_auth_combinations(
+        auth_methods_dict=authentication_methods_with_passwords,
+    )
+    with Pool(6) as executor:
+        for num, auth_methods in enumerate(auth_methods):
+            Scenario(
+                f"{num}",
+                test=create_user_with_multiple_auth_methods,
+                parallel=True,
+                executor=executor,
+            )(auth_methods=auth_methods)
+        join()
+
+
 @TestFeature
-@Name("create")
+@Name("create user with multiple auth methods")
 @Requirements(
     RQ_SRS_006_RBAC_User_MultipleAuthenticationMethods_CreateUser("1.0"),
     RQ_SRS_006_RBAC_User_MultipleAuthenticationMethods_CreateUser_NoPassword("1.0"),
@@ -85,15 +163,11 @@ def check_create_user_with_multiple_auth_methods(self, auth_methods, node=None):
 def feature(self, node="clickhouse1"):
     """Check that user can be created with multiple authentication methods."""
     self.context.node = self.context.cluster.node(node)
-    auth_methods = generate_auth_combinations(
-        auth_methods_dict=authentication_methods_with_passwords,
-    )
-    with Pool(4) as executor:
-        for num, auth_methods in enumerate(auth_methods):
-            Scenario(
-                f"{num}",
-                test=check_create_user_with_multiple_auth_methods,
-                parallel=True,
-                executor=executor,
-            )(auth_methods=auth_methods)
+    with Pool(2) as executor:
+        # Scenario(test=check_create_user_v2, parallel=True, executor=executor)()
+        Scenario(
+            test=check_create_user_with_multiple_auth_methods,
+            parallel=True,
+            executor=executor,
+        )()
         join()
