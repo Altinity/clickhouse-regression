8099229f68c7d9e1cb297d873392dde8a3b716df
alsu <alsugilyazova1@mail.ru>
2024-08-20 12:08:52 +0300

    update sanity check rbac multiple auth methods

--- rbac/tests/privileges/multiple_auth_methods/sanity.py
+++ rbac/tests/privileges/multiple_auth_methods/sanity.py
@@ -28,7 +28,7 @@ def omit_with_and_auth_type(self):
 
 
 @TestScenario
-@Name("create user with NO_PASSWORD")
+@Name("create user with NO_PASSWORD upper case")
 def no_password_upper_case(self):
     """Check that user can be created with no password using IDENTIFIED WITH NO_PASSWORD clause."""
     try:
@@ -48,7 +48,7 @@ def no_password_upper_case(self):
 
 
 @TestScenario
-@Name("create user with no_password")
+@Name("create user with no_password lower case")
 def no_password_lower_case(self):
     """Check that user can be created with no password using IDENTIFIED WITH no_password clause."""
     try:
@@ -67,11 +67,35 @@ def no_password_lower_case(self):
             common.execute_query(query=f"DROP USER IF EXISTS {user_name}")
 
 
+@TestScenario
+@Name("create user not identified")
+def create_user_not_identified(self):
+    """Check that user can be created with no password by using NOT IDENTIFIED clause."""
+    try:
+        with Given(
+            "construct CREATE USER query with no_password using NOT IDENTIFIED clause"
+        ):
+            user_name = f"user_{getuid()}"
+            query = f"CREATE USER {user_name} NOT IDENTIFIED"
+            note(query)
+
+        with Then("execute query, expect success"):
+            common.execute_query(query=query)
+
+        with And("login without password"):
+            common.login(user_name=user_name)
+    finally:
+        with Finally("drop user"):
+            common.execute_query(query=f"DROP USER IF EXISTS {user_name}")
+
+
 @TestFeature
 @Name("sanity check")
 def feature(self, node="clickhouse1"):
     """Check syntax when creating or altering user with one or multiple auth methods."""
     self.context.node = self.context.cluster.node(node)
 
-    for scenario in loads(current_module(), Scenario):
-        Scenario(run=scenario, flags=TE)
+    with Pool(3) as executor:
+        for scenario in loads(current_module(), Scenario):
+            Scenario(run=scenario, flags=TE, parallel=True, executor=executor)
+        join()
