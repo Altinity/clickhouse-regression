77537b7c26df7243718a1bb14e663533b593e04f
alsu <alsugilyazova1@mail.ru>
2024-08-21 13:22:31 +0300

    update common files multiple auth methods rbac

--- rbac/tests/privileges/multiple_auth_methods/errors.py
+++ rbac/tests/privileges/multiple_auth_methods/errors.py
@@ -2,6 +2,15 @@ def syntax_error():
     exitcode, message = 62, "Exception: Syntax error: failed at position"
     return exitcode, message
 
+
+def no_password_cannot_coexist_with_others():
+    exitcode, message = (
+        36,
+        "DB::Exception: Authentication method 'no_password' cannot co-exist with other authentication methods.",
+    )
+    return exitcode, message
+
+
 def no_user_with_such_name(user_name):
     def return_exitcode_and_message():
         exitcode, message = (
@@ -11,6 +20,7 @@ def no_user_with_such_name(user_name):
         return exitcode, message
 
     return return_exitcode_and_message
+
 
 def wrong_password(user_name):
     def return_exitcode_and_message():
