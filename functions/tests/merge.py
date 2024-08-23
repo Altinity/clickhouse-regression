138e01d2ff14f87bd7f124beb80840ea2c56acee
alsu <alsugilyazova1@mail.ru>
2024-08-23 23:50:46 +0300

    stabilize merge test

--- functions/tests/merge.py
+++ functions/tests/merge.py
@@ -5,77 +5,82 @@ from helpers.cluster import *
 
 
 @TestScenario
+@Repeat(1000, until="fail")
 def merge(self):
     "Automated test for issue 59401."
-
-    with Given("I create table on three servers"):
+    node_1 = self.context.node_1
+    with Given("create table on three nodes"):
         table_name = "test_local" + getuid()
         create_table_query = f"""           
-                CREATE TABLE default.{table_name} (name String, date Date, sign Int8) 
-                ENGINE = CollapsingMergeTree(sign) 
-                PARTITION BY date 
-                ORDER BY name 
-                SETTINGS index_granularity = 8192
-                """
+            CREATE TABLE default.{table_name} (name String, date Date, sign Int8) 
+            ENGINE = CollapsingMergeTree(sign) 
+            PARTITION BY date 
+            ORDER BY name 
+            SETTINGS index_granularity = 8192
+        """
         for node in self.context.nodes:
             node.query(create_table_query)
 
-    with And("I create distributed table on first server"):
+    with And("I create distributed table on first node"):
         distributed_table_name = "test_distributed" + getuid()
         create_distributed = f"""
-                CREATE TABLE {distributed_table_name} 
-                ENGINE = Distributed(sharded_cluster, currentDatabase(), {table_name}, rand64());
-                """
-        self.context.node_1.query(create_distributed)
+            CREATE TABLE {distributed_table_name} 
+            ENGINE = Distributed(sharded_cluster, currentDatabase(), {table_name}, rand64());
+        """
+        node_1.query(create_distributed)
 
-    with And("I insert data in the distributed table"):
+    with And("insert data into the distributed table"):
         insert_query = f"""
-                INSERT INTO {distributed_table_name} 
-                VALUES ('11', '2024-01-01', 1),\
-                ('3', '2024-01-01', 1),\
-                ('2', '2024-05-01', 1),\
-                ('8', '2024-01-05', 1),\
-                ('5', '2024-01-06', 1),\
-                ('4', '2024-02-01', 1),\
-                ('6', '2024-04-01', 1),\
-                ('16', '2024-01-11', 1),\
-                ('12', '2024-01-12', 1),\
-                ('1', '2024-08-01', 1),\
-                ('9', '2024-11-01', 1)\
-                """
-        self.context.node_1.query(insert_query)
+            INSERT INTO {distributed_table_name} 
+            VALUES ('11', '2024-01-01', 1),
+            ('3', '2024-01-01', 1),\
+            ('2', '2024-05-01', 1),\
+            ('8', '2024-01-05', 1),\
+            ('5', '2024-01-06', 1),\
+            ('4', '2024-02-01', 1),\
+            ('6', '2024-04-01', 1),\
+            ('16', '2024-01-11', 1),\
+            ('12', '2024-01-12', 1),\
+            ('1', '2024-08-01', 1),\
+            ('9', '2024-11-01', 1)\
+        """
+        node_1.query(insert_query)
 
-    with And("I check that data was inserted"):
+    with And("check that data was inserted into the distributed table"):
         select_query = f"""
-                SELECT *, hostName()
-                FROM default.{distributed_table_name}
-                """
-        self.context.node_1.query(select_query)
+            SELECT count()
+            FROM default.{distributed_table_name}
+            FORMAT TabSeparated
+        """
+        for retry in retries(timeout=60, delay=2):
+            with retry:
+                count = node_1.query(select_query)
+                assert count.output == "11", error()
 
-    with Then("I select count() without using `merge`"):
+    with Then("select count() without using `merge`"):
         correct_query = f"""
-                SELECT count()
-                FROM default.{distributed_table_name}
-                WHERE name GLOBAL IN (
-                SELECT name
-                FROM default.{distributed_table_name}
-                )
-                """
-        without_merge = self.context.node_1.query(correct_query).output
+            SELECT count()
+            FROM default.{distributed_table_name}
+            WHERE name GLOBAL IN (
+            SELECT name
+            FROM default.{distributed_table_name}
+            )
+        """
+        without_merge = self.context.node_1.query(correct_query)
 
-    with And("I select count() with using `merge` and expect the same result"):
+    with And("select count() with using `merge` and expect the same result"):
         incorrect_query = f"""
-                SELECT count()
-                FROM merge('default', '{distributed_table_name}')
-                WHERE name GLOBAL IN (
+            SELECT count()
+            FROM merge('default', '{distributed_table_name}')
+            WHERE name GLOBAL IN (
                 SELECT name
                 FROM default.{distributed_table_name}
-                )
-                """
+            )
+        """
+        with_merge = self.context.node_1.query(incorrect_query)
 
-        with_merge = self.context.node_1.query(incorrect_query).output
-
-        assert with_merge == without_merge, error()
+    with And("check that the results without and with `merge` are the same"):
+        assert with_merge.output == without_merge.output, error()
 
 
 @TestFeature
