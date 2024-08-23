b3428d60ca9528eca67e5c16eab3a5e0640beb4d
alsu <alsugilyazova1@mail.ru>
2024-08-23 16:29:28 +0300

    add server setting test multi auth methods feature

---
+++ rbac/tests/privileges/multiple_auth_methods/server_setting/feature.py
@@ -0,0 +1,34 @@
+from testflows.core import *
+from rbac.helper.common import *
+
+
+@TestFeature
+@Name("server setting max_authentication_methods_per_user")
+def feature(self):
+    """Check different manipulations with user's authentication methods with
+    different values of `max_authentication_methods_per_user` setting."""
+    Feature(
+        run=load(
+            "rbac.tests.privileges.multiple_auth_methods.server_setting.server_setting_create_user",
+            "feature",
+        ),
+    )
+    Feature(
+        run=load(
+            "rbac.tests.privileges.multiple_auth_methods.server_setting.server_setting_alter_user",
+            "feature",
+        ),
+    )
+    Feature(
+        run=load(
+            "rbac.tests.privileges.multiple_auth_methods.server_setting.server_setting_reset_auth_methods",
+            "feature",
+        ),
+    )
+    Feature(
+        run=load(
+            "rbac.tests.privileges.multiple_auth_methods.server_setting.server_setting_add_identified",
+            "feature",
+        ),
+    )
+    
