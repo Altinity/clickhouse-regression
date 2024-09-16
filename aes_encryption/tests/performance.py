07801b2e17c72b97c6b28f4809d54e3a2a6114a9
alsu <alsugilyazova1@mail.ru>
2024-09-16 14:10:36 +0200

    update expected_time in aes_encryption performance test

--- aes_encryption/tests/performance.py
+++ aes_encryption/tests/performance.py
@@ -43,7 +43,7 @@ def encryption_decryption(self):
         if mode == "'aes-128-ecb'":
             expected_time = 11
         elif "gcm" in mode:
-            expected_time = 16.5
+            expected_time = 18
         else:
             expected_time = 6.8
 
