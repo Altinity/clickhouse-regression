cb9728776abc9f9fd3d24a5568abb26a8f1a28d4
alsu <alsugilyazova1@mail.ru>
2024-09-24 09:31:44 +0200

    fix exception message in window functions suite

--- window_functions/tests/common.py
+++ window_functions/tests/common.py
@@ -106,7 +106,7 @@ def invalid_expression_error():
     if check_clickhouse_version(">=24.9")(current()):
         return (47, "DB::Exception: Unknown expression identifier `foo`")
     elif is_with_analyzer(node=current().context.node):
-        return (47, "DB::Exception: Unknown expression or function identifier 'foo'")
+        return (47, "DB::Exception: Unknown expression identifier 'foo'")
     else:
         return (47, "Exception: Missing columns: 'foo'")
 
