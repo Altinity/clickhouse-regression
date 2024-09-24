a3a1cc559d7ea0f30e824d651a64b0e7108fe54f
alsu <alsugilyazova1@mail.ru>
2024-09-24 08:33:53 +0200

    update version_when_attach_partition_with_different_keys_merged, still not merged

--- alter/table/attach_partition/common.py
+++ alter/table/attach_partition/common.py
@@ -5,7 +5,7 @@ from testflows.core import *
 
 from helpers.tables import *
 
-version_when_attach_partition_with_different_keys_merged = "24.10"
+version_when_attach_partition_with_different_keys_merged = "24.11"
 
 
 def current_cpu():
