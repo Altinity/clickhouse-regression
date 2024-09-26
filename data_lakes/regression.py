9b9dc68e79d9b1904fc22bccde9ea5986ad0a4cd
alsu <alsugilyazova1@mail.ru>
2024-09-26 18:31:36 +0200

    create data_lakes suite: initial commit

---
+++ data_lakes/regression.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+from testflows.core import *
+
+append_path(sys.path, "..")
+
+from helpers.tables import *
+from helpers.argparser import argparser, CaptureClusterArgs
+from helpers.cluster import create_cluster
+from helpers.common import experimental_analyzer
+
+xfails = {}
+
+ffails = {}
+
+
+@TestModule
+@ArgumentParser(argparser)
+@Name("data lakes")
+@XFails(xfails)
+@FFails(ffails)
+@CaptureClusterArgs
+def regression(
+    self,
+    cluster_args,
+    clickhouse_version,
+    stress=None,
+    with_analyzer=False,
+):
+    """Data lakes regression suite."""
+    nodes = {"clickhouse": ("clickhouse1", "clickhouse2", "clickhouse3")}
+
+    self.context.clickhouse_version = clickhouse_version
+
+    if stress is not None:
+        self.context.stress = stress
+
+    with Given("docker-compose cluster"):
+        cluster = create_cluster(
+            **cluster_args,
+            nodes=nodes,
+            docker_compose_project_dir=os.path.join(
+                current_dir(), os.path.basename(current_dir()) + "_env"
+            ),
+            configs_dir=current_dir(),
+        )
+        self.context.cluster = cluster
+        self.context.node = cluster.node("clickhouse1")
+
+    with And("I enable or disable experimental analyzer if needed"):
+        for node in nodes["clickhouse"]:
+            experimental_analyzer(node=cluster.node(node), with_analyzer=with_analyzer)
+
+
+if main():
+    regression()
