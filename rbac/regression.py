18628cf59d849924414adb7fcc471c5b4bd8288e
alsu <alsugilyazova1@mail.ru>
2024-10-20 23:02:18 +0200

    update rbac ffails

--- rbac/regression.py
+++ rbac/regression.py
@@ -337,13 +337,13 @@ ffails = {
     ),
     "/rbac/multiple authentication methods/valid until clause combinatorics": (
         Skip,
-        "multiple authentication methods were introduced in 24.10",
-        check_clickhouse_version("<24.10"),
+        "multiple authentication methods were introduced in 24.11",
+        check_clickhouse_version("<24.11"),
     ),
     "/rbac/multiple authentication methods/valid until": (
         Skip,
-        "multiple authentication methods were introduced in 24.10",
-        check_clickhouse_version("<24.10"),
+        "multiple authentication methods were introduced in 24.11",
+        check_clickhouse_version("<24.11"),
     ),
 }
 
