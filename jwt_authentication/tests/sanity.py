5f634cb872a31c856b8dc80229167728a7960411
alsu <alsugilyazova1@mail.ru>
2024-11-08 15:58:40 +0100

    remove unused functions from jwt authentication suite

--- jwt_authentication/tests/sanity.py
+++ jwt_authentication/tests/sanity.py
@@ -8,36 +8,6 @@ from helpers.common import getuid
 import jwt_authentication.tests.steps as steps
 
 
-def curl_with_jwt(token, ip, https=False):
-    http_prefix = "https" if https else "http"
-    curl = f'curl -H "X-ClickHouse-JWT-Token: Bearer {token}" "{http_prefix}://{ip}:8123/?query=SELECT%20currentUser()"'
-    return curl
-
-
-def test_static_key():
-    res = (
-        current()
-        .context.node.command(
-            curl_with_jwt(
-                token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqd3RfdXNlciJ9."
-                "kfivQ8qD_oY0UvihydeadD7xvuiO3zSmhFOc_SGbEPQ",
-                ip="localhost",
-            ),
-        )
-        .output
-    )
-    assert res == "jwt_user"
-
-
-def test_static_key_2():
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqd3RfdXNlciJ9.kfivQ8qD_oY0UvihydeadD7xvuiO3zSmhFOc_SGbEPQ"
-    res = current().context.node.query(
-        "SELECT currentUser()", settings=[("jwt", token)]
-    )
-
-    assert res.output == "jwt_user"
-
-
 @TestScenario
 @Requirements(RQ_SRS_042_JWT_StaticKey)
 def check_static_key(self):
@@ -54,7 +24,6 @@ def check_static_key(self):
     with Then("check jwt authentication"):
         steps.check_jwt_login(user_name=user_name, token=token)
         steps.check_curl_jwt_login(user_name=user_name, token=token)
-        pause()
 
 
 @TestScenario
