2a86651e6873391aa92ae823cddb37245f8786f7
alsu <alsugilyazova1@mail.ru>
2024-11-13 00:43:30 +0100

    test different algorithms for jwt authentication

--- jwt_authentication/tests/static_key/feature.py
+++ jwt_authentication/tests/static_key/feature.py
@@ -9,3 +9,4 @@ def feature(self, node="clickhouse1"):
 
     Feature(run=load("jwt_authentication.tests.static_key.sanity", "feature"))
     Feature(run=load("jwt_authentication.tests.static_key.recreate_user", "feature"))
+    Feature(run=load("jwt_authentication.tests.static_key.different_algorithms", "feature"))
