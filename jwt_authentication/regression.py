066fe9386733bbb15411628ec3354e0478ee6bb8
alsu <alsugilyazova1@mail.ru>
2024-11-14 15:00:37 +0100

    update jwt regression.py

--- jwt_authentication/regression.py
+++ jwt_authentication/regression.py
@@ -11,14 +11,18 @@ from helpers.argparser import argparser, CaptureClusterArgs
 
 
 xfails = {
-    # decrypt
     "/jwt authentication/static key/different algorithms/adding validator asymmetric algorithm/check ES256K algorithm/*": [
         (Fail, "ES256K algorithm is not working, need to investigate")
     ],
-    "/jwt authentication/static key/different algorithms/adding validator asymmetric algorithm/check Ed448 algorithm/*": (
-        Fail,
-        "Ed448 algorithm is not working, need to investigate",
-    ),
+    "/jwt authentication/static key/different algorithms/adding validator asymmetric algorithm/check Ed448 algorithm/*": [
+        (
+            Fail,
+            "Ed448 algorithm is not working, need to investigate",
+        )
+    ],
+    "/jwt authentication/static jwks/feature/login fails with mismatched algorithm/*": [
+        (Fail, "Needs investigation")
+    ],
 }
 
 ffails = {
@@ -57,7 +61,10 @@ def regression(
         )
         self.context.cluster = cluster
 
+    self.context.node = self.context.cluster.node("clickhouse1")
+
     Scenario(run=load("jwt_authentication.tests.static_key.feature", "feature"))
+    Scenario(run=load("jwt_authentication.tests.jwks.feature", "feature"))
 
 
 if main():
