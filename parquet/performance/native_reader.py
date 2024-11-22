42ceb1674c1eeef7177392cbc9908f89ed19f2e8
Selfeer <davit.mnatobishvili.1@btu.edu.ge>
2024-11-22 14:28:03 +0400

    add queries

--- parquet/performance/native_reader.py
+++ parquet/performance/native_reader.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-import os
-import sys
 import subprocess
-import re
+import sys
 
 from testflows.core import *
 
 append_path(sys.path, "../..")
 
-from helpers.cluster import short_hash, Shell, Cluster, get_binary_from_docker_container
-from helpers.argparser import CaptureClusterArgs
-from parquet.performance.tests.duckdb.reports import (
-    write_to_csv,
-    convert_to_markdown,
-    create_bar_chart,
-)
+from helpers.cluster import get_binary_from_docker_container
 
 
 def argparser(parser):
-    """Default argument parser for regressions."""
+    """Default argument parser for native reader checks."""
     parser.add_argument(
         "--clickhouse",
         "--clickhouse-package-path",
@@ -27,7 +19,7 @@ def argparser(parser):
         dest="clickhouse_path",
         help="Path to ClickHouse package or binary.",
         metavar="PATH",
-        default="docker://altinity/clickhouse-server:24.3.12.76.altinitystable",
+        default="docker://clickhouse/clickhouse-server:head",
     )
 
 
@@ -39,20 +31,6 @@ def get_binary_and_copy_here(self, clickhouse_binary_path):
     )
 
     return binary_path
-
-
-@TestStep(Given)
-def run_local_query(self, query, clickhouse_binary):
-    """Run a query locally."""
-    result = subprocess.run(
-        [clickhouse_binary, "-n", "--print-profile-events"],
-        input=query,
-        text=True,
-        capture_output=True,
-    )
-
-    return result
-
 
 @TestModule
 @Name("native reader")
