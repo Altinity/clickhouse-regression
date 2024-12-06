e03f15a2e93f76e0cfa704151c18ceaa5edaf531
Selfeer <davit.mnatobishvili.1@btu.edu.ge>
2024-12-06 15:39:52 +0400

    update performance suite

--- parquet/performance/native_reader.py
+++ parquet/performance/native_reader.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
-
+import os
 from testflows.core import *
 
 append_path(sys.path, "../..")
 
-from helpers.cluster import get_binary_from_docker_container
+from helpers.cluster import get_binary_from_docker_container, download_http_binary, unpack_deb, unpack_tgz
 
 
 def argparser(parser):
@@ -22,6 +22,24 @@ def argparser(parser):
         default="docker://clickhouse/clickhouse-server:head",
     )
 
+@TestStep(Given)
+def get_binary_from_deb(self, source):
+    self.context.binary_path = unpack_deb(
+        deb_binary_path=source,
+        program_name="clickhouse",
+    )
+
+@TestStep(Given)
+def get_binary_from_package(self, source):
+    self.context.package_path = source
+    if source.endswith(".deb"):
+        get_binary_from_deb(source)
+    elif source.endswith(".rpm"):
+        pass
+    elif source.endswith(".tgz"):
+        self.context.binary_path = os.path.join(
+            unpack_tgz(source), "usr/bin", "clickhouse"
+        )
 
 @TestStep(Given)
 def get_binary_clickhouse_binary(self, clickhouse_binary_path):
@@ -32,7 +50,17 @@ def get_binary_clickhouse_binary(self, clickhouse_binary_path):
 
     return binary_path
 
+@TestStep(Given)
+def get_binary_from_http(self, url):
+    """Get the binary from the HTTP URL."""
+    package_formats = (".deb", ".rpm", ".tgz")
+    binary_path = download_http_binary(url)
+    if binary_path.endswith(package_formats):
+        get_binary_from_package(binary_path)
+    else:
+        binary_path = self.context.binary_path
 
+    return binary_path
 @TestModule
 @Name("native reader")
 @ArgumentParser(argparser)
@@ -40,9 +68,12 @@ def module(self, clickhouse_path=None):
     """Running performance tests for Parquet native reader in ClickHouse."""
 
     with Given("I get the ClickHouse binary from the docker container"):
-        self.context.clickhouse_binary = get_binary_clickhouse_binary(
-            clickhouse_binary_path=clickhouse_path
-        )
+        if clickhouse_path.startswith("docker://"):
+            self.context.clickhouse_binary = get_binary_clickhouse_binary(
+                clickhouse_binary_path=clickhouse_path
+            )
+        elif clickhouse_path.startswith(("http://", "https://")):
+            self.context.clickhouse_binary = get_binary_from_http(url=clickhouse_path)
 
     Feature(run=load("parquet.performance.tests.native_reader.feature", "feature"))
 
