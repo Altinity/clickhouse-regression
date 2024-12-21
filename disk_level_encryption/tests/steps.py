c4ac2441579e6f8839ceeb39a8b0d60f19f43e9a
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-12-21 10:25:19 -0500

    fix syntaxwarning in disk encryption suite

--- disk_level_encryption/tests/steps.py
+++ disk_level_encryption/tests/steps.py
@@ -448,7 +448,7 @@ def get_random_string(self, length, cluster=None, steps=True, *args, **kwargs):
     with tempfile.NamedTemporaryFile("w+", encoding="utf-8") as fd:
         cluster.command(
             None,
-            f"cat /dev/urandom | tr -dc 'A-Za-z0-9#$&()*+,-./:;<=>?@[\]^_~' | head -c {length} > {fd.name}",
+            rf"cat /dev/urandom | tr -dc 'A-Za-z0-9#$&()*+,-./:;<=>?@[\]^_~' | head -c {length} > {fd.name}",
             steps=steps,
             *args,
             **kwargs,
