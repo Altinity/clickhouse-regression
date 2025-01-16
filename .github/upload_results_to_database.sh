ba84cd3527ec0bba62f12a49ac7893e114d06b87
Stuart <146047128+strtgbb@users.noreply.github.com>
2025-01-15 21:34:35 -0500

    Convert uploader to http client

--- .github/upload_results_to_database.sh
+++ .github/upload_results_to_database.sh
@@ -5,6 +5,6 @@ set -x
 
 if [[ $1 == 1 ]];
 then
-    ./.github/upload_results_to_database.py -o nice --log-file raw.log --db-name="gh-data" --db-port=9440 --secure --no-verify --table="clickhouse_regression_results" --log uploader.log
+    ./.github/upload_results_to_database.py -o nice --log-file raw.log --db-name="gh-data" --db-port=8443 --secure --no-verify --table="clickhouse_regression_results" --log uploader.log
     aws s3 cp uploader.log $SUITE_REPORT_BUCKET_PATH/uploader.log
 fi
