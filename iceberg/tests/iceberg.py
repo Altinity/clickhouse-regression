62b367e6e7de9cf79b478487844301a321e5325c
alsu <alsugilyazova1@mail.ru>
2025-01-17 16:13:47 +0100

    some updated to iceberg tests

--- iceberg/tests/iceberg.py
+++ iceberg/tests/iceberg.py
@@ -1,13 +1,14 @@
 from testflows.core import *
 from testflows.asserts import error
-from clickhouse_driver import Client
-import boto3
+
 from pyiceberg.catalog import load_catalog
 from pyiceberg.table import Table
 from pyiceberg.schema import Schema
 from pyiceberg.types import (
     IntegerType,
     NestedField,
+    LongType,
+    StringType,
 )
 from pyiceberg.partitioning import PartitionField, PartitionSpec
 from pyiceberg.transforms import DayTransform, IdentityTransform
@@ -16,14 +17,14 @@ from minio import Minio
 
 import requests
 import urllib3
+import pyarrow as pa
+import pandas as pd
 
 from helpers.common import getuid
 
 
-# S3 Configuration
 S3_BUCKET = "my-bucket"
-CATALOG_NAME = "s3_catalog"
-TABLE_NAME = "my_table"
+CATALOG_NAME = "nessie"
 
 BASE_URL = "http://rest:8181/v1"
 BASE_URL_LOCAL = "http://localhost:8182/v1"
@@ -40,48 +41,59 @@ def list_namespaces():
 
 @TestScenario
 def sanity(self):
-    minio_client = Minio(
-        f"localhost:9001",
-        access_key="minio",
-        secret_key="minio123",
-        secure=False,
-        http_client=urllib3.PoolManager(cert_reqs="CERT_NONE"),
-    )
+    with Given("create minio client"):
+        minio_client = Minio(
+            f"localhost:9001",
+            access_key="minio",
+            secret_key="minio123",
+            secure=False,
+            http_client=urllib3.PoolManager(cert_reqs="CERT_NONE"),
+        )
 
-    if not minio_client.bucket_exists(S3_BUCKET):
-        minio_client.make_bucket(S3_BUCKET)
+    with And("create bucket"):
+        if not minio_client.bucket_exists(S3_BUCKET):
+            minio_client.make_bucket(S3_BUCKET)
 
-    schema = Schema(
-        NestedField(
-            field_id=1, name="num", field_type=IntegerType(), required=False
-        ),
-    )
+    with When("create catalog"):
+        catalog = load_catalog(
+            CATALOG_NAME,
+            **{
+                "uri": "http://localhost:19120/iceberg/main/",
+            },
+        )
 
-    catalog = load_catalog(
-        "nessie",
-        **{
-            "uri": "http://localhost:19120/iceberg/main/",
-        },
-    )
-    catalog.create_namespace("demo")
-    
-    catalog.create_table("demo.sample_table", schema)
+    with And("create namespace"):
+        catalog.create_namespace("demo")
 
-    table = catalog.load_table("demo.sample_table")
+    with And("define schema and create table"):
+        schema = Schema(
+            NestedField(1, "id", LongType(), required=True),
+            NestedField(2, "name", StringType(), required=False),
+        )
 
-    # Get the table schema
-    schema = table.schema()
-    note(f"Table Schema: {schema}")
+        catalog.create_table("demo.sample_table", schema)
 
-    # Get the table properties
-    properties = table.properties
-    note(f"Table Properties: {properties}")
-   
+    with And("print table info"):
+        table = catalog.load_table("demo.sample_table")
+        note(f"Table Schema: {table.schema()}")
+        note(f"Table Properties: {table.properties}")
+        note(f"Table Location: {table.location()}")
+
+    with And("insert rows into table"):
+        data = pa.Table.from_pydict(
+            {"id": [1, 2, 3], "name": ["Alice", "Bob", "Charlie"]}
+        )
+
+        table = catalog.load_table("demo.sample_table")
+        table.append(data)
+
+    with And("print table content"):
+        result = table.scan().to_arrow()
+        note(result)
 
 
 @TestFeature
 def feature(self, servers=None, node="clickhouse1"):
     self.context.node = self.context.cluster.node(node)
 
-    for scenario in loads(current_module(), Scenario):
-        scenario()
+    Scenario(run=sanity)
