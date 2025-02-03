fc0fe7b985b99fe5ee397d2f98c85776b853ed2a
alsu <alsugilyazova1@mail.ru>
2025-02-03 18:45:06 +0100

    separate steps between different functionalities

--- iceberg/tests/feature.py
+++ iceberg/tests/feature.py
@@ -9,6 +9,11 @@ def feature(self):
     """Check different ways of reading (and writing later) data from/to Iceberg
     tables in Clickhouse."""
 
-    Feature(run=load("iceberg.tests.s3_table_function", "feature"))
-    Feature(run=load("iceberg.tests.icebergS3_table_function", "icebergS3_table_function"))
-    Feature(run=load("iceberg.tests.iceberg_engine", "feature"))
+    Feature(run=load("iceberg.tests.s3_table_function.s3_table_function", "feature"))
+    Feature(
+        run=load(
+            "iceberg.tests.icebergS3_table_function.icebergS3_table_function",
+            "icebergS3_table_function",
+        )
+    )
+    Feature(run=load("iceberg.tests.iceberg_engine.iceberg_engine", "feature"))
---
+++ iceberg/tests/s3_table_function/steps.py
@@ -0,0 +1,17 @@
+from testflows.core import *
+
+
+@TestStep(Then)
+def read_data_with_s3_table_function(
+    self, endpoint, s3_access_key_id, s3_secret_access_key, node=None, columns="*"
+):
+    if node is None:
+        node = self.context.node
+
+    result = node.query(
+        f"SELECT {columns} FROM s3('{endpoint}', '{s3_access_key_id}', '{s3_secret_access_key}')"
+    )
+    node.query(
+        f"SELECT count() FROM s3('{endpoint}', '{s3_access_key_id}', '{s3_secret_access_key}')"
+    )
+    return result
