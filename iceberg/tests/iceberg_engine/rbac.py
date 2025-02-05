d9376db6ccb88d0cd9c9b4d035b2a83d40725d89
alsu <alsugilyazova1@mail.ru>
2025-02-05 17:34:45 +0100

    add basic rbac test to iceberg suite

---
+++ iceberg/tests/iceberg_engine/rbac.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+from testflows.core import *
+from testflows.asserts import error
+
+import pyarrow as pa
+
+import iceberg.tests.common_steps as common_steps
+import iceberg.tests.iceberg_engine.steps as steps
+
+from helpers.common import create_user, getuid
+
+
+@TestScenario
+def sanity(self, minio_root_user, minio_root_password):
+    """Test basic RBAC with tables from Iceberg engine."""
+    catalog_type = "rest"
+    namespace = "iceberg"
+    table_name = "name"
+
+    with Given("create catalog"):
+        catalog = common_steps.create_catalog(
+            uri="http://localhost:8182/",
+            catalog_type=catalog_type,
+            s3_endpoint="http://localhost:9002",
+            s3_access_key_id=minio_root_user,
+            s3_secret_access_key=minio_root_password,
+        )
+
+    with And("create namespace"):
+        common_steps.create_namespace(catalog=catalog, namespace=namespace)
+
+    with And(f"delete table {namespace}.{table_name} if already exists"):
+        common_steps.drop_iceberg_table(
+            catalog=catalog, namespace=namespace, table_name=table_name
+        )
+
+    with When(f"define schema and create {namespace}.{table_name} table"):
+        table = common_steps.create_iceberg_table_with_three_columns(
+            catalog=catalog, namespace=namespace, table_name=table_name
+        )
+
+    with Then("create database with Iceberg engine"):
+        database_name = "datalake"
+        steps.drop_database(database_name=database_name)
+        steps.create_experimental_iceberg_database(
+            namespace=namespace,
+            database_name=database_name,
+            rest_catalog_url="http://rest:8181/v1",
+            s3_access_key_id=minio_root_user,
+            s3_secret_access_key=minio_root_password,
+            catalog_type=catalog_type,
+            storage_endpoint="http://minio:9000/warehouse",
+        )
+
+    with And(f"insert data into {namespace}.{table_name} table"):
+        df = pa.Table.from_pylist(
+            [
+                {"name": "Alice", "double": 195.23, "integer": 20},
+                {"name": "Bob", "double": 123.45, "integer": 30},
+                {"name": "Charlie", "double": 67.89, "integer": 40},
+            ]
+        )
+        table.append(df)
+
+    with And("read data in clickhouse from the previously created table"):
+        result = steps.read_data_from_clickhouse_iceberg_table(
+            database_name=database_name, namespace=namespace, table_name=table_name
+        )
+        assert "Alice	195.23	20" in result.output, error()
+        assert "Bob	123.45	30" in result.output, error()
+        assert "Charlie	67.89	40" in result.output, error()
+
+    with Then("create new clickhouse user"):
+        user_name = f"test_user_{getuid()}"
+        create_user(name=user_name)
+
+    with And("try to read from iceberg table with new user"):
+        exitcode, message = 241, f"DB::Exception: {user_name}: Not enough privileges."
+        result = steps.read_data_from_clickhouse_iceberg_table(
+            database_name=database_name,
+            namespace=namespace,
+            table_name=table_name,
+            user=user_name,
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("grant read access to the table"):
+        self.context.node.query(
+            f"GRANT SELECT ON {database_name}.\`{namespace}.{table_name}\` TO {user_name}"
+        )
+
+    with And("try to read from iceberg table with new user"):
+        result = steps.read_data_from_clickhouse_iceberg_table(
+            database_name=database_name,
+            namespace=namespace,
+            table_name=table_name,
+            user=user_name,
+        )
+
+
+@TestFeature
+@Name("rbac")
+def feature(self, minio_root_user, minio_root_password):
+    Scenario(test=sanity)(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
+    )
