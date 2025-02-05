9f0a606d0cf8eb9fb97a5541ebb89aea74870ed5
Stuart <146047128+strtgbb@users.noreply.github.com>
2025-02-04 22:07:39 -0500

    make set_builds_status message fit in 140 chars

--- .github/set_builds_status.py
+++ .github/set_builds_status.py
@@ -23,8 +23,9 @@ state = {
 }.get(job_outcome, "pending")
 
 # Get log summary
+# 140 characters max
 status_message = (
-    subprocess.getoutput("tfs transform new-fails raw.log").strip() or "Job completed"
+    subprocess.getoutput("tfs show totals raw.log").strip()[:140] or "Job completed"
 )
 
 # GitHub API request to set commit status
