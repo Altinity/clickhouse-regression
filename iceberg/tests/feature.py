86505ca900da0e559796e33a41b0fb449cb5b639
alsu <alsugilyazova1@mail.ru>
2025-02-05 16:48:00 +0100

    correct passing of s3 secrets

--- iceberg/tests/feature.py
+++ iceberg/tests/feature.py
@@ -5,15 +5,19 @@ from iceberg.requirements import *
 
 @TestFeature
 @Name("iceberg integration")
-def feature(self):
+def feature(self, minio_root_user=None, minio_root_password=None):
     """Check different ways of reading (and writing later) data from/to Iceberg
     tables in Clickhouse."""
 
-    Feature(run=load("iceberg.tests.s3_table_function.s3_table_function", "feature"))
+    Feature(test=load("iceberg.tests.s3_table_function.s3_table_function", "feature"))(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
+    )
     Feature(
-        run=load(
+        test=load(
             "iceberg.tests.icebergS3_table_function.icebergS3_table_function",
             "icebergS3_table_function",
         )
+    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    Feature(test=load("iceberg.tests.iceberg_engine.iceberg_engine", "feature"))(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
     )
-    Feature(run=load("iceberg.tests.iceberg_engine.iceberg_engine", "feature"))
