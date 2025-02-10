5b55491ea0ae4e82eada7cdfd109aca876325276
antip00 <antipov@altinity.com>
2025-02-10 16:45:58 +0300

    Adding fails.log.txt to s3 bucket.

--- .github/create_and_upload_logs.sh
+++ .github/create_and_upload_logs.sh
@@ -6,6 +6,7 @@ sudo chown -R $(whoami):$(whoami) $SUITE/
 echo $version > version.log.txt
 echo "https://gitlab.com/altinity-qa/clickhouse/cicd/clickhouse-regression/-/pipelines/$GITHUB_RUN_ID" > pipeline_url.log.txt
 tfs --debug --no-colors transform nice-new-fails raw.log nice-new-fails.log.txt
+tfs --debug --no-colors transform fails raw.log fails.log.txt
 tfs --debug --no-colors report results -a "$SUITE_REPORT_INDEX_URL" raw.log - $confidential --copyright "Altinity Inc." --logo ./altinity.png | tfs --debug --no-colors document convert > report.html
 echo "Re-compress the raw.log"
 cat raw.log | xzcat | xz -z -T $(nproc) - > raw.log.2
@@ -19,6 +20,7 @@ then
     ./retry.sh 5 30 aws s3 cp version.log.txt $SUITE_REPORT_BUCKET_PATH/version.log.txt --content-type "\"text/plain; charset=utf-8\""
     ./retry.sh 5 30 aws s3 cp raw.log $SUITE_REPORT_BUCKET_PATH/raw.log
     ./retry.sh 5 30 aws s3 cp nice-new-fails.log.txt $SUITE_REPORT_BUCKET_PATH/nice-new-fails.log.txt --content-type "\"text/plain; charset=utf-8\""
+    ./retry.sh 5 30 aws s3 cp fails.log.txt $SUITE_REPORT_BUCKET_PATH/fails.log.txt --content-type "\"text/plain; charset=utf-8\""
     ./retry.sh 5 30 aws s3 cp report.html $SUITE_REPORT_BUCKET_PATH/report.html
     sudo rm --recursive --force $SUITE/_instances/*/database/
     ./retry.sh 5 30 "aws s3 cp --recursive . $SUITE_REPORT_BUCKET_PATH/"' --exclude "*" --include "*/_instances/*.log" --content-type "\"text/plain; charset=utf-8\"" --no-follow-symlinks'
