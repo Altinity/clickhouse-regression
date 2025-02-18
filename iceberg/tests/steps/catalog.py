81a51486819a36f59676d9352bc16fcf265a623e
alsu <alsugilyazova1@mail.ru>
2025-02-18 13:51:24 +0100

    update iceberg catalog common functions

--- iceberg/tests/steps/catalog.py
+++ iceberg/tests/steps/catalog.py
@@ -8,6 +8,17 @@ from pyiceberg.types import (
     StringType,
     NestedField,
     LongType,
+    BooleanType,
+    IntegerType,
+    FloatType,
+    DecimalType,
+    UUIDType,
+    FixedType,
+    BinaryType,
+    DateType,
+    TimeType,
+    TimestampType,
+    TimestamptzType,
 )
 from pyiceberg.partitioning import PartitionSpec, PartitionField
 from pyiceberg.table.sorting import SortOrder, SortField
@@ -86,6 +97,7 @@ def create_iceberg_table(
     location,
     partition_spec,
     sort_order=None,
+    format_version="2",
 ):
     try:
         table = catalog.create_table(
@@ -94,6 +106,7 @@ def create_iceberg_table(
             location=location,
             sort_order=sort_order,
             partition_spec=partition_spec,
+            properties={"format-version": format_version},
         )
         yield table
     finally:
@@ -110,6 +123,43 @@ def create_iceberg_table_with_three_columns(self, catalog, namespace, table_name
         NestedField(field_id=1, name="name", field_type=StringType(), required=False),
         NestedField(field_id=2, name="double", field_type=DoubleType(), required=False),
         NestedField(field_id=3, name="integer", field_type=LongType(), required=False),
+    )
+    partition_spec = PartitionSpec(
+        PartitionField(
+            source_id=1,
+            field_id=1001,
+            transform=IdentityTransform(),
+            name="symbol_partition",
+        ),
+    )
+    sort_order = SortOrder(SortField(source_id=1, transform=IdentityTransform()))
+    table = create_iceberg_table(
+        catalog=catalog,
+        namespace=namespace,
+        table_name=table_name,
+        schema=schema,
+        location="s3://warehouse/data",
+        partition_spec=partition_spec,
+        sort_order=sort_order,
+    )
+    return table
+
+
+@TestStep(Given)
+def create_iceberg_table_with_five_columns(self, catalog, namespace, table_name):
+    """Define schema, partition spec, sort order and create iceberg table with three columns."""
+    schema = Schema(
+        NestedField(
+            field_id=1, name="boolean_col", field_type=BooleanType(), required=False
+        ),
+        NestedField(field_id=2, name="long_col", field_type=LongType(), required=False),
+        NestedField(
+            field_id=3, name="double_col", field_type=DoubleType(), required=False
+        ),
+        NestedField(
+            field_id=4, name="string_col", field_type=StringType(), required=False
+        ),
+        NestedField(field_id=5, name="date_col", field_type=DateType(), required=False),
     )
     partition_spec = PartitionSpec(
         PartitionField(
