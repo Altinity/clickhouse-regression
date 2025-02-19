c00f7c6f3786041a2a0ca3c57cf230abac736e79
Selfeer <davit.mnatobishvili.1@btu.edu.ge>
2025-02-19 15:43:36 +0400

    add library of steps

---
+++ parquet/tests/steps/metadata_caching.py
@@ -0,0 +1,146 @@
+from testflows.core import *
+from parquet.tests.common import *
+from s3.tests.common import *
+from alter.stress.tests.tc_netem import network_packet_delay
+
+
+@TestStep(Given)
+def create_parquet_on_s3(self, file_name, columns, compression_type=None):
+    """Create a Parquet file on S3."""
+
+    if compression_type is None:
+        compression_type = "NONE"
+
+    full_path = self.context.uri + file_name + ".Parquet"
+
+    table = create_table(
+        name=file_name,
+        engine=f"S3('{full_path}', '{self.context.access_key_id}', '{self.context.secret_access_key}', 'Parquet', '{compression_type.lower()}')",
+        columns=columns,
+    )
+
+    return table
+
+
+@TestStep(Given)
+def create_s3_parquet_all_datatypes(self, file_name, compression_type=None):
+    """Create a Parquet file with all supported data types."""
+    columns = generate_all_column_types(include=parquet_test_columns())
+    file = create_parquet_on_s3(
+        file_name=file_name, columns=columns, compression_type=compression_type
+    )
+
+    file.insert_test_data()
+
+
+@TestStep(Given)
+def create_s3_parquet_basic_numeric_datatypes(self, file_name, compression_type=None):
+    """Create a Parquet file with basic numeric data types."""
+    columns = generate_all_basic_numeric_column_types()
+    file = create_parquet_on_s3(
+        file_name=file_name, columns=columns, compression_type=compression_type
+    )
+
+    file.insert_test_data()
+
+
+@TestStep(Given)
+def create_s3_parquet_common_datatypes(self, file_name, compression_type=None):
+    """Create a Parquet file with common data types."""
+    columns = common_columns()
+    file = create_parquet_on_s3(
+        file_name=file_name, columns=columns, compression_type=compression_type
+    )
+
+    file.insert_test_data()
+
+
+@TestStep(Given)
+def create_s3_parquet_all_map_datatypes(self, file_name, compression_type=None):
+    """Create a Parquet file with all supported map data types."""
+    columns = generate_all_map_column_types()
+    file = create_parquet_on_s3(
+        file_name=file_name, columns=columns, compression_type=compression_type
+    )
+
+    file.insert_test_data()
+
+
+@TestStep(Given)
+def multiple_parquet_files_with_all_datatypes(self, number_of_files):
+    """Create multiple Parquet files with all supported data types."""
+    file_names = [f"file_{getuid()}" for _ in range(number_of_files)]
+
+    with Pool(4) as pool:
+        for name in file_names:
+            By(
+                test=create_s3_parquet_all_datatypes,
+                parallel=True,
+                executor=pool,
+            )(file_name=name)
+        join()
+
+    return file_names
+
+
+@TestStep(Given)
+def multiple_parquet_files_with_common_datatypes(self, number_of_files):
+    """Create multiple Parquet files with common data types."""
+    file_names = [f"file_{getuid()}" for _ in range(number_of_files)]
+
+    with Pool(4) as pool:
+        for name in file_names:
+            By(
+                test=create_s3_parquet_common_datatypes,
+                parallel=True,
+                executor=pool,
+            )(file_name=name)
+        join()
+
+    return file_names
+
+
+@TestStep(Given)
+def multiple_parquet_files_with_basic_numeric_datatypes(self, number_of_files):
+    """Create multiple Parquet files with basic numeric data types."""
+    file_names = [f"file_{getuid()}" for _ in range(number_of_files)]
+
+    with Pool(4) as pool:
+        for name in file_names:
+            By(
+                test=create_s3_parquet_basic_numeric_datatypes,
+                parallel=True,
+                executor=pool,
+            )(file_name=name)
+        join()
+
+    return file_names
+
+
+@TestStep(Given)
+def multiple_parquet_files_with_all_map_datatypes(self, number_of_files):
+    """Create multiple Parquet files with all supported map data types."""
+    file_names = [f"file_{getuid()}" for _ in range(number_of_files)]
+
+    with Pool(4) as pool:
+        for name in file_names:
+            By(
+                test=create_s3_parquet_all_map_datatypes,
+                parallel=True,
+                executor=pool,
+            )(file_name=name)
+        join()
+
+    return file_names
+
+
+@TestStep(When)
+def select_parquet_from_s3(self, file_name, node=None):
+    """Select data from a Parquet file on S3."""
+
+    if node is None:
+        node = self.context.node
+
+    return node.query(
+        f"SELECT * FROM s3('{self.context.uri}{file_name}.Parquet', '{self.context.access_key_id}', '{self.context.secret_access_key}', 'Parquet')"
+    )
