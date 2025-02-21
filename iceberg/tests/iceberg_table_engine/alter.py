deff3b9f1a1cf0feb8e5da41ad2e08888c5a43e6
alsu <alsugilyazova1@mail.ru>
2025-02-21 01:08:01 +0100

    add check for alter statements on iceberg tables

---
+++ iceberg/tests/iceberg_table_engine/alter.py
@@ -0,0 +1,444 @@
+from testflows.core import *
+from testflows.asserts import error
+
+import iceberg.tests.steps.catalog as catalog_steps
+import iceberg.tests.steps.iceberg_table_engine as iceberg_table_engine
+
+from helpers.common import getuid
+
+
+@TestScenario
+def alter_column(self, minio_root_user, minio_root_password, node=None):
+    """Check that ALTER TABLE ADD COLUMN, DROP COLUMN, RENAME COLUMN are
+    not supported for Iceberg tables."""
+
+    if node is None:
+        node = self.context.node
+
+    with Given("create iceberg catalog and table"):
+        catalog_steps.create_catalog_and_iceberg_table_with_data(
+            minio_root_user=minio_root_user,
+            minio_root_password=minio_root_password,
+        )
+
+    with When("create table with Iceberg engine"):
+        iceberg_table_name = "iceberg_table_" + getuid()
+        iceberg_table_engine.create_table_with_iceberg_engine(
+            table_name=iceberg_table_name,
+            url="http://minio:9000/warehouse/data",
+            access_key_id=minio_root_user,
+            secret_access_key=minio_root_password,
+        )
+
+    with Then("try to add new column to ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'ADD_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} ADD COLUMN new_column UInt64",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to drop column from ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'DROP_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DROP COLUMN long_col",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to rename column in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'RENAME_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} RENAME COLUMN long_col TO new_col",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to clear column in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'DROP_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} CLEAR COLUMN long_col",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to modify column in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'MODIFY_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MODIFY COLUMN long_col UInt64",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And(
+        "try to use MODIFY COLUMN MODIFY SETTINGS on ClickHouse table with Iceberg engine"
+    ):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'MODIFY_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MODIFY COLUMN long_col MODIFY SETTING max_compress_block_size = 1048576",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And(
+        "try to use MODIFY COLUMN RESET SETTINGS on ClickHouse table with Iceberg engine"
+    ):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'MODIFY_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MODIFY COLUMN long_col RESET SETTING max_compress_block_size",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+def alter_partitions(self, minio_root_user, minio_root_password, node=None):
+    """Check that ALTER TABLE ADD PARTITION, DROP PARTITION, DROP PARTITION are
+    not supported for Iceberg tables."""
+
+    if node is None:
+        node = self.context.node
+
+    with Given("create iceberg catalog and table"):
+        catalog_steps.create_catalog_and_iceberg_table_with_data(
+            minio_root_user=minio_root_user,
+            minio_root_password=minio_root_password,
+        )
+
+    with When("create table with Iceberg engine"):
+        iceberg_table_name = "iceberg_table_" + getuid()
+        iceberg_table_engine.create_table_with_iceberg_engine(
+            table_name=iceberg_table_name,
+            url="http://minio:9000/warehouse/data",
+            access_key_id=minio_root_user,
+            secret_access_key=minio_root_password,
+        )
+
+    with Then("try to use DROP PARTITION/PART on ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DROP PARTITION ALL",
+            exitcode=exitcode,
+            message=message,
+        )
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DROP PART '1'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And(
+        "try to use DETACH PARTITION/PART from ClickHouse table with Iceberg engine"
+    ):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DETACH PARTITION 'all_2_2_0'",
+            exitcode=exitcode,
+            message=message,
+        )
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DETACH PART 'all_2_2_0'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And(
+        "try to DROP DETACHED PARTITION/PART from ClickHouse table with Iceberg engine"
+    ):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DROP DETACHED PARTITION ALL",
+            exitcode=exitcode,
+            message=message,
+        )
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DROP DETACHED PART '1'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to FORGET PARTITION from ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} FORGET PARTITION 'all_2_2_0'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to ATTACH PARTITION/PART to ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} ATTACH PARTITION 'all_2_2_0'",
+            exitcode=exitcode,
+            message=message,
+        )
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} ATTACH PART 'all_2_2_0'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to REPLACE PARTITION in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} REPLACE PARTITION 'all_2_2_0' FROM some_table",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to MOVE PARTITION in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MOVE PARTITION 'all_2_2_0' TO TABLE some_table",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And(
+        "try to CLEAR COLUMN IN PARTITION in ClickHouse table with Iceberg engine"
+    ):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'DROP_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} CLEAR COLUMN long_col IN PARTITION 'all_2_2_0'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to FREEZE PARTITION in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} FREEZE PARTITION 'all_2_2_0'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to UNFREEZE PARTITION in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} UNFREEZE PARTITION 'all_2_2_0' WITH NAME 'backup_name'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to FETCH PARTITION|PART in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} FETCH PARTITION 'all_2_2_0' FROM 'some_path'",
+            exitcode=exitcode,
+            message=message,
+        )
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} FETCH PART 'all_2_2_0' FROM 'some_path'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And(
+        "try to MOVE PARTITION|PART TO DISK in ClickHouse table with Iceberg engine"
+    ):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support partitioning. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MOVE PARTITION 'all_2_2_0' TO DISK 'some_disk_name'",
+            exitcode=exitcode,
+            message=message,
+        )
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MOVE PART 'all_2_2_0' TO DISK 'some_disk_name'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to UPDATE PARTITION in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support mutations. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} UPDATE x = x + 1 IN PARTITION 2 WHERE p = 2;",
+            exitcode=exitcode,
+            message=message,
+        )
+
+    with And("try to DELETE IN PARTITION in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support mutations. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DELETE IN PARTITION 2 WHERE p = 2;",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+def alter_comment_columns(self, minio_root_user, minio_root_password, node=None):
+    """Check that COMMENT COLUMN works for Iceberg tables."""
+
+    if node is None:
+        node = self.context.node
+
+    with Given("create iceberg catalog and table"):
+        catalog_steps.create_catalog_and_iceberg_table_with_data(
+            minio_root_user=minio_root_user,
+            minio_root_password=minio_root_password,
+        )
+
+    with When("create table with Iceberg engine"):
+        iceberg_table_name = "iceberg_table_" + getuid()
+        iceberg_table_engine.create_table_with_iceberg_engine(
+            table_name=iceberg_table_name,
+            url="http://minio:9000/warehouse/data",
+            access_key_id=minio_root_user,
+            secret_access_key=minio_root_password,
+        )
+
+    with Then("comment columns in ClickHouse table with Iceberg engine"):
+        comment = f"comment_{getuid()}"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} COMMENT COLUMN long_col '{comment}'",
+        )
+        result = node.query(f"DESCRIBE TABLE {iceberg_table_name}").output
+        assert comment in result, error()
+
+    with And("try to remove comment"):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'MODIFY_COLUMN' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MODIFY COLUMN long_col REMOVE COMMENT",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+def alter_delete(self, minio_root_user, minio_root_password, node=None):
+    """Check that DELETE FROM TABLE is not supported for Iceberg tables."""
+
+    if node is None:
+        node = self.context.node
+
+    with Given("create iceberg catalog and table"):
+        catalog_steps.create_catalog_and_iceberg_table_with_data(
+            minio_root_user=minio_root_user,
+            minio_root_password=minio_root_password,
+        )
+
+    with When("create table with Iceberg engine"):
+        iceberg_table_name = "iceberg_table_" + getuid()
+        iceberg_table_engine.create_table_with_iceberg_engine(
+            table_name=iceberg_table_name,
+            url="http://minio:9000/warehouse/data",
+            access_key_id=minio_root_user,
+            secret_access_key=minio_root_password,
+        )
+
+    with Then("try to DELETE FROM ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Table engine IcebergS3 doesn't support mutations. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} DELETE WHERE string_col LIKE '%hello%'",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+def alter_settings(self, minio_root_user, minio_root_password, node=None):
+    """Check that settings manipulation is not supported for Iceberg tables."""
+
+    if node is None:
+        node = self.context.node
+
+    with Given("create iceberg catalog and table"):
+        catalog_steps.create_catalog_and_iceberg_table_with_data(
+            minio_root_user=minio_root_user,
+            minio_root_password=minio_root_password,
+        )
+
+    with When("create table with Iceberg engine"):
+        iceberg_table_name = "iceberg_table_" + getuid()
+        iceberg_table_engine.create_table_with_iceberg_engine(
+            table_name=iceberg_table_name,
+            url="http://minio:9000/warehouse/data",
+            access_key_id=minio_root_user,
+            secret_access_key=minio_root_password,
+        )
+
+    with Then("try to modify settings in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Alter of type 'MODIFY_SETTING' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MODIFY SETTING max_part_loading_threads=8, max_parts_in_total=50000",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestScenario
+def alter_order_by(self, minio_root_user, minio_root_password, node=None):
+    """Check that ORDER BY is not supported for Iceberg tables."""
+
+    if node is None:
+        node = self.context.node
+
+    with Given("create iceberg catalog and table"):
+        catalog_steps.create_catalog_and_iceberg_table_with_data(
+            minio_root_user=minio_root_user,
+            minio_root_password=minio_root_password,
+        )
+
+    with When("create table with Iceberg engine"):
+        iceberg_table_name = "iceberg_table_" + getuid()
+        iceberg_table_engine.create_table_with_iceberg_engine(
+            table_name=iceberg_table_name,
+            url="http://minio:9000/warehouse/data",
+            access_key_id=minio_root_user,
+            secret_access_key=minio_root_password,
+        )
+
+    with Then("try to use ORDER BY in ClickHouse table with Iceberg engine"):
+        exitcode = 48
+        message = "DB::Exception: Received from localhost:9000. DB::Exception: Alter of type 'MODIFY_ORDER_BY' is not supported by storage IcebergS3. (NOT_IMPLEMENTED)"
+        node.query(
+            f"ALTER TABLE {iceberg_table_name} MODIFY ORDER BY long_col",
+            exitcode=exitcode,
+            message=message,
+        )
+
+
+@TestFeature
+def feature(self, minio_root_user, minio_root_password):
+    Scenario(test=alter_column)(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
+    )
+    Scenario(test=alter_comment_columns)(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
+    )
+    Scenario(test=alter_partitions)(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
+    )
+    Scenario(test=alter_settings)(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
+    )
+    Scenario(test=alter_delete)(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
+    )
+    Scenario(test=alter_order_by)(
+        minio_root_user=minio_root_user, minio_root_password=minio_root_password
+    )
--- iceberg/tests/iceberg_table_engine/feature.py
+++ iceberg/tests/iceberg_table_engine/feature.py
@@ -8,3 +8,9 @@ def feature(self, minio_root_user, minio_root_password):
     Feature(
         test=load("iceberg.tests.iceberg_table_engine.row_policy", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    Feature(
+        test=load("iceberg.tests.iceberg_table_engine.column_rbac", "feature"),
+    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    Feature(
+        test=load("iceberg.tests.iceberg_table_engine.alter", "feature"),
+    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
