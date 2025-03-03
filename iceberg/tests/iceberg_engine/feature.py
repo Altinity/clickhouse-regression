1082edc9aff7a916f0a595c44adc50200ccf00d0
alsu <alsugilyazova1@mail.ru>
2025-03-03 17:38:03 +0100

    turn off test that are under development

--- iceberg/tests/iceberg_engine/feature.py
+++ iceberg/tests/iceberg_engine/feature.py
@@ -14,18 +14,18 @@ def feature(self, minio_root_user, minio_root_password):
     Feature(
         test=load("iceberg.tests.iceberg_engine.named_collections", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
-    Feature(
-        test=load("iceberg.tests.iceberg_engine.changing_schema", "feature"),
-    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    # Feature(
+    #     test=load("iceberg.tests.iceberg_engine.schema_evolution", "feature"),
+    # )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
     Feature(
         test=load("iceberg.tests.iceberg_engine.row_policy", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
     Feature(
         test=load("iceberg.tests.iceberg_engine.column_rbac", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
-    Feature(
-        test=load("iceberg.tests.iceberg_engine.where_clause", "feature"),
-    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    # Feature(
+    #     test=load("iceberg.tests.iceberg_engine.where_clause", "feature"),
+    # )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
     Feature(
         test=load("iceberg.tests.iceberg_engine.alter", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
