626da098fce0a4723df58aa42318c2bd42ba436a
alsu <alsugilyazova1@mail.ru>
2025-03-03 19:43:45 +0100

    update extended precision data types suite for 25.3

--- extended_precision_data_types/common.py
+++ extended_precision_data_types/common.py
@@ -83,11 +83,13 @@ def execute_query(
         if "snapshot_id" in current().context:
             snapshot_id = current().context.snapshot_id
         else:
-            snapshot_id = (
-                "tests.pre22.3"
-                if check_clickhouse_version("<22.3")(current())
-                else "tests.post22.3"
-            )
+            if check_clickhouse_version(">=25.3")(current()):
+                # https://github.com/ClickHouse/ClickHouse/pull/76840
+                snapshot_id = "tests.post25.3"
+            elif check_clickhouse_version(">=22.3")(current()):
+                snapshot_id = "tests.post22.3"
+            else:
+                snapshot_id = "tests.pre22.3"
 
         if is_with_analyzer(current().context.node):
             snapshot_id += "_with_analyzer"
