586c06a2003cbe5b8d0adaa23b11cbaacaf98bba
alsu <alsugilyazova1@mail.ru>
2025-03-18 14:06:22 +0100

    add multiple tables in same iceberg database sanity check

--- iceberg/tests/iceberg_engine/sanity.py
+++ iceberg/tests/iceberg_engine/sanity.py
@@ -102,6 +102,123 @@ def sanity(self, minio_root_user, minio_root_password):
 
 
 @TestScenario
+def sort_order(self, minio_root_user, minio_root_password):
+    """Test that ClickHouse preserves the sort order of the Iceberg table."""
+    namespace = "iceberg"
+    table_name = "name"
+
+    with Given("create catalog"):
+        catalog = catalog_steps.create_catalog(
+            uri="http://localhost:8182/",
+            catalog_type=catalog_steps.CATALOG_TYPE,
+            s3_endpoint="http://localhost:9002",
+            s3_access_key_id=minio_root_user,
+            s3_secret_access_key=minio_root_password,
+        )
+
+    with And("create namespace"):
+        catalog_steps.create_namespace(catalog=catalog, namespace=namespace)
+
+    with When(f"define schema and create {namespace}.{table_name} table"):
+        schema = Schema(
+            NestedField(
+                field_id=1, name="name", field_type=StringType(), required=False
+            ),
+            NestedField(
+                field_id=2, name="double", field_type=DoubleType(), required=False
+            ),
+            NestedField(
+                field_id=3, name="integer", field_type=LongType(), required=False
+            ),
+            NestedField(
+                field_id=4, name="boolean", field_type=BooleanType(), required=False
+            ),
+            NestedField(
+                field_id=5,
+                name="details",
+                field_type=StructType(
+                    NestedField(
+                        field_id=4,
+                        name="created_by",
+                        field_type=StringType(),
+                        required=False,
+                    ),
+                ),
+                required=False,
+            ),
+            NestedField(
+                field_id=6,
+                name="decimal",
+                field_type=DecimalType(9, 2),
+                required=False,
+            ),
+        )
+        partition_spec = PartitionSpec(
+            PartitionField(
+                source_id=1,
+                field_id=1001,
+                transform=IdentityTransform(),
+                name="symbol_partition",
+            ),
+        )
+        sort_order = SortOrder(SortField(source_id=1, transform=IdentityTransform()))
+        table = catalog_steps.create_iceberg_table(
+            catalog=catalog,
+            namespace=namespace,
+            table_name=table_name,
+            schema=schema,
+            location="s3://warehouse/data",
+            partition_spec=partition_spec,
+            sort_order=sort_order,
+        )
+
+    with Then("create database with Iceberg engine"):
+        database_name = "datalake"
+        iceberg_engine.drop_database(database_name=database_name)
+        iceberg_engine.create_experimental_iceberg_database(
+            namespace=namespace,
+            database_name=database_name,
+            rest_catalog_url="http://rest:8181/v1",
+            s3_access_key_id=minio_root_user,
+            s3_secret_access_key=minio_root_password,
+            catalog_type=catalog_steps.CATALOG_TYPE,
+            storage_endpoint="http://minio:9000/warehouse",
+        )
+
+    with And(f"insert data into {namespace}.{table_name} table"):
+        data = [
+            {
+                "name": "AAPL",
+                "double": 150.5,
+                "integer": 151,
+                "boolean": True,
+                "details": {"created_by": "Alice"},  # Nested Struct
+                "decimal": Decimal("150.5"),
+            },
+            {
+                "name": "GOOG",
+                "double": 2800.1,
+                "integer": 2805,
+                "boolean": False,
+                "details": {"created_by": "Bob"},  # Nested Struct
+                "decimal": Decimal("2800.1"),
+            },
+        ]
+        df = pa.Table.from_pylist(data)
+        table.append(df)
+
+    with And("scan and display data"):
+        df = table.scan().to_pandas()
+        note(df)
+
+    with And("read data in clickhouse from the previously created table"):
+        result = iceberg_engine.read_data_from_clickhouse_iceberg_table(
+            database_name=database_name, namespace=namespace, table_name=table_name
+        )
+        pause()
+
+
+@TestScenario
 def recreate_table(self, minio_root_user, minio_root_password):
     """Test the Iceberg engine in ClickHouse."""
     node = self.context.node
