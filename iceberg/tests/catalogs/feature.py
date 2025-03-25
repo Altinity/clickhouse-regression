08fc5960e38450d51a809f4676657333e57ccf15
alsu <alsugilyazova1@mail.ru>
2025-03-25 21:06:24 +0100

    add nessie configuration and sanity check

---
+++ iceberg/tests/catalogs/feature.py
@@ -0,0 +1,10 @@
+from testflows.core import *
+
+
+@TestFeature
+@Name("iceberg engine")
+def feature(self, minio_root_user, minio_root_password):
+    """Run iceberg engine test."""
+    Feature(
+        test=load("iceberg.tests.catalogs.nessie_sanity", "feature"),
+    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
