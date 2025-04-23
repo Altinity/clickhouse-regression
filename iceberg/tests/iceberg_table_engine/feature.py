7c469bca8190b1969299f15f8fb5231c5c0eca11
alsu <alsugilyazova1@mail.ru>
2025-04-23 23:53:15 +0200

    add rbac test to feature.py for iceberg table engine

--- iceberg/tests/iceberg_table_engine/feature.py
+++ iceberg/tests/iceberg_table_engine/feature.py
@@ -14,3 +14,6 @@ def feature(self, minio_root_user, minio_root_password):
     Feature(
         test=load("iceberg.tests.iceberg_table_engine.alter", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    Feature(
+        test=load("iceberg.tests.iceberg_table_engine.rbac", "feature"),
+    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
