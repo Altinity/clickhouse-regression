54c5ee27a8cb2752827a60ab1d6f2c261cd8714e
alsu <alsugilyazova1@mail.ru>
2025-04-30 15:20:56 +0200

    rename

--- iceberg/tests/iceberg_engine/feature.py
+++ iceberg/tests/iceberg_engine/feature.py
@@ -21,7 +21,7 @@ def feature(self, minio_root_user, minio_root_password):
         test=load("iceberg.tests.iceberg_engine.column_rbac", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
     Feature(
-        test=load("iceberg.tests.iceberg_engine.where_clause", "feature"),
+        test=load("iceberg.tests.iceberg_engine.sql_clauses", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
     Feature(
         test=load("iceberg.tests.iceberg_engine.alter", "feature"),
