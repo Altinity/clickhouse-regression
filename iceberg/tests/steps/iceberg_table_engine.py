ec60b9b225ac00c734feff4d18d23c970e0422f6
alsu <alsugilyazova1@mail.ru>
2025-05-12 15:28:26 +0200

    add create_table_with_iceberg_engine_from_config step

--- iceberg/tests/steps/iceberg_table_engine.py
+++ iceberg/tests/steps/iceberg_table_engine.py
@@ -42,3 +42,39 @@ def create_table_with_iceberg_engine(
     finally:
         with Finally("drop table"):
             node.query(f"DROP TABLE IF EXISTS {table_name}")
+
+
+@TestStep(Given)
+def create_table_with_iceberg_engine_from_config(
+    self,
+    table_name=None,
+    node=None,
+    config_name="iceberg_conf",
+    filename="data",
+    allow_dynamic_metadata_for_data_lakes=False,
+):
+    """Create table with Iceberg table engine with named collection
+    config_name(iceberg_conf) in config.xml."""
+    if node is None:
+        node = self.context.node
+
+    if table_name is None:
+        table_name = "iceberg_table_" + getuid()
+
+    settings = ""
+    if allow_dynamic_metadata_for_data_lakes:
+        settings = "SETTINGS allow_dynamic_metadata_for_data_lakes = true"
+
+    try:
+        node.query(
+            f"""
+            CREATE TABLE {table_name} 
+            ENGINE=IcebergS3({config_name}, filename = '{filename}')
+            {settings}
+            """
+        )
+        return table_name
+
+    finally:
+        with Finally("drop table {table_name}"):
+            node.query(f"DROP TABLE IF EXISTS {table_name}")
