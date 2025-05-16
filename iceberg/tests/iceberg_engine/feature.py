bfcd455279f04e9ee67fd23a1701e4118df1e9f7
alsu <alsugilyazova1@mail.ru>
2025-05-16 14:30:12 +0200

    add predicate push down test for iceberg engine

--- iceberg/tests/iceberg_engine/feature.py
+++ iceberg/tests/iceberg_engine/feature.py
@@ -6,6 +6,18 @@ from testflows.core import *
 def feature(self, minio_root_user, minio_root_password):
     """Run iceberg engine test."""
     Feature(
+        test=load("iceberg.tests.iceberg_engine.alter", "feature"),
+    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    Feature(
+        test=load("iceberg.tests.iceberg_engine.column_rbac", "feature"),
+    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    Feature(
+        test=load(
+            "iceberg.tests.iceberg_engine.predicate_push_down",
+            "feature",
+        ),
+    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    Feature(
         test=load("iceberg.tests.iceberg_engine.sanity", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
     Feature(
@@ -15,13 +27,7 @@ def feature(self, minio_root_user, minio_root_password):
         test=load("iceberg.tests.iceberg_engine.row_policy", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
     Feature(
-        test=load("iceberg.tests.iceberg_engine.column_rbac", "feature"),
-    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
-    Feature(
         test=load("iceberg.tests.iceberg_engine.sql_clauses", "feature"),
-    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
-    Feature(
-        test=load("iceberg.tests.iceberg_engine.alter", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
     Feature(
         test=load("iceberg.tests.iceberg_engine.overwrite_deletes", "feature"),
