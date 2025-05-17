3a027f2661d3ca53117331492c45b1c78f544488
alsu <alsugilyazova1@mail.ru>
2025-05-17 15:59:21 +0200

    update version_when_attach_partition_with_different_keys_merged

--- alter/table/attach_partition/common.py
+++ alter/table/attach_partition/common.py
@@ -5,7 +5,7 @@ from testflows.core import *
 
 from helpers.tables import *
 
-version_when_attach_partition_with_different_keys_merged = "25.6"
+version_when_attach_partition_with_different_keys_merged = "25.7"
 
 
 def clean_name(name):
