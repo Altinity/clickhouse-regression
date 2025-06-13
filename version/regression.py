8f6000a94f6e31f55fdbdd176562a0e1e2f8ccfc
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-06-13 17:08:40 -0400

    add version suite

---
+++ version/regression.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import os
+import sys
+from testflows.core import *
+
+append_path(sys.path, "..")
+
+from helpers.cluster import create_cluster
+from helpers.argparser import argparser, CaptureClusterArgs
+from helpers.common import experimental_analyzer
+
+
+def check_altinity_build(node):
+    """
+    Check if the build is from Altinity.
+
+    The check needs to be robust, it's possible that a test build
+    will not have the version set correctly.
+    """
+    res = node.command("grep -i -a altinity /usr/bin/clickhouse", no_checks=True)
+    return res.exitcode == 0
+
+
+@TestFeature
+@Name("version")
+@ArgumentParser(argparser)
+@CaptureClusterArgs
+def regression(
+    self,
+    cluster_args,
+    clickhouse_version,
+    stress=None,
+    with_analyzer=False,
+):
+    """Simple example of how you can use TestFlows to test ClickHouse."""
+    nodes = {
+        "clickhouse": ("clickhouse1",),
+    }
+
+    self.context.clickhouse_version = clickhouse_version
+
+    if stress is not None:
+        self.context.stress = stress
+
+    with Given("docker-compose cluster"):
+        cluster = create_cluster(
+            **cluster_args,
+            nodes=nodes,
+            configs_dir=current_dir(),
+        )
+        self.context.cluster = cluster
+
+    node = cluster.node("clickhouse1")
+    is_altinity_build = check_altinity_build(node)
+    if not is_altinity_build:
+        skip("This suite is for Altinity builds only")
+
+    with And("I enable or disable experimental analyzer if needed"):
+        experimental_analyzer(node=node, with_analyzer=with_analyzer)
+
+    Scenario(run=load("version.tests.altinity_version", "feature"))
+
+
+if main():
+    regression()
