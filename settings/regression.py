1cad1459f8a0229a103875cd3f40151c05247870
alsu <alsugilyazova1@mail.ru>
2025-06-16 11:50:36 +0200

    initialize new settings suite

---
+++ settings/regression.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import sys
+from testflows.core import *
+
+append_path(sys.path, "..")
+
+from helpers.cluster import create_cluster
+from helpers.argparser import (
+    argparser,
+    CaptureClusterArgs,
+)
+
+
+xfails = {}
+ffails = {}
+
+
+@TestModule
+@Name("settings")
+@FFails(ffails)
+@XFails(xfails)
+@ArgumentParser(argparser)
+@CaptureClusterArgs
+def regression(
+    self,
+    cluster_args,
+    clickhouse_version,
+    stress=None,
+    with_analyzer=False,
+    minio_args=None,
+):
+    """Run tests for Swarm clusters."""
+    nodes = {
+        "clickhouse": ("clickhouse1",),
+    }
+
+    self.context.clickhouse_version = clickhouse_version
+
+    if stress is not None:
+        self.context.stress = stress
+
+    with Given("docker-compose cluster"):
+        cluster = create_cluster(
+            **cluster_args,
+            nodes=nodes,
+            configs_dir=current_dir(),
+        )
+        self.context.cluster = cluster
+
+    self.context.node = self.context.cluster.node("clickhouse1")
+
+    Feature(test=load("settings.tests.default_values", "feature"))()
+
+
+if main():
+    regression()
