11775744f1f20f7ef572dd05ca66e4b646ae7e63
alsu <alsugilyazova1@mail.ru>
2025-06-18 14:20:39 +0200

    default values test and snapshots

--- settings/tests/default_values.py
+++ settings/tests/default_values.py
@@ -1,51 +1,73 @@
 from testflows.core import *
 from testflows.asserts import snapshot, values, error
-from helpers.common import get_snapshot_id, check_clickhouse_version
+from helpers.common import (
+    get_snapshot_id,
+    check_clickhouse_version,
+    check_if_antalya_build,
+)
 
 
 @TestStep()
 def get_all_settings(self):
     """Get list of all settings from system.settings table."""
     return self.context.node.query(
-        f"SELECT name FROM system.settings"
+        f"SELECT name FROM system.settings ORDER BY name"
     ).output.splitlines()
 
 
 @TestScenario
 def check_default_value(self, setting):
     """Check default value for the setting."""
-    with Given("I have a clickhouse node"):
-        node = self.context.node
+    node = self.context.node
 
-    with When("I check the default value for the setting"):
+    with Given(f"check the default value for {setting}"):
         default_and_changed = node.query(
             f"SELECT value, changed FROM system.settings WHERE name = '{setting}' FORMAT JSONEachRow"
         )
         snapshot_name = f"{setting}"
 
-        with Check("import"):
-            with Then("I check the output is correct"):
-                with values() as that:
-                    assert that(
-                        snapshot(
-                            default_and_changed.output.strip(),
-                            name=(snapshot_name),
-                            id=self.context.snapshot_id,
-                            mode=snapshot.CHECK | snapshot.UPDATE,
-                        )
-                    ), error()
+    with Then("compare with snapshot"):
+        with values() as that:
+            assert that(
+                snapshot(
+                    default_and_changed.output.strip(),
+                    name=snapshot_name,
+                    id=self.context.snapshot_id,
+                    mode=snapshot.CHECK,
+                )
+            ), error()
 
 
 @TestFeature
 @Name("default values")
 def feature(self):
-    """Check default values."""
-    if check_clickhouse_version(">=25.6")(self):
-        self.context.snapshot_id = get_snapshot_id(check_clickhouse_version(">=25.6"))
+    """Check default values in `system.settings` table."""
+    versions = [
+        ">=25.6",
+        ">=25.5",
+        ">=25.4",
+        ">=25.3",
+        ">=25.2",
+        ">=25.1",
+        ">=24.12",
+        ">=24.8",
+        ">=24.3",
+        ">=23.8",
+        ">=23.3",
+        ">=22.8",
+    ]
+
+    for version in versions:
+        if check_clickhouse_version(version)(self):
+            self.context.snapshot_id = get_snapshot_id(clickhouse_version=version)
+            break
     else:
         self.context.snapshot_id = get_snapshot_id()
 
-    with Given("get all settings"):
+    if check_if_antalya_build(self):
+        self.context.snapshot_id += "_antalya"
+
+    with Given("get all settings from system.settings table"):
         all_settings = get_all_settings()
 
     for setting in all_settings:
