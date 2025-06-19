88e9656fe04f3569b0457bfdfd3a2ea8bc443d56
alsu <alsugilyazova1@mail.ru>
2025-06-19 15:44:54 +0200

    specify catalog type

--- iceberg/tests/iceberg_table_engine/feature.py
+++ iceberg/tests/iceberg_table_engine/feature.py
@@ -5,6 +5,7 @@ from testflows.core import *
 @Name("iceberg table engine")
 def feature(self, minio_root_user, minio_root_password):
     """Run iceberg table engine test."""
+    self.context.catalog = "rest"
     Feature(
         test=load("iceberg.tests.iceberg_table_engine.row_policy", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
