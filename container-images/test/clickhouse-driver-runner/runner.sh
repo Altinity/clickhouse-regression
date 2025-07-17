ee34dc39346ec3baf5648e9a4a2613ce8b24a3ec
antip00 <antipov@altinity.com>
2025-07-17 17:35:18 +0300

    Updating lts tests.

--- container-images/test/clickhouse-driver-runner/runner.sh
+++ container-images/test/clickhouse-driver-runner/runner.sh
@@ -2,6 +2,8 @@
 set -xe
 
 # install clickhouse, make sure binaries for common and client in /clickhouse folder
+# dpkg -i clickhouse/clickhouse-common-static*.deb
+# dpkg -i clickhouse/clickhouse-client*.deb
 
 clickhouse server --daemon
 sleep 10
@@ -13,11 +15,11 @@ apt install git -y
 git clone --branch "${RELEASE}" --depth 1 --single-branch "https://github.com/mymarilyn/clickhouse-driver.git"
 cd clickhouse-driver
 
-if [ ${RELEASE} == 0.2.3 ]
-then
-git apply /diff-0.2.3.patch
-fi
-git apply /clickhouse-driver.patch
+# if [ ${RELEASE} == 0.2.3 ]
+# then
+# git apply /diff-0.2.3.patch
+# fi
+# git apply /clickhouse-driver.patch
 
 apt install python3-pip -y
 python3 testsrequire.py && python3 setup.py develop
