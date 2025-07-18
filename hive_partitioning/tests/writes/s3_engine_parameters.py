92bacada790a734063f1c6648a49b0f3209b9dfa
antip00 <antipov@altinity.com>
2025-07-18 16:59:01 +0300

    Updates.

--- hive_partitioning/tests/writes/s3_engine_parameters.py
+++ hive_partitioning/tests/writes/s3_engine_parameters.py
@@ -31,44 +31,23 @@ def s3_engine_parameters(
         "CustomSeparated",
         "CustomSeparatedWithNames",
         "CustomSeparatedWithNamesAndTypes",
-        "SQLInsert",
         "Values",
-        "Vertical",
         "JSON",
-        "JSONStrings",
         "JSONColumns",
         "JSONColumnsWithMetadata",
         "JSONCompact",
-        "JSONCompactStrings",
         "JSONCompactColumns",
         "JSONEachRow",
-        "PrettyJSONEachRow",
-        "JSONEachRowWithProgress",
         "JSONStringsEachRow",
-        "JSONStringsEachRowWithProgress",
         "JSONCompactEachRow",
         "JSONCompactEachRowWithNames",
         "JSONCompactEachRowWithNamesAndTypes",
-        "JSONCompactEachRowWithProgress",
         "JSONCompactStringsEachRow",
         "JSONCompactStringsEachRowWithNames",
         "JSONCompactStringsEachRowWithNamesAndTypes",
-        "JSONCompactStringsEachRowWithProgress",
         "JSONObjectEachRow",
         "BSONEachRow",
         "TSKV",
-        "Pretty",
-        "PrettyNoEscapes",
-        "PrettyMonoBlock",
-        "PrettyNoEscapesMonoBlock",
-        "PrettyCompact",
-        "PrettyCompactNoEscapes",
-        "PrettyCompactMonoBlock",
-        "PrettyCompactNoEscapesMonoBlock",
-        "PrettySpace",
-        "PrettySpaceNoEscapes",
-        "PrettySpaceMonoBlock",
-        "PrettySpaceNoEscapesMonoBlock",
         "Protobuf",
         "ProtobufSingle",
         "ProtobufList",
@@ -82,13 +61,8 @@ def s3_engine_parameters(
         "RowBinaryWithNames",
         "RowBinaryWithNamesAndTypes",
         "Native",
-        "Null",
-        "XML",
         "CapnProto",
-        "LineAsString",
-        "RawBLOB",
         "MsgPack",
-        "Markdown",
     )
     compressions = ("none", "gzip", "brotli", "LZMA", "zstd")
 
@@ -109,6 +83,105 @@ def s3_engine_parameters(
                     values="(1, 1)",
                     settings=[("use_hive_partitioning", "1")],
                 )
+
+            with Then("I check data in table"):
+                check_select(
+                    select=f"SELECT i FROM {table_name} WHERE d = 1 ORDER BY i",
+                    expected_result="1",
+                    node=node,
+                )
+
+            with Then("I check files in bucket"):
+                files = get_bucket_files_list(node=node, filename=table_name)
+                note(files)
+                assert f"{table_name}/d=1/" in files, error()
+                assert f".{format}".lower() in files, error()
+
+@TestScenario
+def s3_engine_parameters_partition_columns_in_data_file(
+    self,
+    uri=None,
+    minio_root_user=None,
+    minio_root_password=None,
+    uri_readonly=None,
+    node=None,
+):
+    """Check that s3 engine parameters are supported if partition columns in data file is set to true."""
+    if node is None:
+        node = self.context.node
+
+    formats = (
+        "TabSeparated",
+        "TabSeparatedRaw",
+        "TabSeparatedWithNames",
+        "TabSeparatedWithNamesAndTypes",
+        "TabSeparatedRawWithNames",
+        "TabSeparatedRawWithNamesAndTypes",
+        "CSV",
+        "CSVWithNames",
+        "CSVWithNamesAndTypes",
+        "CustomSeparated",
+        "CustomSeparatedWithNames",
+        "CustomSeparatedWithNamesAndTypes",
+        "Values",
+        "JSON",
+        "JSONColumns",
+        "JSONColumnsWithMetadata",
+        "JSONCompact",
+        "JSONCompactColumns",
+        "JSONEachRow",
+        "JSONStringsEachRow",
+        "JSONCompactEachRow",
+        "JSONCompactEachRowWithNames",
+        "JSONCompactEachRowWithNamesAndTypes",
+        "JSONCompactStringsEachRow",
+        "JSONCompactStringsEachRowWithNames",
+        "JSONCompactStringsEachRowWithNamesAndTypes",
+        "JSONObjectEachRow",
+        "BSONEachRow",
+        "TSKV",
+        "Protobuf",
+        "ProtobufSingle",
+        "ProtobufList",
+        "Avro",
+        "Parquet",
+        "Arrow",
+        "ArrowStream",
+        "ORC",
+        "RowBinary",
+        "RowBinaryWithNames",
+        "RowBinaryWithNamesAndTypes",
+        "Native",
+        "CapnProto",
+        "MsgPack",
+    )
+    compressions = ("none", "gzip", "brotli", "LZMA", "zstd")
+
+    for format, compression in product(formats, compressions):
+        table_name = f"s3_engine_parameters_partition_columns_in_data_file_{format}_{compression}"
+        with Scenario(name=f"{format} {compression}"):
+            with Given(f"I create table {table_name}"):
+                create_table(
+                    table_name=table_name,
+                    columns="d Int32, i Int32",
+                    partition_by="d",
+                    engine=f"S3(s3_conn, format = {format}, compression = {compression}, filename='{table_name}/', partition_strategy='hive', partition_columns_in_data_file=true)",
+                    settings=[("use_hive_partitioning", "1")],
+                )
+            with When("I insert data into table"):
+                insert_into_table_values(
+                    table_name=table_name,
+                    values="(1, 1)",
+                    settings=[("use_hive_partitioning", "1")],
+                )
+
+            with Then("I check data in table"):
+                check_select(
+                    select=f"SELECT i FROM {table_name} WHERE d = 1 ORDER BY i",
+                    expected_result="1",
+                    node=node,
+                )
+
             with Then("I check files in bucket"):
                 files = get_bucket_files_list(node=node, filename=table_name)
                 note(files)
