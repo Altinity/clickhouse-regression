5da77275f6eaf198aa9a47b9bda179b5cc6cb45d
Selfeer <selfeers@gmail.com>
2025-08-04 16:01:10 +0400

    update structure

--- oauth/requirements/requirements.py
+++ oauth/requirements/requirements.py
@@ -637,8 +637,8 @@ RQ_SRS_042_OAuth_Grafana_Authentication_IncorrectRequests_Body_Exp = Requirement
     num="7.4.9",
 )
 
-RQ_SRS_042_OAuth_Grafana_Authentication_ExpiredTokenHandling = Requirement(
-    name="RQ.SRS-042.OAuth.Grafana.Authentication.ExpiredTokenHandling",
+RQ_SRS_042_OAuth_Grafana_Authentication_TokenHandling_Expired = Requirement(
+    name="RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.Expired",
     version="1.0",
     priority=None,
     group=None,
@@ -650,6 +650,54 @@ RQ_SRS_042_OAuth_Grafana_Authentication_ExpiredTokenHandling = Requirement(
     link=None,
     level=3,
     num="7.5.1",
+)
+
+RQ_SRS_042_OAuth_Grafana_Authentication_TokenHandling_Incorrect = Requirement(
+    name="RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.Incorrect",
+    version="1.0",
+    priority=None,
+    group=None,
+    type=None,
+    uid=None,
+    description=(
+        "[ClickHouse] SHALL reject JWT tokens that are malformed, have an invalid signature, or do not conform to the expected structure.\n"
+        "\n"
+    ),
+    link=None,
+    level=3,
+    num="7.5.2",
+)
+
+RQ_SRS_042_OAuth_Grafana_Authentication_TokenHandling_NonAlphaNumeric = Requirement(
+    name="RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.NonAlphaNumeric",
+    version="1.0",
+    priority=None,
+    group=None,
+    type=None,
+    uid=None,
+    description=(
+        "[ClickHouse] SHALL reject JWT tokens that contain non-alphanumeric characters in the header or payload sections, as these are not valid according to the JWT specification.\n"
+        "\n"
+    ),
+    link=None,
+    level=3,
+    num="7.5.3",
+)
+
+RQ_SRS_042_OAuth_Grafana_Authentication_TokenHandling_EmptyString = Requirement(
+    name="RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.EmptyString",
+    version="1.0",
+    priority=None,
+    group=None,
+    type=None,
+    uid=None,
+    description=(
+        "[ClickHouse] SHALL reject empty string values in the Authorization header or any other part of the request that expects a JWT token. An empty string is not a valid JWT and SHALL not be accepted.\n"
+        "\n"
+    ),
+    link=None,
+    level=3,
+    num="7.5.4",
 )
 
 RQ_SRS_042_OAuth_Grafana_Authentication_Caching = Requirement(
@@ -998,11 +1046,26 @@ SRS_042_OAuth_Authentication_in_ClickHouse = Specification(
             level=3,
             num="7.4.9",
         ),
-        Heading(name="Expired Token Handling", level=2, num="7.5"),
+        Heading(name="Token Handling", level=2, num="7.5"),
         Heading(
-            name="RQ.SRS-042.OAuth.Grafana.Authentication.ExpiredTokenHandling",
+            name="RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.Expired",
             level=3,
             num="7.5.1",
+        ),
+        Heading(
+            name="RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.Incorrect",
+            level=3,
+            num="7.5.2",
+        ),
+        Heading(
+            name="RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.NonAlphaNumeric",
+            level=3,
+            num="7.5.3",
+        ),
+        Heading(
+            name="RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.EmptyString",
+            level=3,
+            num="7.5.4",
         ),
         Heading(name="Caching", level=2, num="7.6"),
         Heading(
@@ -1069,7 +1132,10 @@ SRS_042_OAuth_Authentication_in_ClickHouse = Specification(
         RQ_SRS_042_OAuth_Grafana_Authentication_IncorrectRequests_Body_Sub,
         RQ_SRS_042_OAuth_Grafana_Authentication_IncorrectRequests_Body_Aud,
         RQ_SRS_042_OAuth_Grafana_Authentication_IncorrectRequests_Body_Exp,
-        RQ_SRS_042_OAuth_Grafana_Authentication_ExpiredTokenHandling,
+        RQ_SRS_042_OAuth_Grafana_Authentication_TokenHandling_Expired,
+        RQ_SRS_042_OAuth_Grafana_Authentication_TokenHandling_Incorrect,
+        RQ_SRS_042_OAuth_Grafana_Authentication_TokenHandling_NonAlphaNumeric,
+        RQ_SRS_042_OAuth_Grafana_Authentication_TokenHandling_EmptyString,
         RQ_SRS_042_OAuth_Grafana_Authentication_Caching,
         RQ_SRS_042_OAuth_Grafana_Authentication_CacheEviction_NoCache,
         RQ_SRS_042_OAuth_Grafana_Authentication_CacheEviction_CacheLifetime,
@@ -1144,8 +1210,11 @@ SRS_042_OAuth_Authentication_in_ClickHouse = Specification(
         * 7.4.7 [RQ.SRS-042.OAuth.Grafana.Authentication.IncorrectRequests.Body.Sub](#rqsrs-042oauthgrafanaauthenticationincorrectrequestsbodysub)
         * 7.4.8 [RQ.SRS-042.OAuth.Grafana.Authentication.IncorrectRequests.Body.Aud](#rqsrs-042oauthgrafanaauthenticationincorrectrequestsbodyaud)
         * 7.4.9 [RQ.SRS-042.OAuth.Grafana.Authentication.IncorrectRequests.Body.Exp](#rqsrs-042oauthgrafanaauthenticationincorrectrequestsbodyexp)
-    * 7.5 [Expired Token Handling](#expired-token-handling)
-        * 7.5.1 [RQ.SRS-042.OAuth.Grafana.Authentication.ExpiredTokenHandling](#rqsrs-042oauthgrafanaauthenticationexpiredtokenhandling)
+    * 7.5 [Token Handling](#token-handling)
+        * 7.5.1 [RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.Expired](#rqsrs-042oauthgrafanaauthenticationtokenhandlingexpired)
+        * 7.5.2 [RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.Incorrect](#rqsrs-042oauthgrafanaauthenticationtokenhandlingincorrect)
+        * 7.5.3 [RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.NonAlphaNumeric](#rqsrs-042oauthgrafanaauthenticationtokenhandlingnonalphanumeric)
+        * 7.5.4 [RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.EmptyString](#rqsrs-042oauthgrafanaauthenticationtokenhandlingemptystring)
     * 7.6 [Caching](#caching)
         * 7.6.1 [RQ.SRS-042.OAuth.Grafana.Authentication.Caching](#rqsrs-042oauthgrafanaauthenticationcaching)
         * 7.6.2 [Disable Caching](#disable-caching)
@@ -1575,12 +1644,27 @@ version: 1.0
 
 [ClickHouse] SHALL reject requests that include an Authorization header with an `exp` value that indicates the token has expired.
 
-### Expired Token Handling
+### Token Handling
 
-#### RQ.SRS-042.OAuth.Grafana.Authentication.ExpiredTokenHandling
+#### RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.Expired
 version: 1.0
 
 [ClickHouse] SHALL reject expired JWT tokens sent by [Grafana].
+
+#### RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.Incorrect
+version: 1.0
+
+[ClickHouse] SHALL reject JWT tokens that are malformed, have an invalid signature, or do not conform to the expected structure.
+
+#### RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.NonAlphaNumeric
+version: 1.0
+
+[ClickHouse] SHALL reject JWT tokens that contain non-alphanumeric characters in the header or payload sections, as these are not valid according to the JWT specification.
+
+#### RQ.SRS-042.OAuth.Grafana.Authentication.TokenHandling.EmptyString
+version: 1.0
+
+[ClickHouse] SHALL reject empty string values in the Authorization header or any other part of the request that expects a JWT token. An empty string is not a valid JWT and SHALL not be accepted.
 
 ### Caching
 
