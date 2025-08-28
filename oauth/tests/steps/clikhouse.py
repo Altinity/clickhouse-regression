fde4ba6d5c0b24e1f8a3f11b42f4c3d32c5fc028
selfeer <selfeers@gmail.com>
2025-08-28 22:54:35 +0400

    reformat steps

--- oauth/tests/steps/clikhouse.py
+++ oauth/tests/steps/clikhouse.py
@@ -1,5 +1,6 @@
 import requests
 from testflows.core import *
+from jwt_authentication.tests.steps import change_clickhouse_config
 
 
 @TestStep(Then)
@@ -18,3 +19,108 @@ def access_clickhouse(token, ip="clickhouse", https=False):
     response.raise_for_status()
 
     return response.text.strip()
+
+
+@TestStep(Given)
+def change_token_processors(
+    processor_name,
+    algo=None,
+    static_key=None,
+    static_jwks=None,
+    jwks_uri=None,
+    jwks_cache_lifetime=None,
+    claims=None,
+    verifier_leeway=None,
+    configuration_endpoint=None,
+    userinfo_endpoint=None,
+    token_introspection_endpoint=None,
+    processor_type=None,
+    config_d_dir="/etc/clickhouse-server/config.d",
+    node=None,
+):
+    """Change ClickHouse token processor configuration."""
+
+    entries = {"token_processor": {}}
+    entries["jwt_validators"][processor_name] = {}
+
+    if processor_type is not None:
+        entries["token_processor"][processor_name]["type"] = processor_type
+
+    if algo is not None:
+        entries["token_processor"]["algo"] = algo
+
+    if static_key is not None:
+        entries["token_processor"][processor_name]["static_key"] = static_key
+
+    if static_jwks is not None:
+        entries["token_processor"][processor_name]["static_jwks"] = static_jwks
+
+    if jwks_uri is not None:
+        entries["token_processor"][processor_name]["jwks_uri"] = jwks_uri
+
+    if jwks_cache_lifetime is not None:
+        entries["token_processor"][processor_name][
+            "jwks_cache_lifetime"
+        ] = jwks_cache_lifetime
+
+    if claims is not None:
+        entries["jwt_validators"][processor_name]["claims"] = claims
+
+    if verifier_leeway is not None:
+        entries["jwt_validators"][processor_name]["verifier_leeway"] = verifier_leeway
+
+    if configuration_endpoint is not None:
+        entries["token_processor"][processor_name][
+            "configuration_endpoint"
+        ] = configuration_endpoint
+
+    if userinfo_endpoint is not None:
+        entries["token_processor"][processor_name][
+            "userinfo_endpoint"
+        ] = userinfo_endpoint
+
+    if token_introspection_endpoint is not None:
+        entries["token_processor"][processor_name][
+            "token_introspection_endpoint"
+        ] = token_introspection_endpoint
+
+    change_clickhouse_config(
+        entries=entries,
+        config_d_dir=config_d_dir,
+        preprocessed_name="config.xml",
+        restart=True,
+        config_file=f"{processor_name}_config.xml",
+        node=node,
+    )
+
+
+@TestStep(Given)
+def change_user_directories_config(
+    self,
+    processor,
+    common_roles=None,
+    roles_filter=None,
+    node=None,
+    config_d_dir="/etc/clickhouse-server/config.d",
+):
+    """Change ClickHouse user directories configuration."""
+
+    entries = {"user_directories": {}}
+    entries["user_directories"]["token"] = {}
+
+    entries["user_directories"]["token"]["processor"] = processor
+
+    if common_roles is not None:
+        entries["user_directories"]["token"]["common_roles"] = common_roles
+
+    if roles_filter is not None:
+        entries["user_directories"]["token"]["roles_filter"] = roles_filter
+
+    change_clickhouse_config(
+        entires=entries,
+        config_d_dir=config_d_dir,
+        preprocessed_name="config.xml",
+        restart=True,
+        config_file=f"user_directory_{processor}.xml",
+        node=node,
+    )
