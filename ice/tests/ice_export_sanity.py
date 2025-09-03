3e52ae1f84f1826f12795c50b0ae0dd7d484064b
alsugiliazova <alsugilyazova1@mail.ru>
2025-09-03 18:14:47 +0200

    remove ice delete since sometimes it freezes

--- ice/tests/ice_export_sanity.py
+++ ice/tests/ice_export_sanity.py
@@ -1,129 +1,171 @@
 from testflows.core import *
-
-
-from helpers.common import getuid
+from testflows.asserts import error
 
 from helpers.tables import *
+from helpers.datatypes import *
+from helpers.common import getuid
 
-import iceberg.tests.steps.catalog as catalog_steps
+from alter.table.attach_partition.replica.common import get_partition_list
+
+from steps.export import export_partitions_to_s3
+from steps.common import generate_database_name
+from steps.ice_steps import ice_create_table_from_parquet, ice_insert_data_from_parquet
+
 import iceberg.tests.steps.iceberg_engine as iceberg_engine
-import iceberg.tests.steps.iceberg_table_engine as iceberg_table_engine
 
 
 @TestStep(Given)
-def create_test_merge_tree_table(self):
-    """Create test merge tree table."""
+def create_merge_tree_table_with_specific_column_datatype(
+    self, datatype, table_name=None
+):
+    """Create merge tree table with specific datatype for both partition and data columns."""
     node = self.context.node
-    table_name = f"mt_table_{getuid()}"
+    if table_name is None:
+        table_name = f"mt_{datatype.name.lower().replace('(', '_').replace(')', '_').replace(',', '_')}_{getuid()}"
+
+    partition_col = Column(name="year", datatype=UInt16())
+    data_col = Column(name="data_col", datatype=datatype)
+    columns = [partition_col, data_col]
+
     try:
-        with By("create test merge tree table"):
-            node.query(
-                f"""
-                CREATE TABLE {table_name} (id UInt64, year UInt16) 
-                ENGINE = MergeTree() 
-                PARTITION BY year 
-                ORDER BY id
-            """
+        with By(f"creating merge tree table {table_name} with {datatype.name}"):
+            table = create_table(
+                name=table_name,
+                engine="MergeTree()",
+                columns=columns,
+                partition_by="year",
+                order_by="data_col",
+                node=node,
             )
 
-        with And("populate the table"):
-            node.query(
-                f"INSERT INTO {table_name} VALUES (1, 2020), (2, 2020), (3, 2020), (4, 2021), (1, 2020), (0, 2020)"
+        with And("inserting test data"):
+            table.insert_test_data(
+                row_count=10,
+                cardinality=1,
+                node=node,
+                shuffle_values=True,
             )
-            node.query(f"INSERT INTO {table_name} VALUES (5, 2020), (6, 2020)")
 
         yield table_name
 
     finally:
-        with Finally("drop the table"):
+        with Finally(f"drop the table {table_name}"):
             node.query(f"DROP TABLE IF EXISTS {table_name}")
 
 
 @TestStep(Given)
-def create_test_s3_table(self):
-    """Create test s3 table."""
+def create_s3_table_with_specific_column_datatype(self, datatype, table_name=None):
+    """Create S3 table with specific datatype for both partition and data columns."""
     node = self.context.node
-    table_name = f"s3_table_{getuid()}"
+    if table_name is None:
+        table_name = f"s3_{datatype.name.lower().replace('(', '_').replace(')', '_').replace(',', '_')}_{getuid()}"
+
+    partition_col = Column(name="year", datatype=UInt16())
+    data_col = Column(name="data_col", datatype=datatype)
+    columns = [partition_col, data_col]
+
     try:
-        node.query(
-            f"""
-            CREATE TABLE {table_name} (id UInt64, year UInt16) 
-            ENGINE = S3(s3_conn, filename='{table_name}', format=Parquet, partition_strategy='hive') 
-            PARTITION BY year
-            ORDER BY id
-        """
-        )
+        with By(f"creating S3 table {table_name} with {datatype.name}"):
+            create_table(
+                name=table_name,
+                engine=f"S3(s3_conn, filename='{table_name}', format=Parquet, partition_strategy='hive')",
+                columns=columns,
+                order_by="data_col",
+                partition_by="year",
+                node=node,
+            )
+
         yield table_name
 
     finally:
-        with Finally("drop the table"):
+        with Finally(f"drop the table {table_name}"):
             node.query(f"DROP TABLE IF EXISTS {table_name}")
 
 
-@TestFeature
-def feature(self, minio_root_user, minio_root_password):
-    """Run Ice tests."""
+@TestStep(Given)
+def test_datatype_export_pipeline(self, datatype, minio_root_user, minio_root_password):
+    """Test `export partitions + ice create + ice insert` pipeline for a specific datatype."""
     ice_node = self.context.ice_node
     node = self.context.node
 
-    with Given("check ice version"):
-        version = ice_node.command("ice -V").output
+    with Given(f"create merge tree table with {datatype.name} columns"):
+        mt_table_name = create_merge_tree_table_with_specific_column_datatype(
+            datatype=datatype
+        )
 
-    with And("create test merge tree table"):
-        mt_table_name = create_test_merge_tree_table()
+    with And(f"create s3 table with {datatype.name} columns"):
+        s3_table_name = create_s3_table_with_specific_column_datatype(datatype=datatype)
 
-    with And("create test s3 table"):
-        s3_table_name = create_test_s3_table()
+    with And("get partition ids for merge tree table"):
+        partition_ids = get_partition_list(table_name=mt_table_name, node=node)
 
-    with When("export partition from merge tree table to s3 table"):
-        partition_ids = ["2020", "2021"]
-        for partition_id in partition_ids:
-            node.query(
-                f"""
-                ALTER TABLE {mt_table_name} 
-                EXPORT PARTITION ID '{partition_id}' 
-                TO TABLE {s3_table_name} 
-                SETTINGS 
-                    allow_experimental_export_merge_tree_partition = 1, 
-                    export_merge_tree_partition_background_execution = 0
-                """
-            )
+    with When("exporting partitions from merge tree to s3"):
+        export_partitions_to_s3(
+            merge_tree_table_name=mt_table_name,
+            s3_table_name=s3_table_name,
+            partition_ids=partition_ids,
+            minio_root_user=minio_root_user,
+            minio_root_password=minio_root_password,
+        )
 
-    with And("create database with Iceberg engine"):
-        database_name = f"iceberg_database_{getuid()}"
+    with And("creating Iceberg database"):
+        database_name = generate_database_name(datatype)
         iceberg_engine.create_experimental_iceberg_database(
             database_name=database_name,
-            rest_catalog_url="http://ice-rest-catalog:5000",
             s3_access_key_id=minio_root_user,
             s3_secret_access_key=minio_root_password,
-            catalog_type=catalog_steps.CATALOG_TYPE,
-            storage_endpoint="http://minio:9000/warehouse",
         )
 
-    with Then(
-        "create iceberg table with partitioning and sort order using schema from parquet file"
-    ):
-        iceberg_table_name = f"iceberg_table_{getuid()}"
-        ice_node.command(
-            f'ice create-table default.{iceberg_table_name} -p --schema-from-parquet s3://warehouse/{s3_table_name}/year=2020/*.parquet --partition=\'[{{"column":"year","transform":"identity"}}]\' --sort=\'[{{"column":"id"}}]\''
+    with And("creating Iceberg table from Parquet schema"):
+        iceberg_table_name = f"iceberg_{datatype.name.lower().replace('(', '_').replace(')', '_').replace(',', '_')}_{getuid()}"
+        # ice_node.command(
+        #     f'ice create-table default.{iceberg_table_name} -p --schema-from-parquet s3://warehouse/{s3_table_name}/year=*/*.parquet --partition=\'[{{"column":"year","transform":"identity"}}]\' --sort=\'[{{"column":"data_col"}}]\''
+        # )
+        ice_create_table_from_parquet(
+            iceberg_table_name=iceberg_table_name,
+            parquet_path=f"s3://warehouse/{s3_table_name}/year=*/*.parquet",
+            partition_column="year",
+            sort_column="data_col",
+            ice_node=ice_node,
         )
 
-    with And("run ice insert command"):
+    with And("inserting data into Iceberg table"):
         for partition_id in partition_ids:
-            ice_node.command(
-                f"ice insert default.{iceberg_table_name} -p s3://warehouse/{s3_table_name}/year={partition_id}/*.parquet"
+            # ice_node.command(
+            #     f"ice insert default.{iceberg_table_name} -p s3://warehouse/{s3_table_name}/year={partition_id}/*.parquet"
+            # )
+            ice_insert_data_from_parquet(
+                iceberg_table_name=iceberg_table_name,
+                parquet_path=f"s3://warehouse/{s3_table_name}/year={partition_id}/*.parquet",
+                ice_node=ice_node,
             )
 
-    with And("read data from iceberg table"):
-        result = iceberg_engine.read_data_from_clickhouse_iceberg_table(
+    with Then("verifying data integrity"):
+        mt_result = node.query(f"SELECT * FROM {mt_table_name} ORDER BY data_col")
+        iceberg_result = iceberg_engine.read_data_from_clickhouse_iceberg_table(
             database_name=database_name,
             namespace="default",
             table_name=iceberg_table_name,
             object_storage_cluster=None,
+            order_by="data_col",
         )
-        note(result.output)
+        assert mt_result.output == iceberg_result.output, error()
 
-    with And("check show create table"):
+    with And("checking table schema"):
         node.query(
             f"SHOW CREATE TABLE {database_name}.\\`default.{iceberg_table_name}\\`"
+        )
+
+
+@TestFeature
+def test_basic_datatypes_export(self, minio_root_user, minio_root_password):
+    """Test `export partitions + ice create + ice insert` pipeline with all basic datatypes."""
+
+    basic_datatypes_list = basic_datatypes()
+
+    for datatype in basic_datatypes_list:
+        Scenario(name=f"testing {datatype.name}", test=test_datatype_export_pipeline)(
+            datatype=datatype,
+            minio_root_user=minio_root_user,
+            minio_root_password=minio_root_password,
         )
