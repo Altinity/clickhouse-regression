75d207a349914fad2f42f1c893c97ee4b345fba8
alsugiliazova <alsugilyazova1@mail.ru>
2025-09-17 13:47:09 +0200

    revert turn off hive

--- iceberg/tests/steps/s3.py
+++ iceberg/tests/steps/s3.py
@@ -10,6 +10,6 @@ def read_data_with_s3_table_function(
         node = self.context.node
 
     result = node.query(
-        f"SELECT {columns} FROM s3('{endpoint}', '{s3_access_key_id}', '{s3_secret_access_key}') SETTINGS use_hive_partitioning=0"
+        f"SELECT {columns} FROM s3('{endpoint}', '{s3_access_key_id}', '{s3_secret_access_key}')"
     )
     return result
