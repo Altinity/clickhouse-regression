cd8ecfb7ceb75cac4821ba5e7c9be8b50d1bc1c1
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-09-18 08:49:08 -0400

    update upload paths for builds

--- .github/setup.sh
+++ .github/setup.sh
@@ -99,11 +99,9 @@ elif [[ $artifacts == 'builds' ]]; then
   artifact_s3_bucket_path="altinity-build-artifacts"
   confidential=""
   if [[ $event_name == "pull_request" ]]; then
-    artifact_s3_dir="$pr_number/$build_sha/regression"
-  elif [[ $event_name == "release" || $event_name == "push" ]]; then
-    artifact_s3_dir="0/$build_sha/regression"
-  elif [[ $event_name == "workflow_dispatch" ]]; then
-    artifact_s3_dir="maintenance/$GITHUB_REF_NAME/$GITHUB_RUN_ID/regression"
+    artifact_s3_dir="PRs/$pr_number/$build_sha/regression"
+  else # release, push, workflow_dispatch
+    artifact_s3_dir="REFs/$GITHUB_REF_NAME/$build_sha/regression"
   fi
 
 fi
