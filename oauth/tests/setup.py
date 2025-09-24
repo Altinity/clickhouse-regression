95b20893009ecf036a1d3a862693ab03f3d42ec0
Selfeer <selfeers@gmail.com>
2025-09-24 17:27:17 +0400

    bring back assertions that were disabled

--- oauth/tests/setup.py
+++ oauth/tests/setup.py
@@ -15,9 +15,8 @@ def verify_keycloak_realm_setup(self, realm_setup_step):
         token = client.OAuthProvider.get_oauth_token()
 
     with Then("I try to access ClickHouse with the token"):
-        # response = access_clickhouse(token=token)
-        # assert response.status_code == 200, error()
-        pass
+        response = access_clickhouse(token=token)
+        assert response.status_code == 200, error()
 
     with And("I check that the ClickHouse server is still alive"):
         check_clickhouse_is_alive()
