ebaf69c8d3527aac9ec4ecec8114b3119fb15753
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-10-06 11:20:20 -0400

    get-deb-url don't crash due to binary path missing version

--- .github/get-deb-url.py
+++ .github/get-deb-url.py
@@ -140,7 +140,8 @@ if __name__ == "__main__":
     for url in build_report["build_urls"]:
         if is_desired_file(url):
             with open(github_env, "a") as f:
-                f.write("version=" + url.split("/")[-1].split("_")[1] + "\n")
                 f.write("clickhouse_path=" + url + "\n")
+                if not args.binary:  # cannot get version from binary url
+                    f.write("version=" + url.split("/")[-1].split("_")[1] + "\n")
             sys.stdout.write(url)
             sys.exit(0)
