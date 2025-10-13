ba750741f518b9c3d135f32a4aa582e0b8338ab6
alsugiliazova <alsugilyazova1@mail.ru>
2025-10-13 17:50:57 +0200

    update to sqlalchemy lts tests

--- container-images/test/clickhouse-sqlalchemy-runner/runner.sh
+++ container-images/test/clickhouse-sqlalchemy-runner/runner.sh
@@ -16,10 +16,11 @@ apt install git -y
 git clone --branch "${RELEASE}" --depth 1 --single-branch "https://github.com/xzkostyan/clickhouse-sqlalchemy.git"
 cd clickhouse-sqlalchemy
 # git apply /sqlalchemy.patch
+git apply /diff_0_2_9.patch
 
 apt install python3-pip -y
 
 pip install --upgrade pip
-pip install asynch==0.2.5
+pip install "asynch==0.2.5" "alembic==1.11.1" "pytest-asyncio"
 python3 testsrequire.py && python3 setup.py develop
 pytest -v
