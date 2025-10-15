45f54ea8caa7ecc4b60fd54eba44873190629436
Selfeer <selfeers@gmail.com>
2025-10-15 21:40:53 +0400

    black

--- ice/tests/all_parquet_files.py
+++ ice/tests/all_parquet_files.py
@@ -2,15 +2,16 @@ from parquet.tests.steps.generate_all_files import *
 from testflows.core import *
 
 
-
-
 @TestScenario
 def run_all_parquet_files(self, pool=3):
     """Run all parquet files generation in parallel."""
 
-
     with Given("I generate all possible parquet files"):
-        run_all_possible_files(pool=3, json_file_location=os.path.join(current_dir(), "..", "data", "json_files"))
+        run_all_possible_files(
+            pool=3,
+            json_file_location=os.path.join(current_dir(), "..", "data", "json_files"),
+        )
+
 
 @TestFeature
 @Name("parquet files generation")
