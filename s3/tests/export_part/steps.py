75ca3482df9f2a9e350a6018d03f4f5bb4e4aafd
Julian Huang <jhuang@altinity.com>
2025-10-24 16:16:39 -0400

    steps.py rewrite

--- s3/tests/export_part/steps.py
+++ s3/tests/export_part/steps.py
@@ -1,136 +1,100 @@
 import json
 
 from testflows.core import *
+from testflows.asserts import error
 from helpers.common import getuid
-from helpers.tables import *
+from helpers.create import *
 from s3.tests.common import temporary_bucket_path
 
 
 @TestStep(Given)
-def create_source_and_destination_tables(self, engine=None, columns=None, partition_by=None, order_by=None, node=None, cluster=None, stop_merges=True):
-    """Create source and destination tables."""
+def create_temp_bucket(self):
+    """Create temporary S3 bucket."""
 
-    create_temp_bucket()
-    source = create_source_table(engine=engine, columns=columns, partition_by=partition_by, order_by=order_by, node=node, cluster=cluster)
-    destination = create_destination_table(source=source)
-    if stop_merges:
-        source.stop_merges()
-    
-    return source, destination
+    temp_s3_path = temporary_bucket_path(
+        bucket_prefix=f"{self.context.bucket_prefix}/export_part"
+    )
+
+    self.context.uri = f"{self.context.uri_base}export_part/{temp_s3_path}/"
+    # Delete the next line if the context var is never used
+    # self.context.bucket_path = f"{self.context.bucket_prefix}/export_part/{temp_s3_path}"
 
 
 @TestStep(Given)
-def create_temp_bucket(self, uri=None, bucket_prefix=None):
-    """Create temporary s3 bucket."""
-
-    if uri is None:
-        uri = self.context.uri_base
-        
-    if bucket_prefix is None:
-        bucket_prefix = self.context.bucket_prefix
-
-    temp_s3_path = temporary_bucket_path(
-        bucket_prefix=f"{bucket_prefix}/export_part"
-    )
-
-    self.context.uri = f"{uri}export_part/{temp_s3_path}/"
-    self.context.bucket_path = f"{bucket_prefix}/export_part/{temp_s3_path}"
-
-
-@TestStep(When)
-def export_events(self, node=None):
-    """Get the number of successful parts exports from the system.events table."""
-
-    if node is None:
-        node = self.context.node
-
-    output = node.query(
-        "SELECT name, value FROM system.events WHERE name LIKE '%%Export%%' FORMAT JSONEachRow",
-        exitcode=0,
-    ).output
-    return {
-        row["name"]: int(row["value"])
-        for row in [json.loads(row) for row in output.splitlines()]
-    }
-
-
-@TestStep(When)
-def export_part(self, parts, source, destination, exitcode=0, node=None):
-    """Alter export of parts."""
-
-    if node is None:
-        node = self.context.node
-
-    no_checks = exitcode != 0
-    results = []
-
-    # we should be able to set the settings here instead of using the SET query, but this is a quick workaround for the bug
-    for part in parts:
-        results.append(node.query(
-            f"SET allow_experimental_export_merge_tree_part = 1; ALTER TABLE {source.name} EXPORT PART '{part}' TO TABLE {destination.name}",
-            # settings=[("allow_experimental_export_merge_tree_part", 1)],
-            exitcode=exitcode,
-            no_checks=no_checks
-        ))
-
-    return results
-
-
-@TestStep(When)
-def create_source_table(self, engine=None, columns=None, partition_by=None, order_by=None, node=None, cluster=None):
-    """Create a source table."""
-
-    if engine is None:
-        engine = "MergeTree"
-    if columns is None:
-        columns = [
-            Column(name="p", datatype=UInt16()),
-            Column(name="i", datatype=UInt64()),
-        ]
-    if partition_by is None:
-        partition_by = columns[0].name
-    if order_by is None:
-        order_by = "tuple()"
-    if cluster is None:
-        cluster = "one_shard_cluster"
-
-    source = create_table(
-        name="source_table_" + getuid(),
-        engine=engine,
-        columns=columns,
-        partition_by=partition_by,
-        order_by=order_by,
-        cluster=cluster,
-        node=node,
-    )
-
-    return source
-
-
-@TestStep(When)
-def create_destination_table(self, source, engine=None):
-    """Create a destination table."""
+def create_s3_table(self, table_name, cluster):
+    """Create a destination S3 table."""
     
-    name = "destination_table_" + getuid()
-    
-    if engine is None:
-        engine = f"""
+    table_name = f"{table_name}_{getuid()}"
+    engine = f"""
         S3(
             '{self.context.uri}',
             '{self.context.access_key_id}',
             '{self.context.secret_access_key}',
-            filename='{name}',
+            filename='{table_name}',
             format='Parquet',
             compression='auto',
             partition_strategy='hive'
         )
     """
 
-    destination = create_table(
-        name=name,
-        columns=source.columns,
-        partition_by=source.partition_by,
+    # TODO columns and partition_by are hardcoded for now, but i should make them configurable
+    create_table(
+        table_name=table_name,
+        columns=[{"name": "p", "type": "Int8"}, {"name": "i", "type": "UInt64"}],
+        partition_by="p",
         engine=engine,
+        cluster=cluster,
     )
 
-    return destination
+
+@TestStep(When)
+def get_parts(self, table_name, node):
+    """Get all parts for a table on a given node."""
+
+    output = node.query(f"SELECT name FROM system.parts WHERE table = '{table_name}'", exitcode=0).output
+    return [row.strip() for row in output.splitlines()]
+
+
+@TestStep(When)
+def select_all_ordered(self, table_name, node):
+    """Select all data from a table ordered by partition and index columns."""
+
+    output = node.query(f"SELECT * FROM {table_name} ORDER BY p, i", exitcode=0).output
+    return [row.strip() for row in output.splitlines()]
+
+
+@TestStep(When)
+def export_parts(self, source_table, destination_table, node, parts=None, exitcode=0):
+    """Export parts from a source table to a destination table on the same node. If parts are not provided, all parts will be exported."""
+
+    if parts is None:
+        parts = get_parts(source_table, node)
+    no_checks = exitcode != 0
+
+    for part in parts:
+        node.query(# we should be able to set the settings here instead of using the SET query, but this is a quick workaround for the bug
+            f"SET allow_experimental_export_merge_tree_part = 1; ALTER TABLE {source_table} EXPORT PART '{part}' TO TABLE {destination_table}",
+            # settings=[("allow_experimental_export_merge_tree_part", 1)],
+            exitcode=exitcode,
+            no_checks=no_checks
+        )
+
+
+# TODO find the simplest way to parse the output
+@TestStep(When)
+def get_export_events(self, node):
+    """Get the export data from the system.events table of a given node."""
+
+    output = node.query("SELECT name, value FROM system.events WHERE name LIKE '%%Export%%' FORMAT JSONEachRow", exitcode=0).output
+    # return {row.name: int(row.value) for row in json.loads(output)}
+    # return [json.loads(row) for row in output.splitlines()]
+    return output
+
+
+@TestStep(Then)
+def source_matches_destination(self, source_table, destination_table, source_node, destination_node):
+    """Check that source and destination table data matches."""
+
+    source_data = select_all_ordered(source_table, source_node)
+    destination_data = select_all_ordered(destination_table, destination_node)
+    assert source_data == destination_data, error()
