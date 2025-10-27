fd546ccbf416ac7eec0698bc91ecdaf4d23e6a7c
alsugiliazova <alsugilyazova1@mail.ru>
2025-10-27 10:16:07 +0100

    update ice common steps

--- ice/steps/common.py
+++ ice/steps/common.py
@@ -5,4 +5,4 @@ from helpers.common import getuid
 
 def generate_database_name(datatype):
     """Generate a unique database name for the given datatype."""
-    return f"iceberg_db_{datatype.name.lower().replace('(', '_').replace(')', '_').replace(',', '_')}_{getuid()}"
+    return f"iceberg_db_{datatype.name.lower().replace('(', '_').replace(')', '_').replace(',', '_').replace("'", '_').replace('=', '_').replace(' ', '_')}_{getuid()}"
