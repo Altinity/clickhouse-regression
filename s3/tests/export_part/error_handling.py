c1b61d94c4472c3f9af909e190badd681704e8d6
julian <jhuang@altinity.com>
2025-10-27 15:46:40 -0400

    invalid_part_name rewrite, plus black

--- s3/tests/export_part/error_handling.py
+++ s3/tests/export_part/error_handling.py
@@ -1,6 +1,5 @@
 from testflows.core import *
 from testflows.asserts import error
-
 from s3.tests.export_part.steps import *
 from helpers.tables import *
 
@@ -9,24 +8,31 @@ from helpers.tables import *
 def invalid_part_name(self):
     """Check that exporting a non-existent part returns the correct error."""
 
-    with Given("I create source and destination tables"):
-        source, destination = create_source_and_destination_tables()
-
-    with When("I insert random test data into the source table"):
-        source.insert_test_data()  # default row_count=10, cardinality=1
+    with Given("I create a populated source table and empty S3 table"):
+        partitioned_merge_tree_table(
+            table_name="source",
+            partition_by="p",
+            columns=default_columns(simple=True),
+            stop_merges=True,
+            populate=True,
+        )
+        s3_table_name = create_s3_table(
+            table_name="s3", create_new_bucket=True, simple_columns=True
+        )
 
     with And("I create an invalid part name"):
         invalid_part_name = "in_va_lid_part"
 
-    with Then("I try to export the invalid part and expect an error"):
-        results = export_part(
+    with When("I try to export the invalid part"):
+        results = export_parts(
+            source_table="source",
+            destination_table=s3_table_name,
+            node=self.context.node,
             parts=[invalid_part_name],
-            source=source,
-            destination=destination,
             exitcode=1,
         )
-        assert len(results) == 1, error()
-        # note(f"Result: {results[0].output}")
+
+    with Then("I should see an error related to the invalid part name"):
         assert results[0].exitcode == 233, error()
         assert (
             f"Unexpected part name: {invalid_part_name}" in results[0].output
