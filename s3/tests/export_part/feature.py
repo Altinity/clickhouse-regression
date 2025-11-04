e46c11148df9303107d886fcb91f97177c5bb179
julian <jhuang@altinity.com>
2025-11-04 15:21:54 -0500

    all working tests

--- s3/tests/export_part/feature.py
+++ s3/tests/export_part/feature.py
@@ -16,10 +16,10 @@ def minio(self, uri, bucket_prefix):
     self.context.uri_base = uri
     self.context.bucket_prefix = bucket_prefix
 
-    # Feature(run=load("s3.tests.export_part.sanity", "feature"))
-    # Feature(run=load("s3.tests.export_part.error_handling", "feature"))
-    # Feature(run=load("s3.tests.export_part.system_monitoring", "feature"))
-    # Feature(run=load("s3.tests.export_part.clusters_nodes", "feature"))
-    # Feature(run=load("s3.tests.export_part.engines_volumes", "feature"))
-    # Feature(run=load("s3.tests.export_part.datatypes", "feature"))
+    Feature(run=load("s3.tests.export_part.sanity", "feature"))
+    Feature(run=load("s3.tests.export_part.error_handling", "feature"))
+    Feature(run=load("s3.tests.export_part.clusters_nodes", "feature"))
+    Feature(run=load("s3.tests.export_part.engines_volumes", "feature"))
+    Feature(run=load("s3.tests.export_part.datatypes", "feature"))
     Feature(run=load("s3.tests.export_part.concurrency_networks", "feature"))
+    # Feature(run=load("s3.tests.export_part.system_monitoring", "feature"))
