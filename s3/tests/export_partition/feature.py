4ff46e6a880215cc6c7698e65e7b06874d602832
selfeer <selfeers@gmail.com>
2025-11-10 17:39:27 +0400

    add specification

--- s3/tests/export_partition/feature.py
+++ s3/tests/export_partition/feature.py
@@ -1,8 +1,9 @@
 from testflows.core import *
+from s3.requirements.export_partition import *
 
 
 @TestFeature
-@Specifications()
+@Specifications(SRS_016_ClickHouse_Export_Partition_to_S3)
 @Requirements()
 @Name("export partition")
 def minio(self, uri, bucket_prefix):
@@ -12,9 +13,9 @@ def minio(self, uri, bucket_prefix):
     self.context.bucket_prefix = bucket_prefix
 
     Feature(run=load("s3.tests.export_partition.sanity", "feature"))
-    # Feature(run=load("s3.tests.export_part.error_handling", "feature"))
-    # Feature(run=load("s3.tests.export_part.clusters_nodes", "feature"))
-    # Feature(run=load("s3.tests.export_part.engines_volumes", "feature"))
-    # Feature(run=load("s3.tests.export_part.datatypes", "feature"))
-    # Feature(run=load("s3.tests.export_part.concurrency_networks", "feature"))
-    # Feature(run=load("s3.tests.export_part.system_monitoring", "feature"))
+    Feature(run=load("s3.tests.export_partition.error_handling", "feature"))
+    Feature(run=load("s3.tests.export_partition.clusters_nodes", "feature"))
+    Feature(run=load("s3.tests.export_partition.engines_volumes", "feature"))
+    Feature(run=load("s3.tests.export_partition.datatypes", "feature"))
+    Feature(run=load("s3.tests.export_partition.concurrency_networks", "feature"))
+    Feature(run=load("s3.tests.export_partition.system_monitoring", "feature"))
