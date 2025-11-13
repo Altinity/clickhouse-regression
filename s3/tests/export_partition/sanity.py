467cafe3b2abb0b8955fea353bff62326a89b746
Selfeer <selfeers@gmail.com>
2025-11-13 18:14:15 +0400

    fix

--- s3/tests/export_partition/sanity.py
+++ s3/tests/export_partition/sanity.py
@@ -8,42 +8,46 @@ from s3.requirements.export_part import *
 from alter.table.replace_partition.partition_types import (
     table_with_compact_and_wide_parts,
 )
-from s3.tests.export_partition.steps import export_partitions
+from s3.tests.export_partition.steps import (
+    export_partitions,
+    source_matches_destination,
+)
 
 
 @TestScenario
 @Requirements(RQ_ClickHouse_ExportPart_Settings_AllowExperimental("1.0"))
 def export_setting(self):
-    """Check that the export setting is settable in 2 ways when exporting parts."""
+    """Check that the export setting is settable when exporting partitions."""
 
+    source_table = f"source_{getuid()}"
     with Given("I create a populated source table and 2 empty S3 tables"):
         partitioned_merge_tree_table(
-            table_name="source",
+            table_name=source_table,
             partition_by="p",
             columns=default_columns(),
             stop_merges=True,
         )
         s3_table_name1 = create_s3_table(table_name="s3_1", create_new_bucket=True)
-        s3_table_name2 = create_s3_table(table_name="s3_2")
+        s3_table_name2 = create_s3_table(table_name="s3_2", create_new_bucket=True)
 
-    with When("I export parts to the first S3 table using the SET query"):
-        export_parts(
-            source_table="source",
+    with When("I export partitions to the first S3 table"):
+        export_partitions(
+            source_table=source_table,
             destination_table=s3_table_name1,
             node=self.context.node,
-            explicit_set=1,
         )
 
-    with And("I export parts to the second S3 table using the settings argument"):
-        export_parts(
-            source_table="source",
+    with And("I export partitions to the second S3 table"):
+        export_partitions(
+            source_table=source_table,
             destination_table=s3_table_name2,
             node=self.context.node,
-            explicit_set=0,
         )
 
     with And("I read data from all tables"):
-        source_data = select_all_ordered(table_name="source", node=self.context.node)
+        source_data = select_all_ordered(
+            table_name=source_table, node=self.context.node
+        )
         destination_data1 = select_all_ordered(
             table_name=s3_table_name1, node=self.context.node
         )
@@ -59,11 +63,12 @@ def export_setting(self):
 @TestScenario
 @Requirements(RQ_ClickHouse_ExportPart_SchemaCompatibility("1.0"))
 def mismatched_columns(self):
-    """Test exporting parts when source and destination tables have mismatched columns."""
+    """Test exporting partitions when source and destination tables have mismatched columns."""
 
+    source_table = f"source_{getuid()}"
     with Given("I create a source table and S3 table with different columns"):
         partitioned_merge_tree_table(
-            table_name="source",
+            table_name=source_table,
             partition_by="p",
             columns=default_columns(),
             stop_merges=True,
@@ -74,9 +79,9 @@ def mismatched_columns(self):
             columns=default_columns(simple=False),
         )
 
-    with When("I export parts to the S3 table"):
-        results = export_parts(
-            source_table="source",
+    with When("I export partitions to the S3 table"):
+        results = export_partitions(
+            source_table=source_table,
             destination_table=s3_table_name,
             node=self.context.node,
             exitcode=1,
@@ -117,28 +122,29 @@ def basic_table(self):
 
 @TestScenario
 def empty_table(self):
-    """Test exporting parts from an empty table."""
+    """Test exporting partitions from an empty table."""
 
+    source_table = f"source_{getuid()}"
     with Given("I create empty source and S3 tables"):
         partitioned_merge_tree_table(
-            table_name="empty_source",
+            table_name=source_table,
             partition_by="p",
             columns=default_columns(),
             stop_merges=False,
             populate=False,
         )
-        s3_table_name = create_s3_table(table_name="empty_s3", create_new_bucket=True)
+        s3_table_name = create_s3_table(table_name="s3", create_new_bucket=True)
 
-    with When("I export parts to the S3 table"):
-        export_parts(
-            source_table="empty_source",
+    with When("I export partitions to the S3 table"):
+        export_partitions(
+            source_table=source_table,
             destination_table=s3_table_name,
             node=self.context.node,
         )
 
     with And("I read data from both tables"):
         source_data = select_all_ordered(
-            table_name="empty_source", node=self.context.node
+            table_name=source_table, node=self.context.node
         )
         destination_data = select_all_ordered(
             table_name=s3_table_name, node=self.context.node
@@ -152,11 +158,12 @@ def empty_table(self):
 @TestScenario
 @Requirements(RQ_ClickHouse_ExportPart_PartitionKeyTypes("1.0"))
 def no_partition_by(self):
-    """Test exporting parts when the source table has no PARTITION BY type."""
+    """Test exporting partitions when the source table has no PARTITION BY type."""
 
+    source_table = f"source_{getuid()}"
     with Given("I create a populated source table and empty S3 table"):
         partitioned_merge_tree_table(
-            table_name="source",
+            table_name=source_table,
             partition_by="tuple()",
             columns=default_columns(),
             stop_merges=True,
@@ -165,16 +172,16 @@ def no_partition_by(self):
             table_name="s3", create_new_bucket=True, partition_by="tuple()"
         )
 
-    with When("I export parts to the S3 table"):
-        export_parts(
-            source_table="source",
+    with When("I export partitions to the S3 table"):
+        export_partitions(
+            source_table=source_table,
             destination_table=s3_table_name,
             node=self.context.node,
         )
 
     with Then("Check source matches destination"):
         source_matches_destination(
-            source_table="source",
+            source_table=source_table,
             destination_table=s3_table_name,
         )
 
@@ -182,33 +189,35 @@ def no_partition_by(self):
 @TestScenario
 @Requirements(RQ_ClickHouse_ExportPart_PartTypes("1.0"))
 def wide_and_compact_parts(self):
-    """Check that exporting with both wide and compact parts is supported."""
+    """Check that exporting partitions with both wide and compact parts is supported."""
 
+    source_table = f"source_{getuid()}"
     with Given("I create a source table with wide and compact parts"):
-        table_with_compact_and_wide_parts(table_name="source")
+        table_with_compact_and_wide_parts(table_name=source_table)
         s3_table_name = create_s3_table(table_name="s3", create_new_bucket=True)
 
-    with When("I export parts to the S3 table"):
-        export_parts(
-            source_table="source",
+    with When("I export partitions to the S3 table"):
+        export_partitions(
+            source_table=source_table,
             destination_table=s3_table_name,
             node=self.context.node,
         )
 
     with Then("Check source matches destination"):
         source_matches_destination(
-            source_table="source",
+            source_table=source_table,
             destination_table=s3_table_name,
         )
 
 
 @TestScenario
 def large_export(self):
-    """Test exporting a large part."""
+    """Test exporting partitions with many parts."""
 
+    source_table = f"source_{getuid()}"
     with Given("I create a populated source table and empty S3 table"):
         partitioned_merge_tree_table(
-            table_name="source",
+            table_name=source_table,
             partition_by="p",
             columns=default_columns(),
             stop_merges=True,
@@ -216,16 +225,16 @@ def large_export(self):
         )
         s3_table_name = create_s3_table(table_name="s3", create_new_bucket=True)
 
-    with When("I export parts to the S3 table"):
-        export_parts(
-            source_table="source",
+    with When("I export partitions to the S3 table"):
+        export_partitions(
+            source_table=source_table,
             destination_table=s3_table_name,
             node=self.context.node,
         )
 
     with Then("Check source matches destination"):
         source_matches_destination(
-            source_table="source",
+            source_table=source_table,
             destination_table=s3_table_name,
         )
 
