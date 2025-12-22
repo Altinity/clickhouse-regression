102bd900b9b76d6990f5d7573675b4815701a1e7
alsugiliazova <alsugilyazova1@mail.ru>
2025-12-22 13:54:17 +0100

    update iceberg regression

--- iceberg/regression.py
+++ iceberg/regression.py
@@ -15,6 +15,7 @@ from helpers.common import (
     check_clickhouse_version,
     check_if_not_antalya_build,
     check_is_altinity_build,
+    experimental_analyzer,
 )
 
 
@@ -188,6 +189,12 @@ ffails = {
         "setting used for test introduced in 25.8",
         check_clickhouse_version("<25.8"),
     ),
+    "/iceberg/iceberg engine/rest catalog/iceberg iterator race condition/iceberg iterator race condition": (
+        Skip,
+        "https://github.com/ClickHouse/ClickHouse/issues/92120",
+        lambda test: check_clickhouse_version(">=25.8.12")(test)
+        and check_if_not_antalya_build(test),
+    ),
     # "/iceberg/iceberg engine/: catalog/feature/alter:/*": (
     #     Skip,
     #     "https://github.com/clickhouse/clickhouse/issues/86024",
@@ -250,6 +257,10 @@ def regression(
     self.context.node2 = self.context.cluster.node("clickhouse2")
     self.context.node3 = self.context.cluster.node("clickhouse3")
     self.context.nodes = [self.context.node, self.context.node2, self.context.node3]
+
+    with And("enable or disable experimental analyzer if needed"):
+        for node in self.context.nodes:
+            experimental_analyzer(node=node, with_analyzer=with_analyzer)
 
     Feature(
         test=load("iceberg.tests.iceberg_engine.feature", "feature"),
