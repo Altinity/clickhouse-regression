86ca717ff23dd830152dac008d4de814696816ef
Julian Huang <jhuang@altinity.com>
2025-12-23 15:38:57 -0500

    PR 1229 tests - export max bytes per file and rows perfile

--- s3/tests/export_partition/feature.py
+++ s3/tests/export_partition/feature.py
@@ -29,11 +29,11 @@ def minio(self, uri, bucket_prefix):
     # Feature(run=load("s3.tests.export_partition.schema_evolution", "feature"))
     # Feature(run=load("s3.tests.export_partition.parallel_export_partition", "feature"))
     # Feature(
-        # run=load("s3.tests.export_partition.alter_destination_during_export", "feature")
+    # run=load("s3.tests.export_partition.alter_destination_during_export", "feature")
     # )
     # Feature(run=load("s3.tests.export_partition.alter_source_timing", "feature"))
     # Feature(run=load("s3.tests.export_partition.replica_failover", "feature"))
     # Feature(run=load("s3.tests.export_partition.versions", "feature"))
     # Feature(
-        # run=load("s3.tests.export_partition.parallel_inserts_and_selects", "feature")
+    # run=load("s3.tests.export_partition.parallel_inserts_and_selects", "feature")
     # )
