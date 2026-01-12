5d6087d4138764e2bd801b750d0954bbb12c74fe
alsugiliazova <alsugilyazova1@mail.ru>
2026-01-12 14:46:49 +0100

    hybrid table sql fuzzing

---
+++ ice/steps/hybrid.py
@@ -0,0 +1,38 @@
+from testflows.core import *
+
+from helpers.common import getuid
+
+
+@TestStep(Given)
+def create_hybrid_table(
+    self,
+    table_name=None,
+    node=None,
+    database_name="default",
+    left_table_name=None,
+    right_table_name=None,
+    left_predicate=None,
+    right_predicate=None,
+):
+    """Test step to create a hybrid table."""
+    if node is None:
+        node = self.context.node
+
+    if table_name is None:
+        table_name = f"hybrid_table_{getuid()}"
+
+    query = f"SET allow_experimental_hybrid_table = 1;\n"
+
+    query += f"CREATE TABLE {database_name}.{table_name}\n"
+
+    query += f"ENGINE = Hybrid({left_table_name}, {left_predicate}, {right_table_name}, {right_predicate})\n"
+
+    try:
+        with By(f"creating hybrid table {database_name}.{table_name}"):
+            node.query(query)
+            yield table_name
+
+    finally:
+        with Finally(f"drop the hybrid table {database_name}.{table_name}"):
+            pass
+            # node.query(f"DROP TABLE IF EXISTS {database_name}.{table_name}")
