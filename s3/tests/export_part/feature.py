9a31ad3614eacfe320b078a7348ff5892b40c297
Julian Huang <jhuang@altinity.com>
2026-01-22 07:39:07 -0500

    Default, alias, materialized, ephemeral columns for export part

--- s3/tests/export_part/feature.py
+++ s3/tests/export_part/feature.py
@@ -23,7 +23,7 @@ def minio(self, uri, bucket_prefix):
     Feature(run=load("s3.tests.export_part.shards", "feature"))
     Feature(run=load("s3.tests.export_part.engines_volumes", "feature"))
     Feature(run=load("s3.tests.export_part.datatypes", "feature"))
-    # Feature(run=load("s3.tests.export_part.columns", "feature"))
+    Feature(run=load("s3.tests.export_part.columns", "feature"))
     Feature(run=load("s3.tests.export_part.network", "feature"))
     Feature(run=load("s3.tests.export_part.system_monitoring", "feature"))
     Feature(run=load("s3.tests.export_part.concurrent_alter", "feature"))
