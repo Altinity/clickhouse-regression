e048734d1f8cf7c9e16e50a74feddbea7dd91ba2
CarlosFelipeOR <carlosfelipeor@gmail.com>
2026-02-05 13:43:34 -0300

    Add xfail for zero-copy replication tests

--- s3/regression.py
+++ s3/regression.py
@@ -37,6 +37,18 @@ xfails = {
     ":/:/zero copy replication/:mutation/:/the size of the s3 bucket:": [
         (Fail, "test doesn't clean up, needs investigation")
     ],
+    ":/:/zero copy replication/bad detached part": [
+        (Fail, "https://github.com/Altinity/ClickHouse/issues/1338")
+    ],
+    ":/:/zero copy replication/add remove one replica": [
+        (Fail, "https://github.com/Altinity/ClickHouse/issues/1338")
+    ],
+    ":/:/zero copy replication/add remove replica parallel": [
+        (Fail, "https://github.com/Altinity/ClickHouse/issues/1338")
+    ],
+    ":/:/zero copy replication/insert multiple replicas": [
+        (Fail, "https://github.com/Altinity/ClickHouse/issues/1338")
+    ],
     "minio/:/backup/:/alter freeze": [(Fail, "External disks do not create backups")],
     "minio/:/disk/environment credentials/:": [
         (Fail, "AWS S3 credentials not set for minio tests.")
