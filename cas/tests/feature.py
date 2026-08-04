5429277d8814968ad0f8371e90856e6efc600b0d
Alsu <74852806+alsugiliazova@users.noreply.github.com>
2026-08-04 17:05:42 +0300

    add ref_collision test to cas suite

--- cas/tests/feature.py
+++ cas/tests/feature.py
@@ -5,4 +5,10 @@ from testflows.core import *
 @Name("content addressed storage")
 def feature(self):
     """Content-addressed storage test suite."""
-    Feature(run=load("cas.tests.sanity", "feature"))
+    # Feature(run=load("cas.tests.sanity", "feature"))
+    # Feature(run=load("cas.tests.partition", "feature"))
+    # Feature(run=load("cas.tests.concurrent_attach", "feature"))
+    # Feature(run=load("cas.tests.pool_corruption", "feature"))
+    Feature(run=load("cas.tests.ref_collision", "feature"))
+    # Feature(run=load("cas.tests.server_root_id_unique", "feature"))
+    # Feature(run=load("cas.tests.stress_create_drop", "feature"))
