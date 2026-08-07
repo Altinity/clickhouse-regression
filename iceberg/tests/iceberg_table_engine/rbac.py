bb59bfbb270759792fceabd983d77bb38b8969b2
Julian Huang <jhuang@altinity.com>
2026-08-06 21:03:36 -0400

    Fix flaky drop table check

--- iceberg/tests/iceberg_table_engine/rbac.py
+++ iceberg/tests/iceberg_table_engine/rbac.py
@@ -78,10 +78,10 @@ def drop_table(self, minio_root_user, minio_root_password, node=None):
         )
 
     with And("check that table is dropped"):
-        res = node.query(f"SHOW TABLES")
         for retry in retries(count=10, delay=1):
             with retry:
-                assert res.output == "", error()
+                res = node.query(f"SHOW TABLES")
+                assert iceberg_table_name not in res.output, error()
 
 
 @TestFeature
