e98f2cb35d53ea19aa4df9ae72944f63b6208a82
Julian Huang <jhuang@altinity.com>
2026-08-22 13:31:48 -0400

    atomic insert cas s3 cache support

--- atomic_insert/regression.py
+++ atomic_insert/regression.py
@@ -12,6 +12,7 @@ from helpers.common import check_clickhouse_version
 from atomic_insert.requirements import *
 
 from atomic_insert.tests.steps import *
+from helpers.cas_storage import add_cas_arguments
 from atomic_insert.cas_mode import (
     enable_cas_default_storage,
     reset_cas_config,
@@ -20,12 +21,9 @@ from atomic_insert.cas_mode import (
 
 def argparser(parser):
     argparser_base(parser)
-    parser.add_argument(
-        "--cas",
-        action="store_true",
-        default=False,
-        dest="use_cas",
-        help="use content-addressed storage as the default MergeTree disk",
+    add_cas_arguments(
+        parser,
+        cas_help="use content-addressed storage as the default MergeTree disk",
     )
 
 ffails = {
@@ -74,6 +72,7 @@ def regression(
     stress=None,
     with_analyzer=False,
     use_cas=False,
+    use_cas_s3_cache=False,
 ):
     """ClickHouse atomic inserts regression."""
     nodes = {
@@ -90,9 +89,13 @@ def regression(
     if stress is not None:
         self.context.stress = stress
 
-    if use_cas:
-        with Given("content-addressed storage as the default MergeTree disk"):
-            enable_cas_default_storage()
+    if use_cas_s3_cache or use_cas:
+        with Given(
+            "content-addressed storage with an S3 cache disk as the default MergeTree disk"
+            if use_cas_s3_cache
+            else "content-addressed storage as the default MergeTree disk"
+        ):
+            enable_cas_default_storage(s3_cache=use_cas_s3_cache)
     else:
         with Given("no content-addressed storage configuration"):
             reset_cas_config()
