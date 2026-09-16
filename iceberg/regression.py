c29164fb8baef40b18187d8faae07da9725545cd
selfeer <selfeers@gmail.com>
2026-09-16 23:47:10 +0400

    revert some changes

--- iceberg/regression.py
+++ iceberg/regression.py
@@ -874,17 +874,17 @@ def regression(
         test=load("iceberg.tests.export_partition.feature", "feature"),
     )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
 
-    Feature(
-        test=load("iceberg.tests.deletion_vectors.feature", "feature"),
-    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
-
-    Feature(
-        test=load("iceberg.tests.deletion_vectors.delta_feature", "feature"),
-    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
-
-    Feature(
-        test=load("iceberg.tests.cluster_join_filter.feature", "feature"),
-    )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    # Feature(
+    #     test=load("iceberg.tests.deletion_vectors.feature", "feature"),
+    # )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    #
+    # Feature(
+    #     test=load("iceberg.tests.deletion_vectors.delta_feature", "feature"),
+    # )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
+    #
+    # Feature(
+    #     test=load("iceberg.tests.cluster_join_filter.feature", "feature"),
+    # )(minio_root_user=minio_root_user, minio_root_password=minio_root_password)
 
     # Feature(
     #     test=load("iceberg.tests.hybrid.feature", "feature"),
